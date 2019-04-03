a4710ab10e5141de94c0ab7ab208d20dd500aa8b
Guo, Quan <guoquanscu@gmail.com>
2019-04-03 22:10:04 +0000

    Update expose a interface to loss function in model so we can inject the graph as loss function.

--- examples/emr/emr/__main__.py
+++ examples/emr/emr/__main__.py
@@ -4,7 +4,8 @@ import torch
 
 from .conll04reader import Conll04DatasetReader
 from .model import get_model, get_trainer
-
+from .model import sequence_cross_entropy_with_logits_loss_func as loss_func
+# NB: the loss_func will be replace by graph
 
 relative_path = "data/EntityMentionRelation"
 
@@ -16,7 +17,7 @@ EPOCH = 1000
 PATIENCE = 10
 
 #import logging
-#logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
 
 torch.manual_seed(1)
 
@@ -33,8 +34,14 @@ def main():
     # get model
     model = get_model(vocab, EMBEDDING_DIM, HIDDEN_DIM)
 
+    # TODO:
+    # bind the output of model to the graph
+    # and
+    # get a loss_func out of a graph
+    # loss_func = ...
+
     # get trainer
-    trainer = get_trainer(model, vocab, train_dataset, validation_dataset,
+    trainer = get_trainer(model, loss_func, vocab, train_dataset, validation_dataset,
                           LR, BATCH, EPOCH, PATIENCE)
 
     # train the model
--- examples/emr/emr/model.py
+++ examples/emr/emr/model.py
@@ -1,17 +1,66 @@
+import abc
 from typing import List, Dict
 from allennlp.models import Model
 from allennlp.modules.text_field_embedders import TextFieldEmbedder
 from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder
 from allennlp.training.metrics import CategoricalAccuracy
 from allennlp.data.vocabulary import Vocabulary
-from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
+from allennlp.nn.util import get_text_field_mask
 import torch
 
 
-class LstmTagger(Model):
+class BaseTagger(Model):
+    __metaclass__ = abc.ABCMeta
+
     import logging
     logger = logging.getLogger(__name__)
 
+    def __init__(self, vocab: Vocabulary) -> None:
+        super().__init__(vocab)
+
+    @abc.abstractmethod
+    def _forward(self,
+                 sentence: Dict[str, torch.Tensor],
+                 mask: torch.Tensor): pass
+
+    def _update_metrics(self, output, labels, mask):
+        for metric_name, metric in self.metrics.items():
+            metric(output['logits'], labels, mask)
+            output[metric_name] = metric.get_metric(False)
+
+    def _update_loss(self, output, labels, mask):
+        if self.loss_func is not None:
+            output['loss'] = self.loss_func(output["logits"], labels, mask)
+
+    def forward(self,
+                sentence: Dict[str, torch.Tensor],
+                labels: torch.Tensor = None,
+                labels_mask: List[torch.LongTensor] = None,
+                ) -> Dict[str, torch.Tensor]:
+        text_mask = get_text_field_mask(sentence)
+        self.logger.debug(text_mask)
+
+        output = self._forward(sentence, text_mask)
+
+        if labels is not None:
+            self.logger.debug(labels_mask)
+            # label mask apply to metric and loss
+            metric_mask = text_mask & labels_mask.type_as(text_mask)
+            self.logger.debug(metric_mask)
+
+            self._update_metrics(output, labels, metric_mask)
+            self._update_loss(output, labels, metric_mask)
+
+        return output
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        output = {}
+        for metric_name, metric in self.metrics.items():
+            output[metric_name] = metric.get_metric(reset)
+        return output
+
+
+class Tagger(BaseTagger):
     def __init__(self,
                  word_embeddings: TextFieldEmbedder,
                  encoder: Seq2SeqEncoder,
@@ -22,34 +71,28 @@ class LstmTagger(Model):
         self.hidden2tag = torch.nn.Linear(
             in_features=encoder.get_output_dim(),
             out_features=vocab.get_vocab_size('labels'))
-        self.accuracy = CategoricalAccuracy()
+        self.metrics = {"accuracy": CategoricalAccuracy()}
 
-    def forward(self,
-                sentence: Dict[str, torch.Tensor],
-                labels: torch.Tensor = None,
-                labels_mask: List[torch.LongTensor] = None,
-                ) -> Dict[str, torch.Tensor]:
-        text_mask = get_text_field_mask(sentence)
+    def _forward(self,
+                 sentence: Dict[str, torch.Tensor],
+                 mask: torch.Tensor,
+                 ) -> Dict[str, torch.Tensor]:
         embeddings = self.word_embeddings(sentence)
-        encoder_out = self.encoder(embeddings, text_mask)
-        tag_logits = self.hidden2tag(encoder_out)
-        output = {"tag_logits": tag_logits}
-        if labels is not None:
-            LstmTagger.logger.debug(labels_mask)
-            LstmTagger.logger.debug(text_mask)
-            # label mask apply to metric and loss
-            metric_mask = text_mask & labels_mask.type_as(text_mask)
-            LstmTagger.logger.debug(metric_mask)
-            self.accuracy(tag_logits, labels, metric_mask)
-            output["loss"] = sequence_cross_entropy_with_logits(
-                tag_logits, labels, metric_mask)
-
+        encoder_out = self.encoder(embeddings, mask)
+        logits = self.hidden2tag(encoder_out)
+        output = {'logits': logits}
         return output
 
-    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
-        return {"accuracy": self.accuracy.get_metric(reset)}
+
+# loss function
+from allennlp.nn.util import sequence_cross_entropy_with_logits
 
 
+def sequence_cross_entropy_with_logits_loss_func(logits, labels, mask):
+    return sequence_cross_entropy_with_logits(logits, labels, mask)
+
+
+# model
 from allennlp.modules.token_embedders import Embedding
 from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
 from allennlp.modules.seq2seq_encoders import PytorchSeq2SeqWrapper
@@ -61,16 +104,19 @@ def get_model(vocab, emb_dim, hid_dim):
     word_embeddings = BasicTextFieldEmbedder({"tokens": token_embedding})
     lstm = PytorchSeq2SeqWrapper(torch.nn.LSTM(
         emb_dim, hid_dim, batch_first=True))
-    model = LstmTagger(word_embeddings, lstm, vocab)
+    model = Tagger(word_embeddings, lstm, vocab)
     return model
 
 
+# trainer
 from allennlp.training.trainer import Trainer
 from allennlp.data.iterators import BucketIterator
 import torch.optim as optim
 
 
-def get_trainer(model, vocab, train_dataset, validation_dataset, lr=0.1, batch=16, epoch=1000, patience=10):
+def get_trainer(model, loss_func, vocab, train_dataset, validation_dataset, lr=0.1, batch=16, epoch=1000, patience=10):
+    model.loss_func = loss_func
+
     # prepare GPU
     if torch.cuda.is_available():
         device = 0
