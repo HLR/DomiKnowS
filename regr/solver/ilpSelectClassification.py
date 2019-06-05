3a87032724f97593cafaeb59545efda1fca26bfa
Andrzej Uszok <auszok@ihmc.us>
2019-06-05 10:46:15 -0500

    Additional constrains for concept based on ontology

--- regr/solver/ilpSelectClassification.py
+++ regr/solver/ilpSelectClassification.py
@@ -19,11 +19,11 @@ from pathlib import Path
 if __package__ is None or __package__ == '':
     from regr.graph.concept import Concept, enum
     from regr.graph.graph import Graph
-    from regr.graph.relation import Relation, Be, Have
+    #from regr.graph.relation import Relation, IsA, HasA
 else:
     from ..graph.concept import Concept, enum
     from ..graph.graph import Graph
-    from ..graph.relation import Relation, Be, Have
+    #from ..graph.relation import Relation, IsA, HasA
 
 def loadOntology(ontologyURL, ontologyPathname = "./"):
     
@@ -36,7 +36,7 @@ def loadOntology(ontologyURL, ontologyPathname = "./"):
         print("Path to load Graph ontology: %s does not exists in current directory %s"%(graphMetaOntologyPath,currentPath))
         exit()
         
-    # Check if ontology path is correct
+    # Check if specific ontology path is correct
     ontologyPath = Path(os.path.normpath(ontologyPathname))
     ontologyPath = ontologyPath.resolve()
     if not os.path.isdir(ontologyPath):
@@ -44,26 +44,34 @@ def loadOntology(ontologyURL, ontologyPathname = "./"):
         exit()
 
     onto_path.append(graphMetaOntologyPath)  # the folder with the Graph Meta ontology
-    onto_path.append(ontologyPath) # the folder with the ontology
+    onto_path.append(ontologyPath) # the folder with the ontology for the specific  graph
 
-    # Load ontology
+    # Load specific ontology
     try :
         myOnto = get_ontology(ontologyURL)
         myOnto.load(only_local = True, fileobj = None, reload = False, reload_if_newer = False)
     except FileNotFoundError as e:
-        print('Error when loading - ' + ontologyURL + " from: %s"%(ontologyPath))
+        print("Error when loading - %s from: %s"%(ontologyURL,ontologyURL,ontologyPath))
 
     return myOnto
-        
+
 def addTokenConstrains(m, myOnto, tokens, conceptNames, x, graphResultsForPhraseToken):
         
+    # Create variables for token - concept and negative variables
     for token in tokens:            
         for conceptName in conceptNames: 
             x[token, conceptName]=m.addVar(vtype=GRB.BINARY,name="x_%s_%s"%(token, conceptName))
+            x[token, conceptName+'-neg']=m.addVar(vtype=GRB.BINARY,name="x_%s_%s"%(token, conceptName+'-neg'))
+
+    # Add constraints forcing decision between variable and negative variables 
+    for conceptName in conceptNames:
+        for token in tokens:
+            constrainName = 'c_%s_%sselfDisjoint'%(token, conceptName)
+            m.addConstr(x[token, conceptName] + x[token, conceptName+'-neg'], GRB.LESS_EQUAL, 1, name=constrainName)
             
     m.update()
      
-    # Add constraints based on concept disjoint statments in ontology
+    # -- Add constraints based on concept disjoint statements in ontology - not(and(var1, var2)) = nand(var1, var2)
     foundDisjoint = dict() # too eliminate duplicates
     for conceptName in conceptNames:
         
@@ -93,23 +101,146 @@ def addTokenConstrains(m, myOnto, tokens, conceptNames, x, graphResultsForPhrase
                     continue
                         
             for token in tokens:
-                constrainName = 'c_%s_%sDisjoint%s'%(token, currentConcept, disjointConcept)
+                constrainName = 'c_%s_%s_Disjoint_%s'%(token, conceptName, disjointConcept)
                 m.addConstr(x[token, conceptName] + x[token, disjointConcept], GRB.LESS_EQUAL, 1, name=constrainName)
                   
             #print("disjointConcept %s %s"%(currentConcept._name, disjointConcept))   
-                   
+                        
             if not (conceptName in foundDisjoint):
                 foundDisjoint[conceptName] = {disjointConcept}
             else:
                 foundDisjoint[conceptName].add(disjointConcept)
+       
+    # -- Add constraints based on concept equivalent (sameAs) statements in ontology - and(var1, av2)
+    foundEquivalent = dict() # too eliminate duplicates
+    for conceptName in conceptNames:
         
+        currentConcept = myOnto.search_one(iri = "*%s"%(conceptName))
+            
+        if currentConcept is None :
+            continue
+            
+        for equivalentConcept in currentConcept.equivalent_to:
+            if equivalentConcept.name not in graphResultsForPhraseToken.columns:
+                 continue
+                    
+            if conceptName in foundEquivalent:
+                if equivalentConcept.name in foundEquivalent[conceptName]:
+                    continue
+            
+            if equivalentConcept.name in foundEquivalent:
+                if conceptName in foundEquivalent[equivalentConcept.name]:
+                    continue
+                        
+            for token in tokens:
+                constrainName = 'c_%s_%s_Equivalent_%s'%(token, conceptName, equivalentConcept.name)
+                _varAnd = m.addVar(name="andVar_%s"%(constrainName))
+    
+                m.addGenConstrAnd(_varAnd, [x[token, conceptName], x[token, equivalentConcept.name]], constrainName)
+                #m.addConstr(x[token, conceptName] + x[token, equivalentConcept], GRB.LESS_EQUAL, 1, name=constrainName) #?
+                                         
+            if not (conceptName in foundEquivalent):
+                foundEquivalent[conceptName] = {equivalentConcept.name}
+            else:
+                foundEquivalent[conceptName].add(equivalentConcept.name)
+       
+
+    # -- Add constraints based on concept subClassOf statements in ontology - var1 -> var2
+    for conceptName in conceptNames :
+        
+        currentConcept = myOnto.search_one(iri = "*%s"%(conceptName))
+            
+        if currentConcept is None :
+            continue
+            
+        for ancestorConcept in currentConcept.ancestors(include_self = False) :
+            if ancestorConcept.name not in graphResultsForPhraseToken.columns :
+                 continue
+                        
+            for token in tokens:
+                constrainName = 'c_%s_%s_Ancestor_%s'%(token, currentConcept, ancestorConcept.name)
+                m.addGenConstrIndicator(x[token, conceptName], True, x[token, ancestorConcept.name], GRB.EQUAL, 1)
+       
+    # -- Add constraints based on concept intersection statements in ontology - and(var1, var2, var3, ..)
+    for conceptName in conceptNames :
+        
+        currentConcept = myOnto.search_one(iri = "*%s"%(conceptName))
+            
+        if currentConcept is None :
+            continue
+            
+        for conceptConstruct in currentConcept.constructs(Prop = None) :
+            if type(conceptConstruct) is And :
+                
+                for token in tokens:
+                    
+                    constrainName = 'c_%s_%s_Intersection'%(token, conceptName)
+                    _varAnd = m.addVar(name="andVar_%s"%(constrainName))
+    
+                    andList = []
+                
+                    for currentClass in conceptConstruct.Classes :
+                        andList.append(x[token, currentClass.name])
+
+                    andList.append(x[token, conceptName])
+                    
+                    m.addGenConstrAnd(_varAnd, andList, constrainName)
+    
+    # -- Add constraints based on concept union statements in ontology -  or(var1, var2, var3, ..)
+    for conceptName in conceptNames :
+        
+        currentConcept = myOnto.search_one(iri = "*%s"%(conceptName))
+            
+        if currentConcept is None :
+            continue
+            
+        for conceptConstruct in currentConcept.constructs(Prop = None) :
+            if type(conceptConstruct) is Or :
+                
+                for token in tokens:
+                    
+                    constrainName = 'c_%s_%s_Union'%(token, conceptName)
+                    _varOr = m.addVar(name="orVar_%s"%(constrainName))
+    
+                    orList = []
+                
+                    for currentClass in conceptConstruct.Classes :
+                        orList.append(x[token, currentClass.name])
+
+                    orList.append(x[token, conceptName])
+                    
+                    m.addGenConstrOr(_varOr, orList, constrainName)
+    
+    # -- Add constraints based on concept objectComplementOf statements in ontology - var1 + var 2 = 1
+    for conceptName in conceptNames :
+        
+        currentConcept = myOnto.search_one(iri = "*%s"%(conceptName))
+            
+        if currentConcept is None :
+            continue
+            
+        for conceptConstruct in currentConcept.constructs(Prop = None) :
+            if type(conceptConstruct) is Not :
+                
+                for token in tokens:                    
+                    for currentClass in conceptConstruct.Classes :
+                        constrainName = 'c_%s_%s_ComplementOf_%s'%(token, conceptName, currentClass)
+                        
+                        m.addConstr(x[token, conceptName] + x[token, currentClass], GRB.EQUAL, 1, name=constrainName)
+
+    # -- Add constraints based on concept disjonitUnion statements in ontology - 
+    
+    # -- Add constraints based on concept oneOf statements in ontology - 
+
     m.update()
             
+    # Add objectives
     X_Q = None
     for token in tokens :
         for conceptName in conceptNames :
             X_Q += graphResultsForPhraseToken[conceptName][token]*x[token, conceptName]
-    
+            X_Q += (1-graphResultsForPhraseToken[conceptName][token])*x[token, conceptName+'-neg']
+
     return X_Q
     
 def addRelationsConstrains(m, myOnto, tokens, conceptNames, x, y, graphResultsForPhraseRelation):
@@ -125,9 +256,19 @@ def addRelationsConstrains(m, myOnto, tokens, conceptNames, x, y, graphResultsFo
                 y[relationName, token, token1]=m.addVar(vtype=GRB.BINARY,name="y_%s_%s_%s"%(relationName, token, token1))
                 y[relationName+'-neg', token, token1]=m.addVar(vtype=GRB.BINARY,name="y_%s-neg_%s_%s"%(relationName, token, token1))
           
+    # Add constraints forcing decision between variable and negative variables 
+    for relationName in relationNames :
+        for token in tokens: 
+            for token1 in tokens:
+                if token == token1:
+                    continue
+                
+                constrainName = 'c_%s_%s_%sselfDisjoint'%(token, token1, relationName)
+                m.addConstr(y[relationName, token, token1] + y[relationName+'-neg', token, token1], GRB.LESS_EQUAL, 1, name=constrainName)
+
     m.update()
 
-    # Add constraints based on relations domain and range
+    # -- Add constraints based on property domain and range statements in ontology
     for relationName in graphResultsForPhraseRelation :
         currentRelation = myOnto.search_one(iri = "*%s"%(relationName))
             
@@ -152,14 +293,59 @@ def addRelationsConstrains(m, myOnto, tokens, conceptNames, x, y, graphResultsFo
                                 
                         constrainName = 'c_%s_%s_%s'%(currentRelation, token, token1)
                         currentConstrain = y[currentRelation._name, token, token1] + x[token, domain._name] + x[token1, range._name]
-                        #print(currentConstrain)
                         m.addConstr(currentConstrain, GRB.GREATER_EQUAL, 3 * y[currentRelation._name, token, token1], name=constrainName)
 
-                        constrainName = 'c_%s_%s_%sselfDisjoint'%(token, token1, currentRelation)
-                        m.addConstr(y[currentRelation._name, token, token1] + y[currentRelation._name+'-neg', token, token1], GRB.LESS_EQUAL, 1, name=constrainName)
-        
+    # -- Add constraints based on property subProperty statements in ontology
+
+    # -- Add constraints based on property allValueFrom statements in ontology
+
+    # -- Add constraints based on property hasValueFrom statements in ontology
+
+    # -- Add constraints based on property objectHasSelf statements in ontology
+
+    # -- Add constraints based on property minCardinality statements in ontology
+    
+    # -- Add constraints based on property maxCardinality statements in ontology
+
+    # -- Add constraints based on property exactCardinality statements in ontology
+    
+    # -- Add constraints based on property dataSomeValuesFrom statements in ontology
+    
+    # -- Add constraints based on property dataHasValue statements in ontology
+
+    # -- Add constraints based on property dataAllValuesFrom statements in ontology
+
+    # -- Add constraints based on property allValueFrom statements in ontology
+
+    # -- Add constraints based on property equivalentProperty statements in ontology
+    
+    # -- Add constraints based on property disjointProperty statements in ontology
+
+    # -- Add constraints based on property inverseProperty statements in ontology
+    
+    # -- Add constraints based on property functionalProperty statements in ontology
+    
+    # -- Add constraints based on property inverseProperty statements in ontology
+
+    # -- Add constraints based on property inverseFunctiona;Property statements in ontology
+
+    # -- Add constraints based on property reflexiveProperty statements in ontology
+
+    # -- Add constraints based on property irreflexiveProperty statements in ontology
+
+    # -- Add constraints based on property symetricProperty statements in ontology
+
+    # -- Add constraints based on property asymetricProperty statements in ontology
+    
+    # -- Add constraints based on property transitiveProperty statements in ontology
+
+    # -- Add constraints based on property symetricProperty statements in ontology
+
+    # -- Add constraints based on property key statements in ontology
+
     m.update()
 
+    # Add objectives
     Y_Q  = None
     for relationName in relationNames:
         for token in tokens:
@@ -184,14 +370,14 @@ def calculateILPSelection(phrase, graph, graphResultsForPhraseToken, graphResult
         
         myOnto = loadOntology(graph.ontology, ontologyPathname)
 
-        # Get list of tokens, concepts and relations from panda dataframe graphResultsForPhraseToken
+        # Get list of tokens and concepts from panda dataframe graphResultsForPhraseToken
         tokens = graphResultsForPhraseToken.index.tolist()
         conceptNames = graphResultsForPhraseToken.columns.tolist()
         
-        # Create Gurobi variables for concept - token
+        # Gurobi variables for concept - token
         x={}
 
-        # Create Gurobi variables for relation - token, token
+        # Gurobi variables for relation - token, token
         y={}
             
         # -- Set objective - maximize 
@@ -225,7 +411,7 @@ def calculateILPSelection(phrase, graph, graphResultsForPhraseToken, graphResult
         #for v in m.getVars():
         #    print('%s %g' % (v.varName, v.x))
           
-        # Collect results
+        # Collect results for concepts
         tokenResult = pd.DataFrame(0, index=tokens, columns=conceptNames)
         if x or True:
             if m.status == GRB.Status.OPTIMAL:
@@ -238,6 +424,7 @@ def calculateILPSelection(phrase, graph, graphResultsForPhraseToken, graphResult
                             
                             tokenResult[conceptName][token] = 1
 
+        # Collect results for relations
         relationsResult = {}
         if y or True:
             if m.status == GRB.Status.OPTIMAL:
@@ -256,11 +443,7 @@ def calculateILPSelection(phrase, graph, graphResultsForPhraseToken, graphResult
                                 relationResult[token1][token] = 1
                                 
                     relationsResult[relationName] = relationResult
-
-        #print(m)
-        #print(m.getObjective())
-        #print(m.getConstrs())
-
+                    
     except GurobiError as e:
         print('Error code ' + str(e.errno) + ": " + str(e))
     
@@ -281,31 +464,6 @@ with Graph('global') as graph:
             
     with Graph('application') as app_graph:
         app_graph.ontology='http://ontology.ihmc.us/ML/EMR.owl'
-            
-        entity = Concept(name='entity')
-        entity.be(phrase)
-        people = Concept(name='people')
-        people.be(entity)
-        organization = Concept(name='organization')
-        organization.be(entity)
-        other = Concept(name='other')
-        other.be(entity)
-        location = Concept(name='location')
-        location.be(entity)
-        O = Concept(name='O')
-        O.be(entity)
-    
-        work_for = Concept(name='work_for')
-        work_for.be((people, organization))
-    
-        live_in = Concept(name='live_in')
-        live_in.be((people, location))
-    
-        orgbase_on = Concept(name='orgbase_on')
-        orgbase_on.be((organization, location))
-    
-        located_in = Concept(name='located_in')
-        located_in.be((organization, location))
 
 def main() :
     test_phrase = [("John", "NNP"), ("works", "VBN"), ("for", "IN"), ("IBM", "NNP")]
@@ -361,10 +519,11 @@ def main() :
     print("\nResults - ")
     print(tokenResult)
     
-    for name, result in relationsResult.items():
-        print("\n")
-        print(name)
-        print(result)
+    if relationsResult != None :
+        for name, result in relationsResult.items():
+            print("\n")
+            print(name)
+            print(result)
     
 if __name__ == '__main__' :
     main()
