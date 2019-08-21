8f63785ab9fb46029c926de97de643d386621c84
Guo Quan <guoquanscu@gmail.com>
2019-08-20 21:38:31 -0400

    Update ilpConfig.py

--- regr/solver/ilpConfig.py
+++ regr/solver/ilpConfig.py
@@ -1,4 +1,7 @@
+import os
+REGR_SOLVER = 'REGR_SOLVER' in os.environ and os.environ['REGR_SOLVER']
+
 ilpConfig = {
     # variable controlling what ILP solver is used  - one of "Gurobi", "GEKKO", None
-    'ilpSolver' :  "Gurobi" 
+    'ilpSolver' : REGR_SOLVER or 'Gurobi'
 }
