92b66adcbc91fcf0ceb464f59a39f4d1dd3cf28c
zhangyuejoslin <949788345@qq.com>
2019-09-10 10:00:08 +0800

    Update sprlApp.py

--- examples/SpRL_new/sprlApp.py
+++ examples/SpRL_new/sprlApp.py
@@ -3,8 +3,7 @@ from regr.sensor.allennlp.learner import SentenceEmbedderLearner, RNNLearner, Lo
 
 
 from regr.graph.allennlp import AllenNlpGraph
-#from SpRL_reader import SpRLSensorReader as Reader
-from reader_new import SpRLSensorReader as Reader
+from SpRL_reader import SpRLSensorReader as Reader
 from config import Config
 from utils import seed
 
