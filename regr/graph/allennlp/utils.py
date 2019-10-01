d8b850de5ef33c15bab8a1aa0d00b9261e2f7b2c
Guo, Quan <guoquanscu@gmail.com>
2019-10-01 20:02:17 +0000

    WIP structured perceptron

--- regr/graph/allennlp/utils.py
+++ regr/graph/allennlp/utils.py
@@ -145,3 +145,102 @@ def sequence_cross_entropy_with_logits(logits: torch.FloatTensor,
         # shape : (batch_size,)
         per_batch_loss = negative_log_likelihood.sum(non_batch_dims) / (weights_batch_sum + 1e-13)
         return per_batch_loss
+
+
+def structured_perceptron_exact_with_logits(logits: torch.FloatTensor,
+                                            targets: torch.LongTensor,
+                                            inferences: torch.LongTensor,
+                                            weights: torch.FloatTensor,
+                                            average: str = "batch",
+                                            label_smoothing: float = None,
+                                            gamma: float = None,
+                                            eps: float = 1e-8,
+                                            alpha: Union[float, List[float], torch.FloatTensor] = None
+                                           ) -> torch.FloatTensor:
+    if average not in {None, "token", "batch"}:
+        raise ValueError("Got average f{average}, expected one of "
+                         "None, 'token', or 'batch'")
+
+    # make sure weights are float
+    weights = weights.float()
+    # sum all dim except batch
+    non_batch_dims = tuple(range(1, len(weights.shape)))
+    # shape : (batch_size,)
+    weights_batch_sum = weights.sum(dim=non_batch_dims)
+    # shape : (batch * sequence_length, num_classes)
+    logits_flat = logits.view(-1, logits.size(-1))
+    # shape : (batch * sequence_length, num_classes)
+    log_probs_flat = torch.nn.functional.log_softmax(logits_flat, dim=-1)
+    # shape : (batch * max_len, 1)
+    targets_flat = targets.view(-1, 1).long()
+    # focal loss coefficient
+    if gamma:
+        # shape : (batch * sequence_length, num_classes)
+        probs_flat = log_probs_flat.exp()
+        eps = torch.tensor(eps, device=probs_flat.device)
+        probs_flat = probs_flat.min(1 - eps)
+        probs_flat = probs_flat.max(eps)
+        # shape : (batch * sequence_length,)
+        probs_flat = torch.gather(probs_flat, dim=1, index=targets_flat)
+        # shape : (batch * sequence_length,)
+        focal_factor = (1. - probs_flat) ** gamma
+        # shape : (batch, sequence_length)
+        focal_factor = focal_factor.view(*targets.size())
+        weights = weights * focal_factor
+
+    if alpha is not None:
+        # shape : () / (num_classes,)
+        if isinstance(alpha, (float, int)):
+            # pylint: disable=not-callable
+            # shape : (2,)
+            alpha_factor = torch.tensor([1. - float(alpha), float(alpha)],
+                                        dtype=weights.dtype, device=weights.device)
+            # pylint: enable=not-callable
+        elif isinstance(alpha, (list, numpy.ndarray, torch.Tensor)):
+            # pylint: disable=not-callable
+            # shape : (c,)
+            alpha_factor = torch.tensor(alpha, dtype=weights.dtype, device=weights.device)
+            # pylint: enable=not-callable
+            if not alpha_factor.size():
+                # shape : (1,)
+                alpha_factor = alpha_factor.view(1)
+                # shape : (2,)
+                alpha_factor = torch.cat([1 - alpha_factor, alpha_factor])
+        else:
+            raise TypeError(('alpha must be float, list of float, or torch.FloatTensor, '
+                             '{} provided.').format(type(alpha)))
+        # shape : (batch, max_len)
+        alpha_factor = torch.gather(alpha_factor, dim=0, index=targets_flat.view(-1)).view(*targets.size())
+        weights = weights * alpha_factor
+
+    if label_smoothing is not None and label_smoothing > 0.0:
+        num_classes = logits.size(-1)
+        smoothing_value = label_smoothing / num_classes
+        # Fill all the correct indices with 1 - smoothing value.
+        one_hot_targets = torch.zeros_like(log_probs_flat).scatter_(-1, targets_flat, 1.0 - label_smoothing)
+        smoothed_targets = one_hot_targets + smoothing_value
+        negative_log_likelihood_flat = - log_probs_flat * smoothed_targets
+        negative_log_likelihood_flat = negative_log_likelihood_flat.sum(-1, keepdim=True)
+    else:
+        # Contribution to the negative log likelihood only comes from the exact indices
+        # of the targets, as the target distributions are one-hot. Here we use torch.gather
+        # to extract the indices of the num_classes dimension which contribute to the loss.
+        # shape : (batch * sequence_length, 1)
+        negative_log_likelihood_flat = torch.gather(log_probs_flat, dim=1, index=inferences) \
+                                     - torch.gather(log_probs_flat, dim=1, index=targets_flat)
+    # shape : (batch, sequence_length)
+    negative_log_likelihood = negative_log_likelihood_flat.view(*targets.size())
+    # shape : (batch, sequence_length)
+    negative_log_likelihood = negative_log_likelihood * weights
+
+    if average == "batch":
+        # shape : (batch_size,)
+        per_batch_loss = negative_log_likelihood.sum(non_batch_dims) / (weights_batch_sum + 1e-13)
+        num_non_empty_sequences = ((weights_batch_sum > 0).float().sum() + 1e-13)
+        return per_batch_loss.sum() / num_non_empty_sequences
+    elif average == "token":
+        return negative_log_likelihood.sum() / (weights_batch_sum.sum() + 1e-13)
+    else:
+        # shape : (batch_size,)
+        per_batch_loss = negative_log_likelihood.sum(non_batch_dims) / (weights_batch_sum + 1e-13)
+        return per_batch_loss
