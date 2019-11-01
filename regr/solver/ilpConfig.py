633d42762dbe69aabf72fff8061fb4357506ea60
Andrzej Uszok <auszok@ihmc.us>
2019-10-31 22:55:47 -0500

    Changed logger levels in solver, changed log file to rotated file and limited its size, added configuration of logger to ilpConfig

--- regr/solver/ilpConfig.py
+++ regr/solver/ilpConfig.py
@@ -1,7 +1,15 @@
 import os
+import logging
 REGR_SOLVER = 'REGR_SOLVER' in os.environ and os.environ['REGR_SOLVER']
 
 ilpConfig = {
     # variable controlling what ILP solver is used  - one of "Gurobi", "GEKKO", None
-    'ilpSolver' : REGR_SOLVER or 'Gurobi'
+    'ilpSolver' : REGR_SOLVER or 'Gurobi',
+    
+    # 'log_name'
+    'log_level' : logging.DEBUG,
+
+    'log_filename' : 'ilpOntSolver.log',
+    'log_filesize' : 5*1024*1024*1024,
+    'log_backupCount' : 5
 }
