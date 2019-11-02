ad2b3d7b52cd452f0f65903fbd069aa1913506ac
Guo, Quan <guoquanscu@gmail.com>
2019-11-02 22:48:32 +0000

    Update lagacy sprl test, avoid ambiguious input and flaky setup.

--- test_regr/solver/test_ilpOntSolver_sprl.py
+++ test_regr/solver/test_ilpOntSolver_sprl.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.fixture()
 def sprl_input(request):
     import numpy as np
@@ -44,7 +45,7 @@ def sprl_input(request):
     test_graphResultsForPhraseToken['TRAJECTOR']         = np.array([0.37, 0.72, 0.78, 0.01, 0.42, 0.22])
     test_graphResultsForPhraseToken['LANDMARK']          = np.array([0.68, 0.15, 0.33, 0.03, 0.43, 0.13])
     test_graphResultsForPhraseToken['SPATIAL_INDICATOR'] = np.array([0.05, 0.03, 0.02, 0.93, 0.03, 0.01])
-    test_graphResultsForPhraseToken['NONE_ENTITY']       = np.array([0.2 , 0.61, 0.48, 0.03, 0.5 , 0.52])
+    test_graphResultsForPhraseToken['NONE_ENTITY']       = np.array([0.2 , 0.61, 0.48, 0.03, 0.51, 0.52])
     
     test_graphResultsForTripleRelations = dict()
     
@@ -53,63 +54,28 @@ def sprl_input(request):
     triplet_relation_table = np.random.rand(6, 6, 6) * 0.2
     triplet_relation_table[0, 1, 3] = 0.85 # ("stairs", "About 20 kids ", "on") - GT
     triplet_relation_table[0, 2, 3] = 0.78 # ("stairs", "About 20 kids", "on") - GT
-    triplet_relation_table[1, 0, 3] = 0.32 # ("About 20 kids ", "stairs", "on")
-    triplet_relation_table[1, 2, 3] = 0.30 # ("About 20 kids ", "About 20 kids", "on")
-    triplet_relation_table[2, 0, 3] = 0.31 # ("About 20 kids", "stairs", "on")
-    triplet_relation_table[2, 1, 3] = 0.30 # ("About 20 kids", "About 20 kids ", "on")
-    triplet_relation_table[0, 4, 3] = 0.42 # ("stairs", "hat", "on")
-    triplet_relation_table[0, 5, 3] = 0.52 # ("stairs", "traditional clothing", "on")
-    triplet_relation_table[1, 4, 3] = 0.32 # ("About 20 kids ", "hat", "on")
-    triplet_relation_table[1, 5, 3] = 0.29 # ("About 20 kids ", "traditional clothing", "on")
-    triplet_relation_table[2, 4, 3] = 0.25 # ("About 20 kids ", "hat", "on")
-    triplet_relation_table[2, 5, 3] = 0.27 # ("About 20 kids ", "traditional clothing", "on")
     # ... can be more
     test_graphResultsForTripleRelations["triplet"] = triplet_relation_table
     
     # spatial_triplet
     # triplet relation is a 3D array
     spatial_triplet_relation_table = np.random.rand(6, 6, 6) * 0.2
-    spatial_triplet_relation_table[0, 1, 3] = 0.25 # ("stairs", "About 20 kids ", "on")
-    spatial_triplet_relation_table[0, 2, 3] = 0.38 # ("stairs", "About 20 kids", "on")
-    spatial_triplet_relation_table[1, 0, 3] = 0.32 # ("About 20 kids ", "stairs", "on")
-    spatial_triplet_relation_table[1, 2, 3] = 0.30 # ("About 20 kids ", "About 20 kids", "on")
-    spatial_triplet_relation_table[2, 0, 3] = 0.31 # ("About 20 kids", "stairs", "on")
-    spatial_triplet_relation_table[2, 1, 3] = 0.30 # ("About 20 kids", "About 20 kids ", "on")
-    spatial_triplet_relation_table[0, 4, 3] = 0.22 # ("stairs", "hat", "on")
-    spatial_triplet_relation_table[0, 5, 3] = 0.12 # ("stairs", "traditional clothing", "on")
-    spatial_triplet_relation_table[1, 4, 3] = 0.22 # ("About 20 kids ", "hat", "on")
-    spatial_triplet_relation_table[1, 5, 3] = 0.39 # ("About 20 kids ", "traditional clothing", "on")
-    spatial_triplet_relation_table[2, 4, 3] = 0.15 # ("About 20 kids ", "hat", "on")
-    spatial_triplet_relation_table[2, 5, 3] = 0.27 # ("About 20 kids ", "traditional clothing", "on")
+    spatial_triplet_relation_table[0, 1, 3] = 0.74 # ("stairs", "About 20 kids ", "on")
+    spatial_triplet_relation_table[0, 2, 3] = 0.83 # ("stairs", "About 20 kids", "on")
     # ... can be more
     test_graphResultsForTripleRelations["spatial_triplet"] = spatial_triplet_relation_table
     
     # region
     # triplet relation is a 3D array
     region_relation_table = np.random.rand(6, 6, 6) * 0.2
-    region_relation_table[0, 1, 3] = 0.25 # ("stairs", "About 20 kids ", "on")
-    region_relation_table[0, 2, 3] = 0.38 # ("stairs", "About 20 kids", "on")
-    region_relation_table[1, 0, 3] = 0.32 # ("About 20 kids ", "stairs", "on")
-    region_relation_table[1, 2, 3] = 0.30 # ("About 20 kids ", "About 20 kids", "on")
-    region_relation_table[2, 0, 3] = 0.31 # ("About 20 kids", "stairs", "on")
-    region_relation_table[2, 1, 3] = 0.30 # ("About 20 kids", "About 20 kids ", "on")
-    region_relation_table[0, 4, 3] = 0.22 # ("stairs", "hat", "on")
-    region_relation_table[0, 5, 3] = 0.12 # ("stairs", "traditional clothing", "on")
-    region_relation_table[1, 4, 3] = 0.22 # ("About 20 kids ", "hat", "on")
-    region_relation_table[1, 5, 3] = 0.39 # ("About 20 kids ", "traditional clothing", "on")
-    region_relation_table[2, 4, 3] = 0.15 # ("About 20 kids ", "hat", "on")
-    region_relation_table[2, 5, 3] = 0.27 # ("About 20 kids ", "traditional clothing", "on")
+    region_relation_table[0, 1, 3] = 0.65 # ("stairs", "About 20 kids ", "on")
+    region_relation_table[0, 2, 3] = 0.88 # ("stairs", "About 20 kids", "on")
     # ... can be more
     test_graphResultsForTripleRelations["region"] = region_relation_table
-    
-    # none_relation
-    # triplet relation is a 3D array
-    none_relation_relation_table = np.random.rand(6, 6, 6) * 0.8
-    none_relation_relation_table[0, 1, 3] = 0.15 # ("stairs", "About 20 kids ", "on")
-    none_relation_relation_table[0, 2, 3] = 0.08 # ("stairs", "About 20 kids", "on")
-    test_graphResultsForTripleRelations["none_relation"] = none_relation_relation_table
+
 
     yield test_phrase, test_graphResultsForPhraseToken, test_graphResultsForTripleRelations
+
 
 @pytest.mark.gurobi
 def test_main_sprl(sprl_input):
@@ -129,30 +95,29 @@ def test_main_sprl(sprl_input):
     # evaluate
     #------------------
     # -- phrase results:
-    assert tokenResult['LANDMARK'].sum() == 1
     assert tokenResult['LANDMARK'][0] == 1 # "stairs" - LANDMARK
+    assert tokenResult['LANDMARK'].sum() == 1
 
-    assert tokenResult['TRAJECTOR'].sum() == 2
     assert tokenResult['TRAJECTOR'][1] == 1 # "About 20 kids" - TRAJECTOR
     assert tokenResult['TRAJECTOR'][2] == 1 # "About 20 kids " - TRAJECTOR
+    assert tokenResult['TRAJECTOR'].sum() == 2
 
-    assert tokenResult['SPATIAL_INDICATOR'].sum() == 1
     assert tokenResult['SPATIAL_INDICATOR'][3] == 1 # "on" - SPATIALINDICATOR
+    assert tokenResult['SPATIAL_INDICATOR'].sum() == 1
 
-    assert tokenResult['NONE_ENTITY'].sum() == 1 # instead of 2
-    #assert tokenResult['NONE_ENTITY'][4] == 1 # "hats" - NONE
+    assert tokenResult['NONE_ENTITY'][4] == 1 # "hats" - NONE
     assert tokenResult['NONE_ENTITY'][5] == 1  # "traditional clothing" - NONE
-        
+    assert tokenResult['NONE_ENTITY'].sum() == 2
+
     # -- relation results:
-    assert tripleRelationsResult['triplet'].sum() == 3 # instead of 2
     assert tripleRelationsResult['triplet'][0, 1, 3] == 1 # ("stairs", "About 20 kids ", "on")
     assert tripleRelationsResult['triplet'][0, 2, 3] == 1 # ("stairs", "About 20 kids", "on")
-    # one more ?
-    
-    assert tripleRelationsResult['region'].sum() == 0 # 0 elsewhere
-    
-    assert tripleRelationsResult['spatial_triplet'].sum() == 0 # 0 elsewhere
-    
-    #assert tripleRelationsResult['none_relation'].sum() == 58 # 4 * 6 * 6 - 3 elsewhere
-    assert tripleRelationsResult['none_relation'][0, 1, 3] == 0 # ("stairs", "About 20 kids ", "on")
-    assert tripleRelationsResult['none_relation'][0, 2, 3] == 0 # ("stairs", "About 20 kids", "on")
+    assert tripleRelationsResult['triplet'].sum() == 2
+
+    assert tripleRelationsResult['region'][0, 1, 3] == 1 # ("stairs", "About 20 kids ", "on")
+    assert tripleRelationsResult['region'][0, 2, 3] == 1 # ("stairs", "About 20 kids", "on")
+    assert tripleRelationsResult['region'].sum() == 2 # 0 elsewhere
+
+    assert tripleRelationsResult['spatial_triplet'][0, 1, 3] == 1 # ("stairs", "About 20 kids ", "on")
+    assert tripleRelationsResult['spatial_triplet'][0, 2, 3] == 1 # ("stairs", "About 20 kids", "on")
+    assert tripleRelationsResult['spatial_triplet'].sum() == 2 # 0 elsewhere
