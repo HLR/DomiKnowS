0068a73c301076767fb61096d7d2b444aabd30b8
Guo, Quan <guoquanscu@gmail.com>
2019-11-07 19:03:28 +0000

    WIP Add data population to get datanodes out of context, and some minor update to tests.

--- regr/graph/allennlp/base.py
+++ regr/graph/allennlp/base.py
@@ -2,8 +2,10 @@ import os
 import logging
 import pickle
 from glob import glob
-from typing import Union
+from collections import defaultdict
+from typing import Dict, List, Tuple, Union
 import torch
+import numpy as np
 import pandas as pd
 from allennlp.data.vocabulary import Vocabulary
 from allennlp.data.iterators import BucketIterator
@@ -13,6 +15,7 @@ from allennlp.training.optimizers import Optimizer
 from allennlp.training.learning_rate_schedulers import LearningRateScheduler
 from allennlp.training.util import evaluate
 from allennlp.nn.util import device_mapping
+from ..trial import Trial
 from ...utils import WrapperMetaClass
 from ...solver import ilpOntSolverFactory
 from ...solver.ilpOntSolverFactory import ilpOntSolverFactory
@@ -244,3 +247,174 @@ class AllenNlpGraph(Graph, metaclass=WrapperMetaClass):
         self.model.train(training_state)
 
         return metrics
+
+    def populate(self, context, root, *concepts, query=None, label_fn=None, data_dict=None):
+        trial = Trial()
+        if data_dict is None:
+            data_dict = defaultdict()
+
+        def peel(node_value, repeat_unpeelable=False):
+            # get a iterator for each instance in the fist level
+            # {a:[1,2,3], b:[[4.1,4.2],[5.1,5.2],[6.1,6.2]], c:{d:[7,8,9]}}
+            # ->
+            # [{a:1, b:[4.1,4.2], c{d:7}}, {a:2, b:[5.1,5.2], c{d:8}}, {a:3, b:[6.1,6.2], c{d:9}}]
+            if isinstance(node_value, (torch.Tensor, np.ndarray, List, Tuple)):
+                return iter(node_value)
+            elif isinstance(node_value, (Dict,)):
+                pvs = []
+                keys = []
+                for k, v in node_value.items():
+                    pv = peel(v, repeat_unpeelable)
+                    if pv:
+                        pvs.append(pv)
+                        keys.append(k)
+                return (dict(zip(keys, zpv)) for zpv in zip(*pvs))
+            # TODO: fields like "loss" fron allennlp might not be possible to be divided
+            # according to dim(/batch). Return None or return the identical one for all?
+            elif repeat_unpeelable:
+                return repeat(node_value)
+            return None
+
+        def contain(node, k):
+            return k.startswith(node.fullname)
+
+        def contained_by(node):
+            return lambda item: contain(node, item[0])
+
+        root_data = []
+        sub_context = {}
+        for k, v in filter(contained_by(self), context.items()):
+            k = k[len(self.fullname)+1:]
+            print('@@@',k)
+            node = self[k]
+            if not isinstance(node, Property): # skip sensors and learners
+                continue
+            if contain(root, k):
+                if not root_data:
+                    for index, _ in enumerate(peel(v)):
+                        dataNode = DataNode(instanceID=len(root_data),
+                                            instanceValue=None,
+                                            ontologyNode=root)
+                        dataNode.index = index
+                        root_data.append(dataNode)
+                prop = k
+                for dataNode, pv in zip(root_data, peel(v)):
+                    dataNode.__dict__[node.name] = pv
+            else:
+                sub_context[k] = v
+        if label_fn:
+            for dataNode in root_data:
+                prob = label_fn(dataNode)
+                if prob is not None:
+                    trial[root, dataNode] = prob
+
+        for rel in root.contains():
+            for dataNode in data_dict[concept]:
+                dataNode
+#             for rel in concept.is_a():
+#                 pass
+#             for rel in concept.has_a():
+#                 pass
+        return trial, data_dict
+
+#     @staticmethod
+#     def peel(context, index):
+#         # get a new context by selecting index for first dim
+#         def peel_one(node_value):
+#             # closure variable - index
+#             if isinstance(node_value, (torch.Tensor, List, Tuple)):
+#                 return node_value[index]
+#             elif isinstance(node_value, (Dict,)):
+#                 return {k, peel_one(v, index): for k, v: node_value.items()}
+#             # TODO: fields like "loss" fron allennlp might not be possible to be divided
+#             # according to dim(/batch). Return None or return the identical one for all?
+#             return None
+#         peeled = {}
+#         for k, v in context:
+#             v = peel_one(v)
+#             if v is not None:
+#                 peeled[k] = v
+#         return peeled
+
+#     @staticmethod
+#     def peeled(context):
+#         keys, values = zip(*context.items())
+#         zip(values)
+#         # get a new context by selecting index for first dim
+#         def peel_one(node_value):
+#             # closure variable - index
+#             if isinstance(node_value, (torch.Tensor, List, Tuple)):
+#                 return node_value[index]
+#             elif isinstance(node_value, (Dict,)):
+#                 return {k, peel_one(v, index): for k, v: node_value.items()}
+#             # TODO: fields like "loss" fron allennlp might not be possible to be divided
+#             # according to dim(/batch). Return None or return the identical one for all?
+#             return None
+#         peeled = {}
+#         for k, v in context:
+#             v = peel_one(v)
+#             if v is not None:
+#                 peeled[k] = v
+#         return peeled
+
+#     def populate(self, context, *concepts, batch=None, query=None, key='label'):
+#         from ..concept import Concept
+#         from ..property import Property
+#         from ..dataNode import DataNode
+
+#         trial = Trial()
+#         data_dict = defaultdict()
+
+#         def contained(item):
+#             k = item[0]
+#             return k.startswith(self.fullname)
+
+#         def extract_batch_value(node_value):
+#             if batch is None:
+#                 return node_value
+#             # closure variables - batch
+#             if isinstance(node_value, (torch.Tensor, List, Tuple)):
+#                 return node_value[batch]
+#             elif isinstance(node_value, (Dict,)):
+#                 return {k, extract_batch(v): for k, v: node_value.items()}
+#             return None # fields like "loss" fron allennlp might not be divided according to batch, or return the identical one for all?
+
+#         for node_key, node_value in filter(contained, context.items()):
+#             node = self[node_key]
+#             if isinstance(node, Property):
+#                 concept_node = node.sup
+#                 if concept_node not in data_dict: # TODO: limit to *concepts?
+#                     # create the nodes
+#                     # get rid of batch
+#                     node_value = extract_batch_value(node_value)
+#                     if node_value is None:
+#                         # skip if get none for batch
+#                         continue
+#                     data_dict[concept_node] = []
+#                     # TODO: estimate the number of argument
+#                     # FIXME: suppose 1 here, that means a single loop
+#                     for _ in node_value:
+#                         dataNode = DataNode(instanceID=len(data_dict[concept_node]),
+#                                             instanceValue=None,
+#                                             ontologyNode=concept_node)
+#                         data_dict[concept_node].append(dataNode)
+#                 # add a property
+#                 # FIXME: same problem as above `for _ in node_value`
+#                 for dataNode, value in zip(data_dict[concept_node], node_value):
+#                     dataNode.__dict__[node.name] = value
+#                     if node.name == key:
+#                         # TODO: how to inteprete the value here then?
+#                         # logistic output have two dim where [1] is the logit for being True
+#                         trial[concept_node, dataNode] = value[-1]
+
+#         # wire up
+#         for concept in concepts:
+#             for rel in concept.contains():
+#                 for dataNode in data_dict[concept]:
+#                     dataNode
+#             for rel in concept.is_a():
+#                 pass
+#             for rel in concept.has_a():
+#                 pass
+
+#         return trial, data_dict
