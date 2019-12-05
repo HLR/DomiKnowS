fc51b6f97927ae3056439e7159cae3eb50ce541d
hfaghihi <rajabyfa@msu.edu>
2019-12-05 16:00:38 -0500

    data loader updated and checked for ACE 2005 Data containing relationships

--- examples/new_interface/data/reader.py
+++ examples/new_interface/data/reader.py
@@ -72,14 +72,28 @@ class DataLoader:
                 sample['type'] = "ignore"
             text = sample['parse'].select_one("TEXT").prettify()
             text = re.sub('<[^<]+>', "\n", text)
-            text = text.replace('\n\n', '. ').replace('\n', ' ')
+            text = text.replace('\n\n', '. ').replace('\n', ' ').replace("Dr.", "Dr ").replace("Mr.", "Mr ").replace(
+                "Al-", "Al").replace("/", " ").replace("'s", " s").replace(".-", "-").replace("-", " ").replace("(",
+                                                                                                                " ").replace(
+                ")", " ").replace("%", ' percent ')
+            x = re.findall(r'[a-z]\.[A-Z]', text)
+            for item in x:
+                item = item.replace(".", "\.")
+                y = item.replace("\.", " . ")
+                text = re.sub(item, y, text)
+            #             x = re.findall(r'[a-z]-[a-zA-Z]', text)
+            #             for item in x:
+            #                 y = item.replace("-", " ")
+            #                 text = re.sub(item, y, text)
             text = re.sub(' +', ' ', text)
             sample['parse'] = text
-            splitted = text.split(".")
-            splitted = [re.sub(' +', ' ', re.sub(r'[^\w\s]', ' ', data)).replace(" t ", "'t ") for data in splitted if
-                        len(data) > 2]
-            sentences = [Sentence(sent) for sent in splitted]
-            sentences = [sentence for sentence in sentences if len(sentence) >= 3]
+            splitted = sentence_split(text)
+            #             splitted = text.split(".")
+            #             splitted = [re.sub(' +', ' ', re.sub(r'[^\w\s]',' ',data)).replace(" t ", "'t ") for data in splitted if len(data) > 2]
+            splitted = [re.sub(' +', ' ', data).replace("'s", "' s").replace("' t ", "'t ") for data in splitted if
+                        len(data) >= 1]
+            sentences = [Sentence(re.sub(r'[^\w\s]', ' ', sent)) for sent in splitted if sent]
+            sentences = [sentence for sentence in sentences if len(sentence) >= 1]
             sample['sentences'] = sentences
             self.inputs.append(sample)
             file1.close()
@@ -98,6 +112,14 @@ class DataLoader:
             iteration += 1
             print('Prepared {} Lables'.format(iteration), end='\r')
 
+    def Sort(self, sub_li):
+
+        # reverse = None (Sorts in Ascending order)
+        # key is set to sort using second element of
+        # sublist lambda has been used
+        sub_li.sort(key=lambda x: x[3])
+        return sub_li
+
     def readLables(self):
         print("starting to read lables", end='\n')
         iteration = 0
@@ -110,32 +132,30 @@ class DataLoader:
             for entity in entities:
                 mentions = entity.findall(".//entity_mention")
                 _all.extend([[mention.find("./head/charseq").text, mention.find("./head/charseq").attrib['START'],
-                              entity.attrib] for mention in mentions])
+                              entity.attrib, mention.attrib['ID'], mention.find("./head/charseq").attrib['END'],
+                              mention.find("./extent/charseq").text] for mention in mentions])
             _all = sorted(_all, key=lambda x: int(x[1]))
-            sample['annotated'] = [(x[0],
-                                    x[2]['TYPE'],
-                                    x[2]['SUBTYPE'],
-                                    x[1],
-                                    x[2]['ID']
-                                    )
-                                   for x in _all]
+            entities = [[x[0], x[2]['TYPE'], x[2]['SUBTYPE'], int(x[1]), x[3], int(x[4]), x[5]] for x in _all]
+            sample['annotated'] = self.Sort(entities)
             relations = root.findall(".//relation")
             _all = []
-            _all.extend([[relation.findall("./relation_argument")[0].attrib['REFID'],
-                          relation.findall("./relation_argument")[1].attrib['REFID'], relation.attrib] for relation in
-                         relations])
+            for relation in relations:
+                mentions = relation.findall(".//relation_mention")
+                _all.extend([[mention.findall("./relation_mention_argument")[0].attrib['REFID'],
+                              mention.findall("./relation_mention_argument")[1].attrib['REFID'], relation.attrib,
+                              mention.attrib["ID"]] for mention in mentions])
             sample['relations'] = [(x[0],
                                     x[1],
                                     x[2]['TYPE'],
                                     x[2]['SUBTYPE'],
-                                    x[2]['ID']
+                                    x[3]
                                     )
                                    for x in _all if x[2]['TYPE'] != "METONYMY"]
             sample['relations'].extend([(x[0],
                                          x[1],
                                          x[2]['TYPE'],
                                          "UNKNOWN",
-                                         x[2]['ID']
+                                         x[3]
                                          )
                                         for x in _all if x[2]['TYPE'] == "METONYMY"])
             #             sample['relations'] = _all
@@ -143,11 +163,54 @@ class DataLoader:
             total_ann += len(sample['annotated'])
             print('Read {} inputs with total {} annotations'.format(iteration, total_ann), end='\r')
 
+    def match(self, s1, s2):
+        if s1 == s2:
+            return True
+        elif s1 in s2.split("-"):
+            return True
+        else:
+            if s1 == "u.s":
+                s1 = "u.s."
+            if s1 == "u.k":
+                s1 = "u.k."
+            if s1 == "u.n":
+                s1 = "u.n."
+            if s1 == "dr":
+                s1 = "dr."
+            if s1 == "mr":
+                s1 = "mr."
+            if s1 == "l":
+                s1 == "led"
+            if s1 == s2:
+                return True
+            elif s1 in s2.split("-"):
+                return True
+            if s2.count(s1):
+                if len(s2) >= 3 and (len(s2) <= len(s1) + 1 or len(s1) / len(s2) >= 0.65):
+                    return True
+                if len(s1) >= 5 and len(s2) >= 8:
+                    return True
+                if len(s1) <= 4 and s1 == s2[0:len(s1)] or s1 == s2[len(s2) - len(s1):]:
+                    return True
+            #                 if s1 == "asia" and s2 == "asiaagent":
+            #                     return True
+
+            s1 = re.sub(r'[^\w\s]', '', s1)
+            s2 = re.sub(r'[^\w\s]', '', s2)
+            if s1 == s2:
+                return True
+
+        return False
+
     def lableInputs(self):
         print("starting to lable inputs", end='\n')
         print(" ")
         iteration = 0
         for sample in self.inputs:
+            if sample['type'] == "ignore":
+                continue
+            total_phrases = 0
+            passed = 0
             sample['inputs'] = []
             sample['phrases'] = []
             # words array cotains arrays of all sentences inside a file
@@ -161,46 +224,68 @@ class DataLoader:
                 phrases = []
                 start = 0
                 for iteration in range(len(sample['annotated'])):
-                    #                     print(iteration)
                     if iteration < checked:
-                        #                         print("passed")
                         continue
                     annotation = sample['annotated'][iteration]
-                    #                     print(annotation)
-                    #                     print(sentence[start:])
+                    if iteration >= 1:
+                        if annotation[3] <= sample['annotated'][iteration - 1][5]:
+                            passed += 1
+                            checked += 1
+                            continue
+                    #                             start = phrases[-1]['before']
                     _list = []
-                    labels = Sentence(annotation[0]).to_tokenized_string().split(" ")
-                    #                     print(labels)
-                    #                     print("Annotation is ")
-                    #                     print(annotation)
-                    #                     print("label are")
-                    #                     print(labels)
-                    # for _it in range(start, len(sentence)):
-                    if labels[0] in sentence[start:]:
-                        value_index = sentence[start:].index(labels[0])
-                        if len(labels) > len(sentence[value_index:]):
-                            #                             print("b1")
+                    text = annotation[0]
+                    x = re.findall(r'[a-z]\.[A-Z]', text)
+                    for item in x:
+                        item = item.replace(".", "\.")
+                        y = item.replace("\.", " . ")
+                        text = re.sub(item, y, text)
+                    #                     x = re.findall(r'[a-z]-[A-Z]', text)
+                    #                     for item in x:
+                    #                         y = item.replace("-", " ")
+                    #                         text = re.sub(item, y, text)
+                    text = text.replace("Al-", "Al").replace("Dr.", "Dr ").replace("Mr.", "Mr ").replace("/",
+                                                                                                         " ").replace(
+                        "'s", " s").replace(".-", "-").replace("-", " ").replace("%", ' percent ')
+                    text = re.sub(' +', ' ', text)
+                    try:
+                        labels = sentence_split(re.sub(r'[^\w\s]', ' ', text))[0]
+                    except:
+                        print("file name is ", sample['annotation_path'])
+                        print("text is ", text)
+                        print("iteration is ", iteration)
+                        raise
+                    labels = Sentence(labels).to_tokenized_string().replace("\n", " ").split(" ")
+                    #                     if labels[0] == "u.s":
+                    #                         labels[0] = "u.s."
+                    #                     if labels[0] == "u.k":
+                    #                         labels[0] = "u.k."
+                    #                     if labels[0] == "u.n":
+                    #                         labels[0] = "u.n."
+                    exist_check = False
+                    for _itt in range(start, len(sentence)):
+                        if self.match(labels[0], sentence[_itt]):
+                            exist_check = True
+                            value_index = _itt - start
+                            if len(labels) > len(sentence[_itt:]):
+                                exist_check = False
+                            else:
+                                for _it in range(len(labels)):
+                                    if self.match(labels[_it], sentence[start + value_index + _it]):
+                                        _list.append(sentence[start + value_index + _it])
+                                    else:
+                                        break
+                                if len(_list) != len(labels):
+                                    _list = []
+                                    exist_check = False
+                        if exist_check:
                             break
-                    else:
-                        #                         print("b2")
+                    if not exist_check:
                         break
 
-                    for _it in range(len(labels)):
-                        if labels[_it] == sentence[start + value_index + _it]:
-                            _list.append(sentence[start + value_index + _it])
-                        else:
-                            #                             print(value_index)
-                            #                             print(sentence[start+value_index+_it])
-                            #                             print(_it)
-                            #                             print(labels[_it])
-                            #                             print("b3")
-                            break
-                    if len(_list) != len(labels):
-                        #                         print("b4")
-                        break
                     check = True
                     example = {"text": annotation[0], "label": annotation[1], 'list': _list, 'value_index': value_index,
-                               'before': start, 'id': annotation[4], 'start': annotation[3]}
+                               'before': start, 'id': annotation[4], 'start': annotation[3], 'end': annotation[5]}
 
                     for _it in range(start, start + value_index):
                         _input = {"word": sentence[_it], 'w-type': '-O-', 'type': '-O-', 'subtype': '-O-',
@@ -251,24 +336,30 @@ class DataLoader:
                     annotated_sentence.append(_input)
                 # annotated_sentence is an array of annotated words inside a sentence
                 relations = []
+                all_relations = []
+                phrases_ids = [phrase['id'] for phrase in phrases]
                 for phrase in phrases:
                     for rel in sample['relations']:
-                        if rel[0] == phrase['id']:
+                        if rel[0] == phrase['id'] and rel[1] in phrases_ids:
                             relations.append(rel)
+                        elif rel[0] == phrase['id'] or rel[1] == phrase['id']:
+                            all_relations.append(rel)
 
                 sample['inputs'].append(annotated_sentence)
                 sample['phrases'].append(phrases)
+                total_phrases += len(phrases)
                 if check:
                     self.data.append(
                         {"words": annotated_sentence, "sentence": sentence1.to_tokenized_string(), "phrases": phrases,
-                         'relations': relations})
+                         'relations': relations, 'all_relations': all_relations, 'file': sample['annotation_path']})
             iteration += 1
+            if (total_phrases + passed != len(sample['annotated'])):
+                print(sample['annotation_path'])
             print('Labled {} inputs with total number of sentences with entities of {} sentences'.format(iteration, len(
                 self.data)), end='\r')
         self.data = [item for item in self.data if len(item["words"]) != 0]
 
-    def index_finder(self, _class):
-        choices = ["ORG", "FAC", "PER", "VEH", "LOC", "WEA", "GPE", "-O-"]
+    def index_finder(self, _class, choices=["ORG", "FAC", "PER", "VEH", "LOC", "WEA", "GPE", "-O-"]):
         return choices.index(_class)
 
     def posTagFinder(self):
@@ -288,7 +379,9 @@ class DataLoader:
         self.prepareLables()
         self.readLables()
         self.lableInputs()
-        self.posTagFinder()
+
+
+#         self.posTagFinder()
 
 
 class SimpleReader:
