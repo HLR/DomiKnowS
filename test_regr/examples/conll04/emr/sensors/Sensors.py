53c755d7b4baef86817d2b718ace571802c06045
Guo, Quan <1118270+guoquan@users.noreply.github.com>
2020-03-30 22:42:21 -0400

    Add test sensor that look for specific input and generate specific output

--- test_regr/examples/conll04/emr/sensors/Sensors.py
+++ test_regr/examples/conll04/emr/sensors/Sensors.py
@@ -3,12 +3,30 @@ from typing import Any
 import torch
 
 
+class TestSensor(TorchSensor):
+    def __init__(self, *pres, output=None, edges=None, label=False, expected_inputs=None, expected_outputs=None, **kwargs):
+        super().__init__(*pres, output=output, edges=edges, label=label)
+        self._expected_inputs = expected_inputs
+        self._expected_outputs = expected_outputs
+
+    @property
+    def expected_inputs(self):
+        return self._expected_inputs
+
+    @property
+    def expected_outputs(self):
+        return self._expected_outputs
+
+    def forward(self,) -> Any:
+        assert self.inputs == self.expected_inputs
+        return self.expected_outputs
+
+
 class DummyEdgeStoW(TorchEdgeSensor):
     def forward(self,) -> Any:
-        return ["word1", "word2", "word3", "word4"]
+        return ["John", "works", "for", "IBM"]
 
 
-class DummyWordEmb(TorchSensor):
-    def forward(self,) -> Any:
-        dummy = torch.randn(len(self.inputs[0]), 2048)
-        return dummy
+class DummyWordEmb(TestSensor): pass
+
+class DummyFullyConnectedLearner(TestSensor): pass
--- test_regr/examples/conll04/test_main.py
+++ test_regr/examples/conll04/test_main.py
@@ -3,6 +3,20 @@ sys.path.append(".")
 sys.path.append("../..")
 
 import pytest
+import torch
+
+from emr.utils import Namespace
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+test_case = {
+    'word': {
+        'raw': ["John", "works", "for", "IBM"],
+        'emb': torch.randn(1, 4, 2048, device=device),
+        'people': torch.tensor([[0.9, 0.4, 0.1, 0.6]], device=device)
+    }
+}
+test_case = Namespace(test_case)
 
 
 def model_declaration(config):
@@ -10,10 +24,11 @@ def model_declaration(config):
     #from emr.sensor.sensor import DataSensor, LabelSensor, CartesianSensor
     #from emr.sensor.learner import EmbedderLearner, RNNLearner, MLPLearner, LRLearner
     from regr.sensor.pytorch.sensors import ReaderSensor
+    from regr.sensor.pytorch.learners import FullyConnectedLearner
 
     from graph import graph, sentence, word, people, organization, location, other, o
     from graph import rel_sentence_contains_word
-    from emr.sensors.Sensors import DummyEdgeStoW, DummyWordEmb
+    from emr.sensors.Sensors import DummyEdgeStoW, DummyWordEmb, DummyFullyConnectedLearner
 
     graph.detach()
 
@@ -21,9 +36,10 @@ def model_declaration(config):
     sentence['raw'] = ReaderSensor(keyword='token', label=True)  # sentence checkpoint
 
     rel_sentence_contains_word['forward'] = DummyEdgeStoW("raw", mode="forward", keyword="raw")
-    word['emb'] = DummyWordEmb('raw', edges=[rel_sentence_contains_word['forward']])
-    word['emb'] = ReaderSensor(keyword='O', label=True)  # word checkpoint
-
+    word['emb'] = DummyWordEmb('raw', edges=[rel_sentence_contains_word['forward']],
+                               expected_inputs=[test_case.word.raw,],
+                               expected_outputs=test_case.word.emb
+                              )
 
     # sentence['emb'] = SentenceWord2VecSensor('raw')
     # rel_sentence_contains_word['forward'] = SentenceToWordEmb('emb', mode="forward", keyword="emb")
@@ -31,13 +47,15 @@ def model_declaration(config):
     # word['encode'] = LSTMLearner('features', input_dim=5236, hidden_dim=240, num_layers=1, bidirectional=True)
     # word['feature'] = MLPLearner(word['ctx_emb'], in_features=200, out_features=200)
 
-    # word[people] = LabelSensor('Peop')
+    word[people] = ReaderSensor(keyword='Peop', label=True)
     # word[organization] = LabelSensor('Org')
     # word[location] = LabelSensor('Loc')
     # word[other] = LabelSensor('Other')
     # word[o] = LabelSensor('O')
 
-    # word[people] = LRLearner(word['feature'], in_features=200)
+    word[people] = DummyFullyConnectedLearner('emb', input_dim=2048, output_dim=2,
+                                         expected_inputs=[test_case.word.emb,],
+                                         expected_outputs=test_case.word.people)
     # word[organization] = LRLearner(word['feature'], in_features=200)
     # word[location] = LRLearner(word['feature'], in_features=200)
     # word[other] = LRLearner(word['feature'], in_features=200)
@@ -51,7 +69,6 @@ def model_declaration(config):
 @pytest.mark.gurobi
 def test_main_conll04():
     from config import Config as config
-    from emr.utils import seed
     from emr.data import ConllDataLoader
 
     training_set = ConllDataLoader(config.Data.train_path,
