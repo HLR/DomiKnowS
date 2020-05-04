2e9e992c432ac332ac7210f089cba2c79a6ed825
Guo, Quan <1118270+guoquan@users.noreply.github.com>
2020-05-03 21:49:18 -0400

    Add model selection to eval

--- examples/SpRL/sprlEval.py
+++ examples/SpRL/sprlEval.py
@@ -8,6 +8,8 @@ from utils import seed
 parser = ArgumentParser(description='Test a trained model.')
 parser.add_argument('--model-path', '-m', type=str,
                     help='Path to the model serialization_dir.')
+parser.add_argument('--model', '-M', type=str, default='last',
+                    help='Model id, last, or best.')
 parser.add_argument('--data-path', '-d', type=str,
                     help='Path to the data.')
 parser.add_argument('--log-solver', '-s', type=str, default='solver.log',
@@ -17,7 +19,7 @@ parser.add_argument('--batch-size', '-b', type=int, default=1,
 
 args = parser.parse_args()
 
-def test(model_path, data_path, log_solver, batch_size):
+def test(model_path, model, data_path, log_solver, batch_size):
     #with open(os.path.join(model_path, 'config.pkl'), 'rb') as fin:
     #    Config = pickle.load(fin)
     from config import Config
@@ -28,13 +30,13 @@ def test(model_path, data_path, log_solver, batch_size):
     lbp = model_declaration(graph, Config.Model)
 
     seed()
-    lbp.load(model_path, 'last')
+    lbp.load(model_path, model)
     metrics = lbp.test(data_path, log_solver, batch_size)
     print(metrics)
 
 
 def main():
-    return test(args.model_path, args.data_path, args.log_solver, args.batch_size)
+    return test(args.model_path, args.model, args.data_path, args.log_solver, args.batch_size)
 
 
 if __name__ == '__main__':
