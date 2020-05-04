e5ea25e114fb749b03340993b075e8ab6a62713f
Guo, Quan <1118270+guoquan@users.noreply.github.com>
2020-05-04 18:46:03 -0400

    Update fix problemetic config

--- examples/SpRL/config.py
+++ examples/SpRL/config.py
@@ -9,12 +9,12 @@ config = {
         'relative_path': "data",
         # 'train_path': "new_train.pkl",
         # 'valid_path': "new_gold.pkl",
-        'train_path': "new_train.xml",
-        'valid_path': "new_gold.xml",
+        # 'train_path': "new_train.xml",
+        # 'valid_path': "new_gold.xml",
         # 'train_path': "sprl2017_train.xml.pkl",
         # 'valid_path': "sprl2017_gold.xml.pkl",
-        'train_path': "sprl2017_gold.xml",
-        'valid_path': "sprl2017_train.xml"
+        'train_path': "sprl2017_train.xml",
+        'valid_path': "sprl2017_gold.xml"
     },
     'Model': {  # model setting
         'embedding_dim': 16,
