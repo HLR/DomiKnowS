e7acc181ae3309978f902069b8d791e20c233824
Andrzej Uszok <auszok@ihmc.us>
2020-05-17 17:41:13 -0500

    Added logical constrain to graph

--- test_regr/examples/graph_coloring/graph.py
+++ test_regr/examples/graph_coloring/graph.py
@@ -2,7 +2,7 @@ from itertools import permutations
 
 from regr.graph import Graph, Concept, Relation
 from regr.graph.relation import disjoint
-from regr.graph.logicalConstrain import ifL, andL, nandL
+from regr.graph.logicalConstrain import orL, andL, existsL
 
 
 Graph.clear()
@@ -14,8 +14,13 @@ with Graph('global') as graph:
         world = Concept(name='world')
         city = Concept(name='city')
         (world_contains_city,) = world.contains(city)
+        firestationCity = Concept(name='firestationCity')
+        (city_contains_firestationCity,) = city.contains(firestationCity)
+
         neighbor = Concept(name='neighbor')
         (neighbor_city1, neighbor_city2) = neighbor.has_a(arg1=city, arg2=city)
 
-        # Add the constraints here
+        # Constraints
+        existsL(orL(firestationCity, (x,), andL(neighbor, (x, y), firestationCity, (y,))))
+        
         # Remember to add that if city a is neighbor of city b then the reverse also holds and should contain the same properties on the edge
