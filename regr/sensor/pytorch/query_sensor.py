a3ff3aa38ce09a293cf5455c31a2bd32d293bea1
Guo, Quan <1118270+guoquan@users.noreply.github.com>
2020-06-07 18:15:36 -0400

    Add  functional and query sensor

--- regr/sensor/pytorch/query_sensor.py
+++ regr/sensor/pytorch/query_sensor.py
@@ -1,9 +1,10 @@
 from typing import Dict, Any
 
-from ...graph import DataNode
+from ...graph import DataNode, DataNodeBuilder
 from .sensors import TorchSensor, Sensor
 
-class QuerySensor(TorchSensor):
+
+class QuerySensor0(TorchSensor):
     def __init__(self, *pres, output=None, edges=None, label=False, query=None):
         super().__init__(*pres, output=output, edges=edges, label=label)
         if callable(query):
@@ -72,3 +73,55 @@ class QuerySensor(TorchSensor):
         for pre in self.pres:
             for _, sensor in self.sup.sup[pre].find(Sensor):
                 sensor(context=context)
+
+
+class FunctionalSensor(TorchSensor):
+    def __init__(self, *pres, output=None, edges=None, label=False, func=None):
+        super().__init__(*pres, output=output, edges=edges, label=label)
+        self.func_ = func
+
+    def forward(self):
+        if self.func_ is not None:
+            return self.func_(*self.inputs)
+        return self.func(*self.inputs)
+
+    def func(self, *inputs):
+        raise NotImplementedError
+
+class QuerySensor(FunctionalSensor):
+    def update_context(
+        self,
+        context: Dict[str, Any],
+        force=False
+    ) -> Dict[str, Any]:
+        if not force and self.fullname in context:
+            # context cached results by sensor name. override if forced recalc is needed
+            val = context[self.fullname]
+        else:
+            self.define_inputs()
+            val = self.forward()
+            
+        if val is not None:
+            context[self.fullname] = val
+            if not self.label:
+                context[self.sup.fullname] = val  # override state under property name
+        else:
+            context[self.fullname] = None
+            if not self.label:
+                context[self.sup.fullname] = None
+            
+        if self.output:
+            context[self.fullname] = self.fetch_value(self.output)
+            context[self.sup.fullname] = self.fetch_value(self.output)
+            
+        return context
+
+    def define_inputs(self):
+        builder = self.context_helper
+        if not isinstance(builder, DataNodeBuilder):
+            raise TypeError('{} should work with DataNodeBuilder context.'.format(type(self)))
+        root = builder.getDataNode()
+        concept = self.sup.sup
+        datanodes = root.findDatanodes(select=concept)
+
+        self.inputs = [datanodes]
