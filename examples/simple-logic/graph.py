6fb436b318c7de352f358662db90842157f04628
Quan Guo <1118270+guoquan@users.noreply.github.com>
2020-07-23 14:19:54 -0400

    Add name to graph for clarity

--- examples/simple-logic/graph.py
+++ examples/simple-logic/graph.py
@@ -5,10 +5,10 @@ Graph.clear()
 Concept.clear()
 Relation.clear()
 
-with Graph('global') as graph:
+with Graph(name='global') as graph:
     world = Concept(name='world')
-    x = Concept('x')
+    x = Concept(name='x')
     (world_contains_x,) = world.contains(x)
-    y0 = x('y0')
-    y1 = x('y1')
+    y0 = x(name='y0')
+    y1 = x(name='y1')
     orL(andL(y0, notL(y1)), andL(notL(y0), y1))
