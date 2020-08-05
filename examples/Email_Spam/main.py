90128557c26442db60a3aba848d7d8e83a8a9c29
hfaghihi <rajabyfa@msu.edu>
2020-08-05 15:29:17 -0400

    change in ModuleLearner inside the EmailSpam Example

--- examples/Email_Spam/main.py
+++ examples/Email_Spam/main.py
@@ -7,7 +7,7 @@ sys.path.append('../..')
 
 def model_declaration():
     from regr.sensor.pytorch.sensors import ReaderSensor, TorchEdgeReaderSensor, ForwardEdgeSensor, ConstantSensor, ConcatSensor
-    from regr.sensor.torch.learner import ModuleLearner
+    from regr.sensor.pytorch.learners import ModuleLearner
     from regr.sensor.pytorch.query_sensor import CandidateReaderSensor
     from regr.program import LearningBasedProgram
     from regr.program.model.pytorch import PoiModel
