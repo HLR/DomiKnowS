0d50eab1a872e73732325d23d5e5f1717d844eb6
kordjamshidi <kordjam@illinois.edu>
2020-08-07 15:15:14 -0400

    renamed the tweet reader

--- examples/SentimentAnalysis/tweet_reader.py
+++ examples/SentimentAnalysis/tweet_reader.py
@@ -2,28 +2,28 @@ from regr.data.reader import RegrReader
 
 
 class SentimentReader(RegrReader):
+
     def gettweetval(self, item):
         return item['tweet']
 
     def getNegativeLabelval(self, item):
+
         if item['Label'] == "0":
             return True
         else:
             return False
 
     def getPositiveLabelval(self, item):
+
         if item['Label'] == "4":
             return True
         else:
             return False
 
-    # def getOptimisticLabelval(self, item):
-    #     return item['Optimistic']
-    #
     def parse_file(self):
+
         object_list = []
         item = {}
-
         with open(self.file, encoding="ISO-8859-1") as f:
             content = f.readlines()
         for l in content:
