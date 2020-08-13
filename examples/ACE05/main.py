9719499874805d13ebfa25eeb9377e8f3dc7b1eb
Guo, Quan <1118270+guoquan@users.noreply.github.com>
2020-08-13 01:04:20 -0400

    Update add simple relation check

--- examples/ACE05/main.py
+++ examples/ACE05/main.py
@@ -5,6 +5,17 @@ import config
 def main():
     reader = Reader(config.path)
     for data_item in reader():
+        text = data_item['text']
+        refs = data_item['referables']
+        rels = data_item['relations']
+        events = data_item['events']
+        # validate relations
+        for rel_id, rel in rels.items():
+            # relation has two arguments
+            assert rel.arguments[0] is not None and rel.arguments[1] is not None
+            # if there is rel.subtype, then rel.subtype is a rel.type
+            assert not rel.subtype or rel.type in set(map(lambda r: r.dst, rel.subtype.is_a()))
+            # 
         # print(data_item)
         pass
     print('done')
