e13249d7db9529a423eef840e39d4b807d50a103
hfaghihi <rajabyfa@msu.edu>
2020-08-21 11:39:57 -0400

    Adding general tokenizer and tokenizer Span Sensors

---
+++ examples/ACE05/sensors/tokenizers.py
@@ -0,0 +1,33 @@
+from regr.sensor.pytorch.sensors import TorchSensor, TorchEdgeSensor
+from typing import Any
+
+
+class Tokenizer(TorchEdgeSensor):
+    def __init__(self, *pres, to, mode="forward", edges=None, label=False, device='auto', tokenizer=None):
+        super().__init__(*pres, to, mode, edges, label, device)
+        if not tokenizer:
+            raise ValueError('You should select a default Tokenizer')
+        self.tokenizer = tokenizer
+
+    def forward(self, ) -> Any:
+        return self.tokenizer(self.inputs[0])['input_ids']
+
+
+class TokenizerSpan(TorchSensor):
+    def __init__(self, *pres, edges=None, label=False, device='auto', tokenizer=None):
+        super().__init__(*pres, edges, label, device)
+        if not tokenizer:
+            raise ValueError('You should select a default Tokenizer')
+        self.tokenizer = tokenizer
+
+    def forward(self, ) -> Any:
+        ids_index = []
+        start = 0
+        for item in self.tokenizer.convert_ids_to_tokens(self.inputs[0], skip_special_tokens=True):
+            if item[0] == "Ġ":
+                ids_index.append((start + 1, start + len(item) - 1))
+            else:
+                ids_index.append((start, start + len(item) - 1))
+            start += len(item)
+
+        return ids_index
