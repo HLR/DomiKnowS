5b757d88b016c72887a9b33a7d12d4b77867fcdc
hfaghihi <rajabyfa@msu.edu>
2020-08-24 10:12:10 -0400

    span changed to phrase

--- examples/ACE05/sensors/candidate.py
+++ examples/ACE05/sensors/candidate.py
@@ -1,8 +1,8 @@
 from regr.sensor.pytorch.query_sensor import CandidateSensor
 
 
-def makeSpanPairs(current_spans, span1, span2):
-    if span1.getAttribute('index') is not span2.getAttribute('index'):
+def makeSpanPairs(current_spans, phrase1, phrase2):
+    if phrase1.getAttribute('index') is not phrase2.getAttribute('index'):
         return True
     else:
         return False
