653a15e45cbdfe503103d10eb4c02f588fde406d
Guo, Quan <1118270+guoquan@users.noreply.github.com>
2020-08-25 13:31:15 -0400

    Add bert embedding of word

--- examples/ACE05/main.py
+++ examples/ACE05/main.py
@@ -9,7 +9,7 @@ def main():
     program = model(graph)
     #traint_reader = Reader(config.path, list_path=config.list_path, type='train', status=config.status)
     traint_reader = [{}]  # dummy data
-    for node in program.populate(traint_reader):
+    for node in program.populate(traint_reader, device='auto'):
         print(node)
 
 if __name__ == "__main__":
