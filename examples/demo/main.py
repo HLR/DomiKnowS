57ac69cc762807ea4568c2028e4bbfedf30ea2b3
Quan Guo <1118270+guoquan@users.noreply.github.com>
2020-09-24 11:27:17 -0400

    Add a new demo example

---
+++ examples/demo/main.py
@@ -0,0 +1,119 @@
+import logging
+
+from regr.graph import Graph, Concept, Relation
+from regr.graph import ifL, notL, andL, orL, nandL
+from regr.program import POIProgram
+from regr.sensor.pytorch.learners import ModuleLearner
+from regr.sensor.pytorch.sensors import ReaderSensor, TorchEdgeSensor
+from regr.program.metric import MacroAverageTracker, PRF1Tracker
+from regr.program.loss import NBCrossEntropyLoss
+
+from models import WordEmbedding, Classifier
+
+
+Graph.clear()
+Concept.clear()
+Relation.clear()
+
+
+#
+# graph
+#
+with Graph('example') as graph:
+    sentence = Concept(name='sentence')
+    word = Concept(name='word')
+    sentence.contains(word)
+    people = word(name='people')
+    organization = word(name='organization')
+    nandL(people, organization)
+    # pair = Concept(name='pair')
+    # pair.has_a(arg1=word, arg2=word)
+    # work_for = pair(name='work_for')
+    # ifL(work_for, ('x', 'y'), andL(people, 'x', organization, 'y'))
+
+#
+# Reader as a list
+#
+reader = [{
+    'text': 'John works for IBM',
+    'peop': [1, 0, 0, 0],
+    'org': [0, 0, 0, 1],
+    'wf': [(0,3)]
+    },]
+
+#
+# Reader as minimal required interface
+#
+SAMPLE = {
+    'text': 'John works for IBM',
+    'peop': [1, 0, 0, 0],
+    'org': [0, 0, 0, 1],
+    'wf': [(0,3)]
+}
+class Reader():
+    def __iter__(self):
+        yield SAMPLE
+
+    def __len__(self):  # optional
+        return 1
+
+reader = Reader()
+
+#
+# Sesnor / Learner
+#
+sentence['index'] = ReaderSensor(keyword='text')
+
+scw = sentence.relate_to(word)[0]
+def tokenize(text):
+    return text.split(' ')
+scw['forward'] = TorchEdgeSensor('index', to='index', forward=tokenize)
+# word['index'] = ContainEdgeSensor('index', forward=tokenize)
+
+word[people] = ReaderSensor(keyword='peop', label=True)
+word[organization] = ReaderSensor(keyword='org', label=True)
+
+
+word['emb'] = ModuleLearner('index', module=WordEmbedding())
+word[people] = ModuleLearner('emb', module=torch.nn.Linear(100, 2))
+word[organization] = ModuleLearner('emb', module=torch.nn.Linear(100, 2))
+
+#
+# Program
+#
+program = POIProgram(graph, loss=MacroAverageTracker(NBCrossEntropyLoss()), metric=PRF1Tracker())
+
+# device options are 'cpu', 'cuda', 'cuda:x', torch.device instance, 'auto', None
+device = 'auto'
+
+logging.basicConfig(level=logging.INFO)
+
+#
+# train
+#
+program.train(reader, train_epoch_num=100, Optim=torch.optim.Adam, device=device)
+print('Training result:')
+print(program.model.loss)
+print(program.model.metric)
+
+print('-'*40)
+
+#
+# test
+#
+program.test(reader, device=device)
+print('Testing result:')
+print(program.model.loss)
+print(program.model.metric)
+
+print('-'*40)
+
+#
+# datanode
+#
+for node in program.populate(reader, device=device):
+    node.inferILPConstrains(fun=lambda val: torch.tensor(val).softmax(dim=-1).detach().cpu().numpy().tolist(), epsilon=None)
+    for word_node in node.getChildDataNodes():
+        print(word_node.getAttribute('index'))
+        print(' - people:', word_node.getAttribute(people), 'ILP:', word_node.getAttribute(people, 'ILP'))
+        print(' - organization:', word_node.getAttribute(organization), 'ILP:', word_node.getAttribute(organization, 'ILP'))
---
+++ examples/demo/models.py
@@ -0,0 +1,22 @@
+import torch
+
+
+class WordEmbedding(torch.nn.Module):
+    i2w = ['__UNK__', 'John', 'works', 'for', 'IBM']
+    w2i = dict((w,i) for i, w in enumerate(i2w))
+
+    def __init__(self):
+        super().__init__()
+        self.embeds = torch.nn.Embedding(len(self.i2w), 100)
+
+    def ws2i(self, words):
+        ids = [self.w2i.get(word, 0) for word in words]
+        device = next(self.embeds.parameters()).device
+        return torch.tensor(ids, device=device)
+
+    def forward(self, words):
+        ids = self.ws2i(words)
+        return self.embeds(ids)
+
+class Classifier(torch.nn.Linear):
+    pass
