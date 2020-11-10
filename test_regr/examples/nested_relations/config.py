d4fe4034397bd1690b33df96069d4f4280cff2b6
hfaghihi <rajabyfa@msu.edu>
2020-11-10 11:44:08 -0500

    updaring the config in nested relations

--- test_regr/examples/nested_relations/config.py
+++ test_regr/examples/nested_relations/config.py
@@ -4,13 +4,13 @@ from regr.utils import Namespace, caller_source
 from regr.program.model.pytorch import PoiModel
 
 
-from .graph import sentence, word, phrase, pair
+from .graph import sentence, word, phrase, pair, work_for, located_in, live_in, orgbase_on, kill, Oword, Bword, Iword, Eword, people, organization, other, location
 
 
 config = {
     'Model': {
         'Model': PoiModel,
-        'poi': (sentence, word, phrase, pair),
+        'poi': (sentence, word, phrase, pair, work_for, located_in, live_in, orgbase_on, kill, Oword, Bword, Iword, Eword, location, other, people, organization),
         'loss': None,
         'metric': None,
     },
