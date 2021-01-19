bf3e6e959058519e8b3ba0a66dd08285df626cda
Guo, Quan <1118270+guoquan@users.noreply.github.com>
2021-01-19 23:04:53 +0800

    Update fix email spam example, issue remains with single string value

--- examples/Email_Spam/main.py
+++ examples/Email_Spam/main.py
@@ -7,9 +7,8 @@ sys.path.append('../..')
 
 
 def model_declaration():
-    from regr.sensor.pytorch.sensors import ReaderSensor, ForwardEdgeSensor, ConstantSensor, ConcatSensor
+    from regr.sensor.pytorch.sensors import ReaderSensor, ConcatSensor
     from regr.sensor.pytorch.learners import ModuleLearner
-    from regr.sensor.pytorch.relation_sensors import CandidateReaderSensor
     from regr.program import LearningBasedProgram
     from regr.program.model.pytorch import PoiModel
     import torch
