e734d813318c45c5615979b04607a961b029a790
Andrzej Uszok <auszok@ihmc.us>
2021-01-22 08:53:24 -0600

    New format for constrains

--- test_regr/examples/graph_coloring/graph2.py
+++ test_regr/examples/graph_coloring/graph2.py
@@ -16,16 +16,18 @@ with Graph('global') as graph2:
         firestationCity = city(name='firestationCity')
         
         # No less then 1 firestationCity
-        atLeastL(1, ('x', ), firestationCity, ('x', ), p=90)
+        atLeastL(firestationCity, V(name='x'), 'x', 1, p=90)
         
         # At most 1 firestationCity
-        atMostL(1, ('x', ), firestationCity, ('x', ), p=80)
+        atMostL(firestationCity, V(name='x'), 'x', 1, p=80)
         
         # Exactly 2 firestationCity
-        exactL(2, ('x', ), firestationCity, ('x', ), p=55)
+        exactL(firestationCity, V(name='x'), 'x', 2, p=55)
         
         # Constraints - For each city x either it is a firestationCity or exists a city y which is in cityLink relation with neighbor attribute equal 1 to city x and y is a firestationCity
-        orL(firestationCity, ('x',), existsL(('y',), andL(eqL(cityLink, 'neighbor', {1}), ('x', 'y'), firestationCity, ('y',))))
+        #orL(firestationCity, ('x',), existsL(('y',), andL(eqL(cityLink, 'neighbor', {1}), ('x', 'y'), firestationCity, ('y',))))
 
-        # Each city has no more then 4 neighbors
-        atMostL(4, ('y', ), andL(firestationCity, ('x',), eqL(cityLink, 'neighbor', 1), ('x', 'y')))
+        orL(firestationCity, V(name='x'), existsL(firestationCity, V(name='y', v=('x', eqL(cityLink, 'neighbor', {1}),  city2.name)), 'y'), V(name='z'))
+
+        # Each city has no more then 4 neighbors which are firestationCity
+        atMostL(andL(city, V(name='x'), firestationCity, V(name='y', v=('x', eqL(cityLink, 'neighbor', {1}), city2.name))), V(name='z'), 'x', 4)
