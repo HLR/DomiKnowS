59b143e2d96d482a88e27fcfbf7f2c818cdd7b72
Andrzej Uszok <auszok@ihmc.us>
2021-01-25 19:14:55 -0600

    Updated examples coloring and conll

--- test_regr/examples/graph_coloring/graph2.py
+++ test_regr/examples/graph_coloring/graph2.py
@@ -2,7 +2,7 @@ from itertools import permutations
 
 from regr.graph import Graph, Concept, Relation
 from regr.graph.relation import disjoint
-from regr.graph.logicalConstrain import orL, andL, existsL, notL, eqL, atLeastL, atMostL, exactL
+from regr.graph.logicalConstrain import orL, andL, existsL, notL, eqL, atLeastL, atMostL, exactL, V
 
 
 Graph.clear()
