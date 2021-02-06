359bcad9eaece53f02610ff3853350735ab8fe09
Guo, Quan <1118270+guoquan@users.noreply.github.com>
2021-02-06 23:55:33 +0800

    Update fix conll pair representation and add classifiers

--- examples/conll04/main.py
+++ examples/conll04/main.py
@@ -11,18 +11,23 @@ import spacy
 from spacy.lang.en import English
 nlp = spacy.load('en_core_web_sm') #English()
 
+class Classifier(torch.nn.Sequential):
+    def __init__(self, in_features) -> None:
+        linear = torch.nn.Linear(in_features, 2)
+        super().__init__(linear)
+        # softmax = torch.nn.Softmax(dim=-1)
+        # super().__init__(linear, softmax)
+
 def model():
     from graph import graph, sentence, word, phrase, pair
     from graph import people, organization, location, other, o
     from graph import work_for, located_in, live_in, orgbase_on, kill
     from graph import rel_sentence_contains_word, rel_phrase_contains_word, rel_pair_phrase1, rel_pair_phrase2, rel_sentence_contains_phrase
-    from models import cartesian_concat
 
     graph.detach()
 
     phrase['text'] = ReaderSensor(keyword='tokens')
     phrase['postag'] = ReaderSensor(keyword='postag')
-
 
     def word2vec(text):
         texts = list(map(lambda x: ' '.join(x.split('/')), text))
@@ -37,17 +42,25 @@ def model():
 
     # word[rel_sentence_contains_word, 'text'] = JointSensor(sentence, )
 
-    phrase[people] = ModuleLearner('w2v', module=torch.nn.Linear(96, 2))
-    phrase[organization] = ModuleLearner('w2v', module=torch.nn.Linear(96, 2))
-    phrase[location] = ModuleLearner('w2v', module=torch.nn.Linear(96, 2))
-    phrase[other] = ModuleLearner('w2v', module=torch.nn.Linear(96, 2))
-    phrase[o] = ModuleLearner('w2v', module=torch.nn.Linear(96, 2))
+    phrase[people] = ModuleLearner('w2v', module=Classifier(96))
+    phrase[organization] = ModuleLearner('w2v', module=Classifier(96))
+    phrase[location] = ModuleLearner('w2v', module=Classifier(96))
+    phrase[other] = ModuleLearner('w2v', module=Classifier(96))
+    phrase[o] = ModuleLearner('w2v', module=Classifier(96))
 
-    pair[rel_pair_phrase1.reversed, rel_pair_phrase1.reversed] = CompositionCandidateSensor(
+    pair[rel_pair_phrase1.reversed, rel_pair_phrase2.reversed] = CompositionCandidateSensor(
         phrase['w2v'],
-        relations=(rel_pair_phrase1.reversed, rel_pair_phrase1.reversed),
+        relations=(rel_pair_phrase1.reversed, rel_pair_phrase2.reversed),
         forward=lambda *_, **__: True)
-    pair['emb'] = FunctionalSensor(phrase['w2v'], forward=lambda emb: cartesian_concat(emb, emb))
+    pair['emb'] = FunctionalSensor(
+        rel_pair_phrase1.reversed('w2v'), rel_pair_phrase2.reversed('w2v'),
+        forward=lambda arg1, arg2: torch.cat((arg1, arg2), dim=-1))
+
+    pair[work_for] = ModuleLearner('emb', module=Classifier(96*2))
+    pair[located_in] = ModuleLearner('emb', module=Classifier(96*2))
+    pair[live_in] = ModuleLearner('emb', module=Classifier(96*2))
+    pair[orgbase_on] = ModuleLearner('emb', module=Classifier(96*2))
+    pair[kill] = ModuleLearner('emb', module=Classifier(96*2))
 
     lbp = POIProgram(graph, poi=(phrase, sentence, pair))
     return lbp
