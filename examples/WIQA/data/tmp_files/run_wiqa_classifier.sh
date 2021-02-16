0b84988e5f735fa88aca2dddf12b1656f849fa9a
AdmiralDarius <DariusSafavy@gmail.com>
2021-02-16 17:20:22 -0500

    darius example

---
+++ examples/WIQA/data/tmp_files/run_wiqa_classifier.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export TOKENIZERS_PARALLELISM=false
+python model/run.py --dataset_basedir ../../../../data/WIQA/ \
+                         --lr 2e-5  --max_epochs 20 \
+                         --gpus 2 \
+                         --accelerator ddp
