97297ecd2a9a1edda0fffb256bb7854c7d5b5ca8
Hossein <rajabyfa@msu.edu>
2021-02-22 19:12:15 +0000

    fix the python files to the latest changes of propara and datanode

--- examples/Propara/main.py
+++ examples/Propara/main.py
@@ -13,6 +13,7 @@ from regr.program import LearningBasedProgram
 from regr.program.model.pytorch import PoiModel
 import torch
 from torch import nn
+from reader import ProparaReader
 
 
 class EdgeReaderSensor(EdgeSensor, ReaderSensor):
@@ -34,83 +35,69 @@ def model_declaration():
     from graph import (
         graph,
         procedure,
-        word,
         step,
-        entity,
-        entity_step,
-        entity_step_word,
-        location_start,
-        location_end,
         non_existence,
         unknown_loc,
         known_loc,
+        before,
         action,
         create,
         destroy,
         other,
     )
     from graph import (
-        entity_of_step,
-        entity_of_step_word,
-        step_contains_word,
-        step_of_entity,
-        step_of_entity_word,
-        word_of_entity_step,
         procedure_contain_step,
         action_arg1,
         action_arg2,
+        before_arg1,
+        before_arg2,
     )
-
-    graph.detach()
 
     # --- City
     procedure["id"] = ReaderSensor(keyword="procedureID")
-    step[procedure_contain_step.forward, "text"] = JoinEdgeReaderSensor(
-        procedure["id"],
-        keyword="steps",
-        relation=procedure_contain_step,
-        mode="forward",
+    step[procedure_contain_step, "text"] = JoinReaderSensor(
+        procedure["id"], keyword="steps"
     )
     # word[step_contains_word, 'raw'] = ReaderSensor(keyword='words')
-    entity["raw"] = ReaderSensor(keyword="entities")
-    entity_step[entity_of_step.forward, step_of_entity.forward] = JoinReaderSensor(
-        entity["raw"],
-        step["text"],
-        step[procedure_contain_step.forward],
-        keyword="entity_step",
+    #     entity['raw'] = ReaderSensor(keyword='entities')
+
+    step[non_existence] = ReaderSensor(
+        procedure_contain_step, "text", keyword="non_existence"
     )
-    entity_step[non_existence] = ReaderSensor(keyword="non_existence")
-    entity_step[unknown_loc] = ReaderSensor(keyword="known")
-    entity_step[known_loc] = ReaderSensor(keyword="unkown")
-    entity_step[non_existence] = ReaderSensor(keyword="non_existence")
-    entity_step[unknown_loc] = ReaderSensor(keyword="known")
-    entity_step[known_loc] = ReaderSensor(keyword="unkown")
-    action[action_arg1.forward, action_arg2.forward] = JoinReaderSensor(
-        entity_step[entity_of_step.forward],
-        entity_step[step_of_entity.forward],
-        keyword="action",
+    step[unknown_loc] = ReaderSensor(procedure_contain_step, "text", keyword="unknown")
+    step[known_loc] = ReaderSensor(procedure_contain_step, "text", keyword="known")
+
+    step[non_existence] = ReaderSensor(keyword="non_existence", label=True)
+    step[unknown_loc] = ReaderSensor(keyword="unknown", label=True)
+    step[known_loc] = ReaderSensor(keyword="known", label=True)
+
+    action[action_arg1.reversed, action_arg2.reversed] = JoinReaderSensor(
+        step["text"], keyword="action"
     )
+
     action[create] = ReaderSensor(
-        action_arg1.forward, action_arg2.forward, keyword="create"
+        action_arg1.reversed, action_arg2.reversed, keyword="create"
     )
     action[destroy] = ReaderSensor(
-        action_arg1.forward, action_arg2.forward, keyword="destroy"
+        action_arg1.reversed, action_arg2.reversed, keyword="destroy"
     )
     action[other] = ReaderSensor(
-        action_arg1.forward, action_arg2.forward, keyword="other"
+        action_arg1.reversed, action_arg2.reversed, keyword="other"
     )
-    action[create] = ReaderSensor(
-        action_arg1.forward, action_arg2.forward, keyword="create"
+
+    action[create] = ReaderSensor(keyword="create", label=True)
+    action[destroy] = ReaderSensor(keyword="destroy", label=True)
+    action[other] = ReaderSensor(keyword="other", label=True)
+
+    before[before_arg1.reversed, before_arg2.reversed] = JoinReaderSensor(
+        step["text"], keyword="before"
     )
-    action[destroy] = ReaderSensor(
-        action_arg1.forward, action_arg2.forward, keyword="destroy"
+
+    before["check"] = ReaderSensor(
+        before_arg1.reversed, before_arg2.reversed, keyword="before_true"
     )
-    action[other] = ReaderSensor(
-        action_arg1.forward, action_arg2.forward, keyword="other"
-    )
-    # entity_step_word[entity_of_step_word, step_of_entity_word, word_of_entity_step] = ReaderSensor(keyword='entity_step_word')
-    # entity_step_word[location_start] = ReaderSensor(keyword='location_start')
-    # entity_step_word[location_end] = ReaderSensor(keyword='location_end')
+    before["check"] = ReaderSensor(keyword="before_true", label=True)
+
     program = LearningBasedProgram(
         graph,
         **{
@@ -127,62 +114,70 @@ def main():
     from graph import (
         graph,
         procedure,
-        word,
         step,
-        entity,
-        entity_step,
-        entity_step_word,
-        location_start,
-        location_end,
         non_existence,
         unknown_loc,
         known_loc,
+        before,
         action,
         create,
         destroy,
         other,
     )
     from graph import (
-        entity_of_step,
-        entity_of_step_word,
-        step_contains_word,
-        step_of_entity,
-        step_of_entity_word,
-        word_of_entity_step,
         procedure_contain_step,
         action_arg1,
         action_arg2,
+        before_arg1,
+        before_arg2,
     )
 
-    # set logger level to see training and testing logs
     import logging
 
     logging.basicConfig(level=logging.INFO)
 
     lbp = model_declaration()
 
-    dataset = ProparaReader(file="test_data.json")  # Adding the info on the reader
+    dataset = ProparaReader(
+        file="updated_test_data.json"
+    )  # Adding the info on the reader
 
     #     lbp.test(dataset, device='auto')
-
+    all_updates = []
     for datanode in lbp.populate(dataset, device="cpu"):
-        print("datanode:", datanode)
-        #         print('Spam:', datanode.getAttribute(Spam).softmax(-1))
-        #         print('Regular:', datanode.getAttribute(Regular).softmax(-1))
         datanode.inferILPResults(
-            fun=lambda val: torch.tensor(val)
-            .softmax(dim=-1)
-            .detach()
-            .cpu()
-            .numpy()
-            .tolist(),
-            epsilon=None,
+            create, destroy, other, non_existence, known_loc, unknown_loc, fun=None
         )
-        print("datanode:", datanode)
+        final_output = {
+            "id": datanode.getAttribute("id"),
+            "steps": [],
+            "actions": [],
+            "steps_before": [],
+            "actions_before": [],
+        }
+        for step_info in datanode.findDatanodes(select=step):
+            k = step_info.getAttribute(known_loc, "ILP")
+            u = step_info.getAttribute(unknown_loc, "ILP")
+            n = step_info.getAttribute(non_existence, "ILP")
+            final_output["steps"].append((k, u, n))
+            k = step_info.getAttribute(known_loc)
+            u = step_info.getAttribute(unknown_loc)
+            n = step_info.getAttribute(non_existence)
+            final_output["steps_before"].append((k, u, n))
+        for action_info in datanode.findDatanodes(select=action):
+            c = action_info.getAttribute(create, "ILP")
+            d = action_info.getAttribute(destroy, "ILP")
+            o = action_info.getAttribute(other, "ILP")
+            final_output["actions"].append((c, d, o))
+            c = action_info.getAttribute(create)
+            d = action_info.getAttribute(destroy)
+            o = action_info.getAttribute(other)
+            final_output["actions_before"].append((c, d, o))
+        all_updates.append(final_output)
+    #         print('datanode:', datanode)
+    #         print('inference spam:', datanode.getAttribute(Spam, 'ILP'))
+    #         print('inference regular:', datanode.getAttribute(Regular, 'ILP'))
+    return all_updates
 
 
-#         print('inference spam:', datanode.getAttribute(Spam, 'ILP'))
-#         print('inference regular:', datanode.getAttribute(Regular, 'ILP'))
-
-
-main()
+updated_data = main()
