b655cc8549056b22c9c1b1a68f6d443015d8d200
AdmiralDarius <DariusSafavy@gmail.com>
2021-03-31 13:24:29 -0400

    clean up the code

--- examples/WIQA/WIQA_aug.py
+++ examples/WIQA/WIQA_aug.py
@@ -1,210 +1,169 @@
-
 import torch
 import numpy as np
-from transformers import  AdamW
-from regr.graph.relation import disjoint
-from regr.program import POIProgram
+from transformers import AdamW
 from torch import nn
-from regr.program.metric import MacroAverageTracker, PRF1Tracker, MetricTracker, CMWithLogitsMetric
+
+from regr.graph.relation import disjoint
 from regr.program.loss import NBCrossEntropyLoss
+from regr.program.metric import MacroAverageTracker, PRF1Tracker, MetricTracker, CMWithLogitsMetric
 import logging
-from regr.program.model.primaldual import PrimalDualModel
 from regr.sensor.pytorch.learners import ModuleLearner
 from regr.sensor.pytorch.sensors import ReaderSensor, JointSensor, FunctionalSensor, FunctionalReaderSensor
-from regr.graph.logicalConstrain import nandL, ifL, V, orL, andL, existsL, notL, atLeastL, atMostL, eqL
+from regr.graph.logicalConstrain import nandL, ifL, V, orL, andL, existsL, notL, atLeastL, atMostL, eqL, xorL
 from regr.graph import Graph, Concept, Relation
-from preprocess import make_reader
-from regr.sensor.pytorch.relation_sensors import EdgeSensor, CompositionCandidateReaderSensor, \
-    CompositionCandidateSensor
-from regr.sensor.pytorch.query_sensor import DataNodeReaderSensor
+from WIQA_reader import make_reader
+from regr.sensor.pytorch.relation_sensors import CompositionCandidateSensor
 from regr.program import LearningBasedProgram
 from regr.program.model.pytorch import model_helper, PoiModel
-from preprocess import RobertaTokenizer,DariusWIQA_Robert,is_ILP_consistant
-def guess_pair_datanode_2(*_, data, datanode):
-    quest1_node = datanode.relationLinks[s_arg1.name][0]
-    quest2_node = datanode.relationLinks[s_arg2.name][0]
-    quest1=quest1_node.getAttribute('quest_id')
-    quest2=quest2_node.getAttribute('quest_id')
-    if quest1 in quest2 and "_symmetric" in quest2: #directed?
-        return True
-    else:
-        return False
-cur_epoch=1
-cur_device="auto"
-reader = make_reader(file_address="data/WIQA_AUG/train.jsonl", sample_num=10)
+from WIQA_utils import RobertaTokenizer,is_ILP_consistant,test_inference_results
+from WIQA_models import WIQA_Robert, RobertaClassificationHead,WIQAModel
+import argparse
+from WIQA_utils import make_pair, make_pair_with_labels, make_triple, make_triple_with_labels, guess_pair, guess_triple
 
-# reader.append({"paragraph":para,"more_list":more_list,"less_list":less_list,"no_effect_list":no_effect_list,"question_list":question_list})
-# print(reader[0]["paragraph"])
-# print(reader[1])
-# exit()
+parser = argparse.ArgumentParser(description='Run Wiqa Main Learning Code')
+parser.add_argument('--cuda', dest='cuda_number', default=0, help='cuda number to train the models on')
+parser.add_argument('--epoch', dest='cur_epoch', default=1, help='number of epochs you want your model to train on')
+parser.add_argument('--lr', dest='learning_rate', default=2e-7, help='learning rate of the adamW optimiser')
+parser.add_argument('--pd', dest='primaldual', default=False, help='whether or not to use primaldual constriant learning')
+parser.add_argument('--samplenum', dest='samplenum', default=10, help='number of samples to train the model on')
+parser.add_argument('--batch', dest='batch_size', default=13, help='batch size for neural network training')
+args = parser.parse_args()
+
+# here we set the cuda we want to use and the number of maximum epochs we want to train our model
+cuda_number= args.cuda_number
+cur_device = "cuda:"+str(cuda_number) if torch.cuda.is_available() else 'cpu'
+
+current_model = WIQAModel if args.primaldual else PoiModel
+
+# our reader is a list of dictionaries and each dictionary has the attributes for the root node to read
+reader_train_aug = make_reader(file_address="data/WIQA_AUG/train.jsonl", sample_num=args.samplenum,batch_size=args.batch_size)
+reader_dev_aug = make_reader(file_address="data/WIQA_AUG/dev.jsonl", sample_num=args.samplenum,batch_size=args.batch_size)
+reader_test_aug = make_reader(file_address="data/WIQA_AUG/test.jsonl", sample_num=args.samplenum,batch_size=args.batch_size)
+reader_dev = make_reader(file_address="data/WIQA/dev.jsonl", sample_num=args.samplenum,batch_size=args.batch_size)
+reader_test = make_reader(file_address="data/WIQA/test.jsonl", sample_num=args.samplenum,batch_size=args.batch_size)
+
+print("Graph Declaration:")
+# reseting the graph
 Graph.clear()
 Concept.clear()
 Relation.clear()
 
 with Graph('WIQA_graph') as graph:
+    #first we define paragrapg, then we define questions and add a constains relation from paragraph to question
     paragraph = Concept(name='paragraph')
     question = Concept(name='question')
     para_quest_contains, = paragraph.contains(question)
+
+    # each question could be one the following three concepts:
     is_more = question(name='is_more')
     is_less = question(name='is_less')
     no_effect = question(name='no_effect')
 
+    # we want only one of the labels to be true
     disjoint(is_more, is_less, no_effect)
-
+    orL(is_more, is_less, no_effect)
+    # the symmetric relation is between questions that are opposite of each other and have opposing values
     symmetric = Concept(name='symmetric')
     s_arg1, s_arg2 = symmetric.has_a(arg1=question, arg2=question)
 
+    # here we define that if a question is is_more or is_less and it has a symmetric relation with another
+    # question, then the second question should be is_less and is_more respectively
     ifL(is_more, V(name='x'), is_less, V(name='y', v=('x', symmetric.name, s_arg2.name)))
     ifL(is_less, V(name='x'), is_more, V(name='y', v=('x', symmetric.name, s_arg2.name)))
 
-    #ifL(is_more, V(name='x'), is_less, V(name='y', v=('x', eqL(symmetric, 'label_', {1}), s_arg2.name)))
-    #ifL(is_less, V(name='x'), is_more, V(name='y', v=('x', eqL(symmetric, 'label_', {1}), s_arg2.name)))
-
+    # the transitive relation is between questions that have a transitive relation between them
+    # meaning that the effect of the first question if the cause of the second question and the
+    # third question si made of the cause of the first and the effect of the second question
     transitive = Concept(name='transitive')
     t_arg1, t_arg2, t_arg3 = transitive.has_a(arg11=question, arg22=question, arg33=question)
 
-    ifL(andL(is_more, V(name='x'),is_more,V(name='z', v=('x', transitive.name, t_arg2.name))),
+
+    # the transitive question impiles that if the first and the second question are is_more, so should be the
+    # third question. but if the first question is is_more and the second question is is_less, then the third
+    # question should also be is_less
+
+    ifL(andL(is_more, V(name='x'), is_more, V(name='z', v=('x', transitive.name, t_arg2.name))),
         is_more, V(name='y', v=('x', transitive.name, t_arg3.name)))
 
-    ifL(andL(is_more, V(name='x'),is_less,V(name='z', v=('x', transitive.name, t_arg2.name))),
+    ifL(andL(is_more, V(name='x'), is_less, V(name='z', v=('x', transitive.name, t_arg2.name))),
         is_less, V(name='y', v=('x', transitive.name, t_arg3.name)))
 
-    #ifL(andL(is_more, V(name='x'),is_more,V(name='z', v=('x', eqL(transitive, 'label_', {1}), t_arg2.name))),
-    #    is_more, V(name='y', v=('x', eqL(transitive, 'label_', {1}), t_arg3.name)))
+print("Sensor Part:")
 
-    #ifL(andL(is_more, V(name='x'),is_less,V(name='z', v=('x', eqL(transitive, 'label_', {1}), t_arg2.name))),
-    #    is_less, V(name='y', v=('x', eqL(transitive, 'label_', {1}), t_arg3.name)))
+# the first sensor reads the text property of the paragraph
+paragraph['paragraph_intext'] = ReaderSensor(keyword='paragraph_intext')
 
-print("Sensor part:")
-
-paragraph['paragraph'] = ReaderSensor(keyword='paragraph')
+# the following sensors read the concatenated properties of the questions related to this paragraph
+# these concatenated properties will be splitted and put into their respective question later
 paragraph['question_list'] = ReaderSensor(keyword='question_list')
-
 paragraph['less_list'] = ReaderSensor(keyword='less_list')
 paragraph['more_list'] = ReaderSensor(keyword='more_list')
 paragraph['no_effect_list'] = ReaderSensor(keyword='no_effect_list')
-
 paragraph['quest_ids'] = ReaderSensor(keyword='quest_ids')
 
 def str_to_int_list(x):
     return torch.LongTensor([[int(i)] for i in x])
+
 def make_questions(paragraph, question_list, less_list, more_list, no_effect_list, quest_ids):
-    #print("make_questions", paragraph,question_list,less_list.split("@@"))
     return torch.ones((len(question_list.split("@@")), 1)), [paragraph for i in range(len(question_list.split("@@")))], \
-           question_list.split("@@"), str_to_int_list(less_list.split("@@")), str_to_int_list(more_list.split("@@")),\
+           question_list.split("@@"), str_to_int_list(less_list.split("@@")), str_to_int_list(more_list.split("@@")), \
            str_to_int_list(no_effect_list.split("@@")), quest_ids.split("@@")
 
-
+# the joint sensor here reads the concatenated question properties from the paragraph and put them into questions
+# the first property is the contain relation between paragraph and its questions
+# the output of the joint sensor is a tuple with 7 members. each member correspond to its respective property in question
+# for example the first element of the output tuple is a tensor of the shape (number of questions,1) meaning that each question
+# has a contain relation with the ith question.
+# the third element of the tuple is a list of str elements each a text of the a question related to the paragraph.
 question[para_quest_contains, "question_paragraph", 'text', "is_more_", "is_less_", "no_effect_", "quest_id"] = JointSensor(
-    paragraph['paragraph'], paragraph['question_list']
-    , paragraph['less_list'], paragraph['more_list'], paragraph['no_effect_list'], paragraph['quest_ids'], forward=make_questions)
+    paragraph['paragraph_intext'], paragraph['question_list'], paragraph['less_list'], paragraph['more_list'],
+    paragraph['no_effect_list'], paragraph['quest_ids'],forward=make_questions)
 
-question["token_ids", "Mask"] = JointSensor(para_quest_contains, "question_paragraph", 'text', forward=RobertaTokenizer("more"))
-#question["token_ids_more", "Mask_more"] = JointSensor(para_quest_contains, "question_paragraph", 'text', forward=RobertaTokenizer("more"))
-#question["token_ids_less", "Mask_less"] = JointSensor(para_quest_contains, "question_paragraph", 'text', forward=RobertaTokenizer("less"))
-#question["token_ids_no_effect", "Mask_no_effect"] = JointSensor(para_quest_contains, "question_paragraph", 'text', forward=RobertaTokenizer("no effect"))
+# we use another joint sensor, so given a roberta transformer tokenizer, we use the paragraph and question text, concat them and feed them
+# to the tokenizer and get the token_ids and masks for the <s>paragraph</s></s>question</s>
+question["token_ids", "Mask"] = JointSensor(para_quest_contains, "question_paragraph", 'text',forward=RobertaTokenizer())
 
 def label_reader(_, label):
     return label
 
+# we load the is_more, is_less and no_effect attributes with label=True to tell the program
+# about the real label of these questions for training
 question[is_more] = FunctionalSensor(para_quest_contains, "is_more_", forward=label_reader, label=True)
 question[is_less] = FunctionalSensor(para_quest_contains, "is_less_", forward=label_reader, label=True)
 question[no_effect] = FunctionalSensor(para_quest_contains, "no_effect_", forward=label_reader, label=True)
 
-roberta_model=DariusWIQA_Robert()
+roberta_model = WIQA_Robert()
 
-#question["all_embs"] = ModuleLearner("token_ids_more", "Mask_more","token_ids_less", "Mask_less","token_ids_no_effect", "Mask_no_effect", module=roberta_model)
-class EmbSeprateor:
-    def __init__(self,dim):
-        self.dim=dim
-    def __call__(self,_,all_embs):
-        return all_embs[self.dim]
-question["emb"] = ModuleLearner("token_ids", "Mask", module=roberta_model)
-#question["emb_is_more"] = ModuleLearner("token_ids_more", "Mask_more", module=roberta_model)
-#question["emb_is_less"] = ModuleLearner("token_ids_less", "Mask_less", module=roberta_model)
-#question["emb_no_effect"] = ModuleLearner("token_ids_no_effect", "Mask_no_effect", module=roberta_model)
+# we calculate the encoding for each <s>paragraph</s></s>question</s> by feeding it to roberta model
+# this is a ModuleLearner meaning that the parameters of roberta will be trained during learning
+question["robert_emb"] = ModuleLearner("token_ids", "Mask", module=roberta_model)
 
-from preprocess import make_pair,make_pair_with_labels,make_triple,make_triple_with_labels,guess_pair,guess_triple
+# the CompositionCandidateSensor takes two or three questions and return True or false if they are symmetric or transitive respectively
+symmetric[s_arg1.reversed, s_arg2.reversed] = CompositionCandidateSensor(question['quest_id'],relations=(s_arg1.reversed, s_arg2.reversed),forward=guess_pair)
+transitive[t_arg1.reversed, t_arg2.reversed, t_arg3.reversed] = CompositionCandidateSensor(question['quest_id'],relations=(t_arg1.reversed,t_arg2.reversed,t_arg3.reversed),forward=guess_triple)
 
-symmetric[s_arg1.reversed, s_arg2.reversed] = CompositionCandidateSensor(question['quest_id'], relations=(s_arg1.reversed, s_arg2.reversed), forward=guess_pair)
-#symmetric['neighbor'] = DataNodeReaderSensor(s_arg1.reversed, s_arg2.reversed, keyword='links', forward=guess_pair_datanode_2)
-#symmetric[s_arg1.reversed, s_arg2.reversed,"labels_"] = JointSensor(question['quest_id'], forward=make_pair)
-#symmetric[s_arg1.reversed, s_arg2.reversed,"labels_"] = JointSensor(question['quest_id'], forward=make_pair_with_labels)
+# finally the embedding are used to learn the label of each question by defining a linear model on top of roberta
+question[is_more] = ModuleLearner("robert_emb", module=RobertaClassificationHead(roberta_model.last_layer_size))
+question[is_less] = ModuleLearner("robert_emb", module=RobertaClassificationHead(roberta_model.last_layer_size))
+question[no_effect] = ModuleLearner("robert_emb", module=RobertaClassificationHead(roberta_model.last_layer_size))
 
-transitive[t_arg1.reversed, t_arg2.reversed, t_arg3.reversed] = CompositionCandidateSensor(question['quest_id'], relations=(t_arg1.reversed, t_arg2.reversed, t_arg3.reversed), forward=guess_triple)
-#transitive[t_arg1.reversed, t_arg2.reversed, t_arg3.reversed,"labels_"] = JointSensor(question['quest_id'], forward=make_triple_with_labels)
+# in our program we define POI ( points of interest) that are the final Concepts we want to be calculated
+# other inputs are graph, loss function and the metric
+program = LearningBasedProgram(graph, model_helper(current_model,poi=[question[is_less], question[is_more], question[no_effect],\
+                                    symmetric, transitive],loss=MacroAverageTracker(NBCrossEntropyLoss()), metric=PRF1Tracker()))
 
-class Classifier(torch.nn.Linear):
-    def __init__(self, dim_in=roberta_model.last_layer_size):
-        super().__init__(dim_in, 2)
-
-#shared_layer = Classifier()
-
-
-
-question[is_more] = ModuleLearner("emb", module=Classifier())
-question[is_less] = ModuleLearner("emb", module=Classifier())
-question[no_effect] = ModuleLearner("emb", module=Classifier())
-
-class WIQAModel(PrimalDualModel):
-    def __init__(self, graph,poi,loss,metric):
-        super().__init__(
-            graph,
-            poi=poi,
-            loss=loss,
-            metric=metric)
-
-class NBCrossEntropyLoss_(torch.nn.CrossEntropyLoss):
-    def forward(self, input, target, *args, **kwargs):
-        input = input.view(-1, input.shape[-1])
-        target = target.view(-1).to(dtype=torch.long)
-        #print(input.shape,target.shape,super().forward(input, target, *args, **kwargs))
-        #print(input,target)
-        return super().forward(input, target, *args, **kwargs)
-
-#program = POIProgram(graph, loss=MacroAverageTracker(NBCrossEntropyLoss_()), metric=PRF1Tracker())
-program = LearningBasedProgram(graph, model_helper(PoiModel,#WIQAModel
-                    poi=[question[is_less], question[is_more], question[no_effect],symmetric,transitive],
-                            loss=MacroAverageTracker(NBCrossEntropyLoss_()), metric=PRF1Tracker()))
 logging.basicConfig(level=logging.INFO)
-program.train(reader, train_epoch_num=cur_epoch, Optim=AdamW, device=cur_device)
+
+for i in range(args.cur_epoch):
+    program.train(reader_train_aug, train_epoch_num=1, Optim=lambda param: AdamW(param, lr = args.learning_rate,eps = 1e-8 ), device=cur_device)
+    print('-' * 40,"\n",'Training result:')
+    print(program.model.loss)
+    ##test_inference_results(program,reader_train_aug,cur_device,is_more,is_less,no_effect)
+    test_inference_results(program,reader_dev_aug,cur_device,is_more,is_less,no_effect)
+    test_inference_results(program,reader_test_aug,cur_device,is_more,is_less,no_effect)
+    test_inference_results(program,reader_dev,cur_device,is_more,is_less,no_effect)
+    test_inference_results(program,reader_test,cur_device,is_more,is_less,no_effect)
 
 
-print('Training result:')
-print(program.model.loss)
-print(program.model.metric)
 
-print('-' * 40)
 
-#
-# test
-#
-# program.test(reader, device='auto')
-# print('Testing result:')
-# print(program.model.loss)
-# print(program.model.metric)
-
-print('-' * 40)
-counter=0
-ac_=0
-for paragraph_ in program.populate(reader, device=cur_device):
-    #print("paragraph:", paragraph_.getAttribute('paragraph'))
-    paragraph_.inferILPResults(is_more,is_less,no_effect,fun=None)
-    questions_id,results=[],[]
-    for question_ in paragraph_.getChildDataNodes():
-        #print(question_.getAttribute('text'))
-        #print(question_.getAttribute('question_paragraph'))
-        #print(question_.getAttribute('emb'))
-        questions_id.append(question_.getAttribute('quest_id'))
-        results.append((question_.getAttribute(is_more,"ILP"),question_.getAttribute(is_less,"ILP"),question_.getAttribute(no_effect,"ILP")))
-
-        #predict_is_more=question_.getAttribute(is_more).softmax(-1).argmax().item()
-        predict_is_more_value=question_.getAttribute(is_more).softmax(-1)[0].item()
-        predict_is_less_value=question_.getAttribute(is_less).softmax(-1)[0].item()
-        predict_no_effect_value=question_.getAttribute(no_effect).softmax(-1)[0].item()
-        counter+=1
-        ac_+=np.array([predict_is_more_value,predict_is_less_value,predict_no_effect_value]).argmax()==np.array([question_.getAttribute("is_more_"),question_.getAttribute("is_less_"),question_.getAttribute("no_effect_")]).argmax()
-
-    if not is_ILP_consistant(questions_id,results):
-        print("ILP inconsistency")
-print("dev accuracy:",ac_/counter)
---
+++ examples/WIQA/WIQA_utils.py
@@ -0,0 +1,154 @@
+from transformers import RobertaTokenizerFast
+import torch
+from itertools import product
+import numpy as np
+
+class RobertaTokenizer:
+    def __init__(self,max_length=256):
+        self.max_length=max_length
+        self.tokenizer= RobertaTokenizerFast.from_pretrained("roberta-base")
+
+    def __call__(self,_, question_paragraph, text):
+        encoded_input = self.tokenizer(question_paragraph,text ,padding="max_length",max_length =self.max_length)
+        input_ids = encoded_input["input_ids"]
+        attention_mask = encoded_input["attention_mask"]
+        return torch.LongTensor(input_ids),torch.LongTensor(attention_mask)
+
+def make_pair(question_ids):
+    n=len(question_ids)
+    p1=[]
+    p2=[]
+    for arg1, arg2 in product(range(n), repeat=2):
+        if arg1 == arg2:
+            continue
+        if question_ids[arg1] in question_ids[arg2] and "_symmetric" in question_ids[arg2]:
+            p1.append([1 if i==arg1 else 0 for i in range(n)])
+            p2.append([1 if i==arg2 else 0 for i in range(n)])
+    return torch.LongTensor(p1),torch.LongTensor(p2)
+
+def make_pair_with_labels(question_ids):
+    n=len(question_ids)
+    p1=[]
+    p2=[]
+    label_output=[]
+    for arg1, arg2 in product(range(n), repeat=2):
+        p1.append([1 if i==arg1 else 0 for i in range(n)])
+        p2.append([1 if i==arg2 else 0 for i in range(n)])
+        if arg1 == arg2:
+            label_output.append([0])
+            continue
+        if question_ids[arg1] in question_ids[arg2] and "_symmetric" in question_ids[arg2]:
+            label_output.append([1])
+        else:
+            label_output.append([0])
+
+    return torch.LongTensor(p1),torch.LongTensor(p2),torch.LongTensor(label_output)
+
+def make_triple(question_ids):
+    n=len(question_ids)
+    p1=[]
+    p2=[]
+    p3=[]
+    for arg1, arg2, arg3 in product(range(n), repeat=3):
+        if arg1 == arg2 or arg2 == arg3:
+            continue
+        if question_ids[arg1] in question_ids[arg3] and \
+           question_ids[arg2] in question_ids[arg3] and \
+                "_transit" in question_ids[arg3]:
+            p1.append([1 if i==arg1 else 0 for i in range(n)])
+            p2.append([1 if i==arg2 else 0 for i in range(n)])
+            p3.append([1 if i==arg3 else 0 for i in range(n)])
+
+    return torch.LongTensor(p1),torch.LongTensor(p2),torch.LongTensor(p3)
+
+def make_triple_with_labels(question_ids):
+    n=len(question_ids)
+    p1=[]
+    p2=[]
+    p3=[]
+    label_output=[]
+    for arg1, arg2, arg3 in product(range(n), repeat=3):
+        p1.append([1 if i==arg1 else 0 for i in range(n)])
+        p2.append([1 if i==arg2 else 0 for i in range(n)])
+        p3.append([1 if i==arg3 else 0 for i in range(n)])
+        if arg1 == arg2 or arg2 == arg3:
+            label_output.append([0])
+            continue
+        if question_ids[arg1] in question_ids[arg3] and \
+           question_ids[arg2] in question_ids[arg3] and \
+                "_transit" in question_ids[arg3]:
+            label_output.append([1])
+        else:
+            label_output.append([0])
+    return torch.LongTensor(p1),torch.LongTensor(p2),torch.LongTensor(p3),torch.LongTensor(label_output)
+
+def guess_pair(quest_id, arg1, arg2):
+    if len(quest_id)<2 or arg1==arg2:
+        return False
+    quest1, quest2 = arg1.getAttribute('quest_id'), arg2.getAttribute('quest_id')
+    if quest1 in quest2 and "_symmetric" in quest2:
+        return True
+    else:
+        return False
+
+def guess_triple(quest_id, arg11, arg22,arg33):
+    if len(quest_id)<3 or arg11==arg22 or arg22==arg33:
+        return False
+    quest1, quest2,quest3 = arg11.getAttribute('quest_id'), arg22.getAttribute('quest_id'), arg33.getAttribute('quest_id')
+    if quest1 in quest3 and quest2 in quest3 and "_transit" in quest3:
+        return True
+    return False
+
+def is_ILP_consistant(questions_id,results):
+    for i in results:
+        if i[0]==None or i[1]==None or i[2]==None:
+            return False
+        if not i[0]+i[1]+i[2]==1:
+            return False
+    n=len(questions_id)
+    for arg1, arg2 in product(range(n), repeat=2):
+        if arg1 == arg2:
+            continue
+        if questions_id[arg1] in questions_id[arg2] and "_symmetric" in questions_id[arg2]:
+            if (results[arg1][0] and not results[arg2][1]) or (results[arg1][1] and not results[arg2][0]):
+                return False
+
+    for arg1, arg2, arg3 in product(range(n), repeat=3):
+        if arg1 == arg2 or arg2 == arg3:
+            continue
+        if questions_id[arg1] in questions_id[arg3] and \
+           questions_id[arg2] in questions_id[arg3] and \
+                "_transit" in questions_id[arg3]:
+            if (results[arg1][0] and results[arg2][0] and not results[arg3][0]) or\
+                    (results[arg1][0] and results[arg2][1] and not results[arg3][1]):
+                return False
+    return True
+
+
+def test_inference_results(program, reader,cur_device,is_more,is_less,no_effect):
+    counter = 0
+    ac_ = 0
+    ILPac_ = 0
+    for paragraph_ in program.populate(reader, device=cur_device):
+        print("paragraph:", paragraph_.getAttribute('paragraph_intext'))
+        paragraph_.inferILPResults(is_more,is_less,no_effect,fun=None)
+        questions_id, results = [], []
+        for question_ in paragraph_.getChildDataNodes():
+            print(question_.getAttribute('text'))
+            questions_id.append(question_.getAttribute('quest_id'))
+            results.append((question_.getAttribute(is_more, "ILP").item(), question_.getAttribute(is_less, "ILP").item(),question_.getAttribute(no_effect, "ILP").item()))
+
+            predict_is_more_value=question_.getAttribute(is_more).softmax(-1)[1].item()
+            predict_is_less_value=question_.getAttribute(is_less).softmax(-1)[1].item()
+            predict_no_effect_value=question_.getAttribute(no_effect).softmax(-1)[1].item()
+            if not "_symmetric" in question_.getAttribute('quest_id') and not "_transit" in question_.getAttribute('quest_id'):
+                counter += 1
+                ac_+=np.array([predict_is_more_value,predict_is_less_value,predict_no_effect_value]).argmax()==np.array([question_.getAttribute("is_more_"),question_.getAttribute("is_less_"),question_.getAttribute("no_effect_")]).argmax()
+                ILPac_+=np.array(list(results[-1])).argmax()==np.array([question_.getAttribute("is_more_"),question_.getAttribute("is_less_"),question_.getAttribute("no_effect_")]).argmax()
+        print(results)
+        if not is_ILP_consistant(questions_id, results):
+            print("ILP inconsistency")
+
+    print("accuracy:", ac_ / counter)
+    print("ILP accuracy:", ILPac_ / counter)
+
