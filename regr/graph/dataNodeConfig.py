a3e6dc2f1ac7a860718054a752c45e4062b5bade
Hossein <rajabyfa@msu.edu>
2021-04-15 21:03:26 +0000

    setting default log to errors

--- regr/graph/dataNodeConfig.py
+++ regr/graph/dataNodeConfig.py
@@ -5,7 +5,7 @@ dnConfig = {
     # Logging configuration forDataNode and DataNoteBuilder
     'ifLog': True,
     'log_name' : 'dataNode', 
-    'log_level' : logging.INFO,
+    'log_level' : logging.ERROR,
     'log_filename' : 'datanode.log',
     'log_filesize' : 5*1024*1024*1024,
     'log_backupCount' : 0,
