1ec9f3af596b74bfadf15ee6878611c5dfae5e25
Hossein <rajabyfa@msu.edu>
2021-04-16 15:07:46 +0000

    adding graph coloring to the example folder

---
+++ examples/graph_coloring/graph.py
@@ -0,0 +1,20 @@
+from regr.graph import Graph, Concept, Relation
+from regr.graph.logicalConstrain import V, orL, andL, existsL, notL, atLeastL, atMostL
+
+
+Graph.clear()
+Concept.clear()
+Relation.clear()
+
+with Graph('global') as graph:
+        world = Concept(name='world')
+        city = Concept(name='city')
+        (world_contains_city,) = world.contains(city)
+         
+        neighbor = Concept(name='neighbor')
+        (city1, city2) = neighbor.has_a(arg1=city, arg2=city)
+        
+        firestationCity = city(name='firestationCity')
+        
+        # Constraints - For each city x either it is a firestationCity or exists a city y which is in neighbor relation to city x and y is a firestationCity        
+        orL(firestationCity, V(name='x'), existsL(firestationCity, V(name='y', v=('x', neighbor.name, city2.name)), 'y'), V(name='z'))
---
+++ examples/graph_coloring/graph2.py
@@ -0,0 +1,33 @@
+from regr.graph import Graph, Concept, Relation
+from regr.graph.logicalConstrain import orL, andL, existsL, eqL, atLeastL, atMostL, exactL, V
+
+
+Graph.clear()
+Concept.clear()
+Relation.clear()
+
+with Graph('global') as graph2:
+        world = Concept(name='world')
+        city = Concept(name='city')
+        (world_contains_city,) = world.contains(city)
+        
+        cityLink = Concept(name='cityLink')
+        (city1, city2) = cityLink.has_a(arg1=city, arg2=city)
+
+        firestationCity = city(name='firestationCity')
+        
+        # Constraints - For each city x either it is a firestationCity or exists a city y which is in cityLink relation with neighbor attribute equal 1 to city x and y is a firestationCity
+        orL(firestationCity, V(name='x'), existsL(firestationCity, V(v=('x', eqL(cityLink, 'neighbor', {True}),  city2))))
+        
+        
+        # No less then 1 firestationCity
+        atLeastL(firestationCity, p=90)
+        
+        # At most 1 firestationCity
+        atMostL(firestationCity, p=80)
+        
+        # Each city has no more then 4 neighbors which are firestationCity
+        atMostL(andL(city, V(name='x'), firestationCity, V(v=('x', eqL(cityLink, 'neighbor', {True}), city2))), 4, p=60)
+
+        # Exactly 2 firestationCity
+        exactL(firestationCity, 2, p=55)
