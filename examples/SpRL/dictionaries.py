f004e3a07c953b468d07b7dea35f926253d5d829
Hossein <rajabyfa@msu.edu>
2021-04-16 15:15:28 +0000

    Removing SPRL example

--- examples/SpRL/dictionaries.py
+++
@@ -1,6 +0,0 @@
-class dictionary():
-
-    prepositions = ["about", "above", "across", "after", "against", "along", "among", "around", "as", "at", "before",
-"behind", "beneath", "beside", "between", "by", "down", "during", "for", "from", "in", "inside",
-"into", "like", "of", "off", "on", "onto", "over", "round", "through", "to", "towards", "with"
-]
