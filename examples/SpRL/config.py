ff75d49fc597440ae0806c87be2550c32cd9c1c0
Hossein <rajabyfa@msu.edu>
2021-04-16 15:16:23 +0000

    Adding the Dummy SPRL data

---
+++ examples/SpRL/config.py
@@ -0,0 +1,56 @@
+import time
+from utils import Namespace
+
+
+config = {
+    'Data': {  # data setting
+        'relative_path': "data",
+#         'train_path': "new_train.xml",
+#         'valid_path': "new_gold.xml",
+        'train_path': "sprl2017_train.xml",
+        'valid_path': "sprl2017_gold.xml"
+    },
+    'Model': {  # model setting
+        'embedding_dim': 8,
+        'dropout': 0.5,
+        'ngram': 5,
+        'encode': {
+            'layers': [64, 64],
+        },
+        'compact': 32,
+        'graph': {
+            'balance_factor': 1,
+            'label_smoothing': 0.01,
+            'focal_gamma': 2,
+            'inference_interval': 1,
+            'inference_training_set': True,
+            'inference_loss': True,
+            'log_solver': True,
+            'soft_penalty': 0.
+        }
+    },
+    'Train': {
+        'pretrained_files': None,
+        'trainer':{
+            'num_epochs': 50,
+            'patience': None,
+            'serialization_dir': 'log.{}'.format(time.strftime("%Y%m%d-%H%M%S", time.gmtime())),
+#             'serialization_dir':None,
+        },
+        'optimizer': {
+            'type': 'adam',
+            'lr': 0.005,
+            'weight_decay': 0.001,
+        },
+
+        'scheduler': {
+            'type': 'reduce_on_plateau',
+            'patience': 10
+        },
+        'iterator': {
+            'batch_size': 2,
+        }
+    }
+}
+
+Config = Namespace(config)
