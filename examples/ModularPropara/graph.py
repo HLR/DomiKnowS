7dff7e50678a4ae22aa8ebd037bd3a65bcd8a38a
Hossein Faghihi <rajabyfa@msu.edu>
2021-05-05 16:19:53 +0000

    updating the graph to be a multiclass

--- examples/ModularPropara/graph.py
+++ examples/ModularPropara/graph.py
@@ -1,5 +1,6 @@
 from regr.graph import Graph, Concept, Relation
 from regr.graph.logicalConstrain import orL, andL, existsL, notL, atLeastL, atMostL, ifL, nandL, V
+from regr.graph.concept import EnumConcept
 
 Graph.clear()
 Concept.clear()
@@ -18,49 +19,42 @@ with Graph('global') as graph:
     
     step = Concept("step")
     (procedure_contain_step, ) = procedure.contains(step)
+    
     before = Concept("before")
     (before_arg1, before_arg2) = before.has_a(arg1=step, arg2=step)
     
     action = Concept(name='action')
     (action_step, action_entity) = action.has_a(step, entity)
     
-    create = action(name="create")
-    destroy = action(name="destroy")
-    move = action(name="move")
-    nochange = action('none')
+    action_label = (name="action_label", ConceptClass=EnumConcept, values=["nochange", "destroy", "create", "move"])
+#     create = action(name="create")
+#     destroy = action(name="destroy")
+#     move = action(name="move")
+#     nochange = action('none')
     
-    Tcreate = action(name="trips_create")
-    Tdestroy = action(name="trips_destroy")
-    Tmove = action(name="trips_move")
-    Tnochange = action('trips_none')
-
-    exactL(Tcreate, Tdestroy, Tmove, Tnochange)
-    exactL(create, destroy, move, nochange)
+#     Tcreate = action(name="trips_create")
+#     Tdestroy = action(name="trips_destroy")
+#     Tmove = action(name="trips_move")
+#     Tnochange = action('trips_none')
+    trips_action_label = (name="trips_action_label", ConceptClass=EnumConcept, values=["trips_none", "trips_destroy", "trips_create", "trips_move"])
     
-    ifL(Tcreate, create)
-    ifL(Tdestroy, destroy)
-    ifL(Tmove, move)
-    ifL(Tnochange, nochange)
+#     exactL(Tcreate, Tdestroy, Tmove, Tnochange)
+#     exactL(create, destroy, move, nochange)
     
-#     ifL(
-#         destroy('x'), 
-#         notL(
-#             andL(
-#                 existsL(
-#                     andL(destroy('y'), existsL(before('y', path=('x', arg1, 'before', arg1)))),
-#                     notL(
-#                         existsL(
-#                             andL(
-#                                 create('z'), 
-#                                 eqL(before('z'))
-#                             )
-#                         )
-#                     )
-#                 )
-#             )
-#         )
-#     )
-#   atMostL(1, ("x"), andL(entity, "y", create()))
-    # No entity_step
+    ifL(trips_action_label.trips_create, action_label.create)
+    ifL(trips_action_label.trips_destroy, action_label.destroy)
+    ifL(trips_action_label.trips_move, action_label.move)
+    
+    # x is destroy at step i and entity e then and (step i, (step j before step i), not(destroy ))
+#     ifL(destroy('x'), andL(step('i', path=('x', arg2)), step('j', path=('i', before1)), notL(destroy('y', path=(’j’, action_step.reversed))), step('k', path=('i', before2)), notL(create('y', path=('j', inverse-arg2)))))
+    
+    
+    ### Rules we need
+    
+    ## if x is destroyed at step i, it should exists at step i-1
+    
+    ## if x is moved at step i, it should exists at step i-1 
+    
+    ## if x is created at step i, it shouldnt exists at step i-1 --> Shouldn't exist means there is no move before that.
 
 
