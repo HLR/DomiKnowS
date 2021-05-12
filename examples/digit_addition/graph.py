18ef7893ea3632b10ca162b6504a892e02e1747a
Drew Hayward <d.andrew.hayward@gmail.com>
2021-05-12 11:11:44 -0400

    change constraints

--- examples/digit_addition/graph.py
+++ examples/digit_addition/graph.py
@@ -1,27 +1,31 @@
 import sys
 sys.path.append("../..")
 
+from itertools import product
 from regr.graph import Graph, Concept, Relation
+from regr.graph.concept import EnumConcept
 from regr.graph.logicalConstrain import orL, andL, existsL, notL, atLeastL, atMostL, ifL, V
 
 Graph.clear()
 Concept.clear()
 Relation.clear()
 
+digit_names =[
+        "zero","one","two","three","four","five","six","seven","eight","nine"]
+
 with Graph('global') as graph:
     # Single digits
     digit = Concept(name='digit')
+    digit_labels = digit(name='tag', ConceptClass=EnumConcept, values=digit_names)
 
-    digits = {}
-    for i in range(10):
-        digits[i] = digit(name=f"digit_{i}")
-
-    disjoint(*list(digits.values()))
-
-    # Number
-    number = Concept(name='number')
-    number.has_a(arg1=digit)
-
-    # Summation
     summation = Concept(name='summation')
-    summation.has_a(arg1=number, arg2=number)
+    summation.has_a(arg1=digit, arg2=digit)
+
+    summation_labels = {}
+    for i, j in product(range(10), range(10)):
+        ifL(andL((digit_labels, i), V("x"), (digit_labels, j), V("y")), summation)
+        digit_labels
+
+        summation_labels[i] = summation_labels(name=f"summation_{i}")
+
+    
---
+++ examples/digit_addition/model.py
@@ -0,0 +1,22 @@
+import sys
+import torch
+
+sys.path.append('.')
+sys.path.append('../..')
+
+import graph as concept_graph
+
+
+# def digit_concat()
+
+class DigitClassifier(torch.nn.Module):
+    def __init__(self):
+        self.layers = torch.nn.Sequential(
+            
+        )
+
+    def forward(self, img):
+        return self.layers(img)
+
+def addition_function(arg_1, arg_2):
+    pass
