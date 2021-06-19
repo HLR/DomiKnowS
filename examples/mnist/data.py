8f5c64dc3e1a3b42555e9c9399a452f5d06374a1
Guo Quan <1118270+guoquan@users.noreply.github.com>
2021-06-19 19:55:59 +0800

    Update data folder of mnist

--- examples/mnist/data.py
+++ examples/mnist/data.py
@@ -4,13 +4,16 @@ from torch.utils.data import DataLoader
 from torch.nn import Flatten
 
 
+DATA_PATH = 'data'
+
+
 def get_readers():
     transform = transforms.Compose([transforms.ToTensor(),
                               transforms.Normalize((0.5,), (0.5,)),
                               Flatten(0)
                               ])
-    trainset = datasets.MNIST('PATH_TO_STORE_TRAINSET', download=True, train=True, transform=transform)
-    testset = datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
+    trainset = datasets.MNIST(DATA_PATH, download=True, train=True, transform=transform)
+    testset = datasets.MNIST(DATA_PATH, download=True, train=False, transform=transform)
     trainloader = DataLoader(trainset,# batch_size=1,
         # sampler=random.sample(range(60000),600),
         shuffle=True,
