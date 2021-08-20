fef6e478c61ee1b7e0aadb6d8346796f430b743d
iamdanialkamali <daniel.kamali@yahoo.com>
2021-08-21 00:21:09 +0430

    Modify model for resnet50

--- examples/AnimalAndFlower/model.py
+++ examples/AnimalAndFlower/model.py
@@ -17,15 +17,22 @@ from torchvision.models.resnet import resnet50
 
 output_size = 45
 
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
 class ImageNetworkNormal(torch.nn.Module):
-    def __init__(self, n_outputs=2,model_size=10):
+    def __init__(self, n_outputs=2, model_size=10):
         super(ImageNetwork, self).__init__()
-        print("size of the model is: ",model_size)
+        print("size of the model is: ", model_size)
         self.num_outputs = n_outputs
         self.conv1 = nn.Conv2d(3, model_size, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(model_size, model_size*2, 5)
-        self.conv3 = nn.Conv2d(model_size*2, model_size, 5)
+        self.conv2 = nn.Conv2d(model_size, model_size * 2, 5)
+        self.conv3 = nn.Conv2d(model_size * 2, model_size, 5)
         self.conv4 = nn.Conv2d(model_size, 5, 3)
         self.bn = nn.BatchNorm2d(5)
 
@@ -41,20 +48,39 @@ class ImageNetworkNormal(torch.nn.Module):
         x = x.view(-1, output_size)
         return x
 
+
 output_size = 1000
+
+
 class ImageNetwork(torch.nn.Module):
-    def __init__(self, n_outputs=2,model_size=10):
+    def __init__(self, n_outputs=2, model_size=10):
         super(ImageNetwork, self).__init__()
-        print("size of the model is: ",model_size)
         self.num_outputs = n_outputs
-        self.conv = resnet50()
+        self.conv = resnet50(pretrained=True)
+        set_parameter_requires_grad(self.conv, True)
+
     def forward(self, x):
         x = self.conv(x)
+        x = F.dropout(x, 0.5)
         x = x.view(-1, output_size)
+        x = F.dropout(x, 0.5)  # I think this part may be redundancy either in here or in the tensorflow model
         return x
 
 
-def model_declaration(device,solver='iml',lambdaValue=0.5,model_size=10):
+class DenseNetwork(torch.nn.Module):
+    def __init__(self, n_outputs=2):
+        super(DenseNetwork, self).__init__()
+        self.dense = nn.Linear(output_size, n_outputs)
+        # self.dense1 = nn.Linear(output_size,output_size//2)
+        # self.dense2 = nn.Linear(output_size//2,output_size//4)
+        # self.dense3 = nn.Linear(output_size//3,n_outputs)
+
+    def forward(self, x):
+        x = self.dense(x)
+        return x
+
+
+def model_declaration(device, solver='iml', lambdaValue=0.5, model_size=10):
     solver = solver.lower()
     """
     this function creates and defines the structure of our graph model
@@ -89,18 +115,18 @@ def model_declaration(device,solver='iml',lambdaValue=0.5,model_size=10):
     image[sunflower] = ReaderSensor(keyword='sunflower', label=True, device=device)
     image[tulip] = ReaderSensor(keyword='tulip', label=True, device=device)
 
-    image['emb'] = ModuleLearner('pixels', module=ImageNetwork(n_outputs=2,model_size=model_size), device=device)
-    image[animal] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[cat] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[dog] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[monkey] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[squirrel] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[flower] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[daisy] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[dandelion] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[rose] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[sunflower] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
-    image[tulip] = ModuleLearner('emb', module=nn.Linear(output_size, 2), device=device)
+    image['emb'] = ModuleLearner('pixels', module=ImageNetwork(n_outputs=2, model_size=model_size), device=device)
+    image[animal] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[cat] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[dog] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[monkey] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[squirrel] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[flower] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[daisy] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[dandelion] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[rose] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[sunflower] = ModuleLearner('emb', module=DenseNetwork(), device=device)
+    image[tulip] = ModuleLearner('emb', module=DenseNetwork(), device=device)
 
     if solver == 'iml':
         print("IMLProgram selected as solver")
@@ -121,4 +147,3 @@ def model_declaration(device,solver='iml',lambdaValue=0.5,model_size=10):
                                    metric=PRF1Tracker(DatanodeCMMetric('local/argmax')))
 
     return program
-
