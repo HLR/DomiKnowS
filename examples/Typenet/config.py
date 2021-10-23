9f1969f10b8d11cdb46a180bd2619d0ff8d91ed4
alexwan0 <wanalex03@gmail.com>
2021-10-23 11:19:06 -0700

    revert

--- examples/Typenet/config.py
+++
@@ -1,4 +0,0 @@
-num_types = 1077
-device = 'cpu'
-
-missing_types = ['NONE', '__skiing__ski_lift', '__food__beer_containment', '__exhibitions__exhibition_producer', '__book__scholarly_financial_support']
