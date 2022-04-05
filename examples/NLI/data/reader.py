1cf6dad04456d6798b6d427c24acc4ec22a3e315
Tanawan Premsro <btanawan2542@hotmail.com>
2022-04-04 23:19:35 -0400

    Swaping the order of roberta representation, hypothesis then premise

--- examples/NLI/data/reader.py
+++ examples/NLI/data/reader.py
@@ -2,11 +2,9 @@ import pandas as pd
 from sklearn.utils import resample
 
 
-def DataReader(file, size, *, batch_size = 16):
-    df = pd.read_csv(file)
+def DataReader(file, size):
+    df = pd.read_csv(file).dropna()
     return_data = []
-    count = 0
-    accumulate_data = {'premise': [], 'hypothesis': [], 'entailment': [], 'neutral': [], 'contradiction': []}
     sample = resample(df, n_samples=size, stratify=df['label']) if size != len(df) else df
     for index, item in sample.iterrows():
         data = {'premise': [item['premise']], 'hypothesis': [item['hypothesis']],
@@ -15,17 +13,37 @@ def DataReader(file, size, *, batch_size = 16):
                 'contradiction': 1 if item['label'] == 2 else 0
                 }
         return_data.append(data)
-    #     if count == batch_size or (len(accumulate_data['premise']) != 0 and accumulate_data['premise'][-1] != item['premise']):
-    #         return_data.append(accumulate_data)
-    #         count = 0
-    #         accumulate_data = {'premise': [], 'hypothesis': [], 'useful': [], 'useless': []}
-    #     accumulate_data['premise'].append(item['premise'])
-    #     accumulate_data['hypothesis'].append(item['hypothesis'])
-    #     accumulate_data['useful'].append(1 if item['label'] == 0 else 0)
-    #     accumulate_data['useless'].append(1 if item['label'] != 0 else 0)
-    #     count += 1
-    #
-    # if len(accumulate_data['premise']):
-    #     return_data.append(accumulate_data)
+    return return_data
 
+
+def DataReaderMulti(file, size, *, batch_size=8):
+    df = pd.read_csv(file).dropna()
+    return_data = []
+    sample = resample(df, n_samples=size, stratify=df['label']) if size != len(df) else df
+    # Doing the basic batch size without relationship, first
+    current_size = 0
+    data = {'premise': [], 'hypothesis': [], 'entailment': [], 'contradiction': [], 'neutral': []}
+    for index, item in sample.iterrows():
+        data['premise'].append(item['premise'])
+        data['hypothesis'].append(item['premise'])
+        data['entailment'].append('1' if item['label'] == 0 else '0')
+        data['neutral'].append('1' if item['label'] == 1 else '0')
+        data['contradiction'].append('1' if item['label'] == 2 else '0')
+        current_size += 1
+        if current_size == batch_size:
+            current_size = 0
+            return_data.append({"premises": "@@".join(data['premise']),
+                                "hypothesises": "@@".join(data['hypothesis']),
+                                "entailment_list": "@@".join(data['entailment']),
+                                "contradiction_list": "@@".join(data['contradiction']),
+                                "neutral_list": "@@".join(data['neutral'])})
+            # Reset data
+            for key in data.keys():
+                data[key] = []
+    if current_size != 0:
+        return_data.append({"premises": "@@".join(data['premise']),
+                            "hypothesises": "@@".join(data['hypothesis']),
+                            "entailment_list": "@@".join(data['entailment']),
+                            "contradiction_list": "@@".join(data['contradiction']),
+                            "neutral_list": "@@".join(data['neutral'])})
     return return_data
--- examples/NLI/main_group_sentences.py
+++ examples/NLI/main_group_sentences.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from data.reader import DataReader
+from data.reader import DataReaderMulti
 from transformers import AdamW
 import torch
 from regr.sensor.pytorch.sensors import ReaderSensor, ConcatSensor, FunctionalSensor, JointSensor
@@ -14,7 +14,7 @@ def program_declaration():
 
     graph.detach()
     sentence_group["premises"] = ReaderSensor(keyword="premises")
-    sentence_group["hypothesises"] = ReaderSensor(keyword="hypothesis")
+    sentence_group["hypothesises"] = ReaderSensor(keyword="hypothesises")
     sentence_group["entailment_list"] = ReaderSensor(keyword="entailment_list")
     sentence_group["contradiction_list"] = ReaderSensor(keyword="contradiction_list")
     sentence_group["neutral_list"] = ReaderSensor(keyword="neutral_list")
@@ -36,17 +36,18 @@ def program_declaration():
         JointSensor(sentence_group["premises"], sentence_group["hypothesises"], sentence_group["entailment_list"],
                     sentence_group["contradiction_list"], sentence_group["neutral_list"], forward=make_sentence)
 
-    sentence["token_ids", "Mask"] = JointSensor(sentence_group_contains, 'premise', "hypothesis",
+    sentence["token_ids", "Mask"] = JointSensor(sentence_group_contains, 'hypothesis', "premise",
                                                 forward=RobertaTokenizerMulti())
     roberta_model = NLI_Robert()
-    sentence["robert_emb"] = ModuleLearner("token_id", "Mask", module=roberta_model)
+    sentence["robert_emb"] = ModuleLearner("token_ids", "Mask", module=roberta_model)
 
     sentence[entailment] = ModuleLearner("robert_emb", module=RobertClassification(roberta_model.last_layer_size))
     sentence[neutral] = ModuleLearner("robert_emb", module=RobertClassification(roberta_model.last_layer_size))
     sentence[contradiction] = ModuleLearner("robert_emb", module=RobertClassification(roberta_model.last_layer_size))
-    sentence[entailment] = FunctionalSensor(sentence_group_contains, "entailment", forward=read_label, label=True)
-    sentence[neutral] = FunctionalSensor(sentence_group_contains, "neutral", forward=read_label, label=True)
-    sentence[contradiction] = FunctionalSensor(sentence_group_contains, "contradiction", forward=read_label, label=True)
+    sentence[entailment] = FunctionalSensor(sentence_group_contains, "entail_list", forward=read_label, label=True)
+    sentence[neutral] = FunctionalSensor(sentence_group_contains, "neutral_list", forward=read_label, label=True)
+    sentence[contradiction] = FunctionalSensor(sentence_group_contains, "cont_list", forward=read_label,
+                                               label=True)
 
     from regr.program import POIProgram, IMLProgram, SolverPOIProgram
     from regr.program.metric import MacroAverageTracker, PRF1Tracker, PRF1Tracker, DatanodeCMMetric
@@ -62,40 +63,49 @@ def program_declaration():
 
 
 def main(args):
-    from graph import sentence, entailment, neutral, contradiction
+    from graph_senetences import entailment, neutral, contradiction
 
     # Set the cuda number we want to use
     cuda_number = args.cuda_number
     cur_device = "cuda:" + str(cuda_number) if torch.cuda.is_available() else 'cpu'
-
-    test_dataset = DataReader(file="data/test.csv", size=args.testing_samples)
-    train_dataset = DataReader(file="data/train.csv", size=args.training_samples)
+    test_dataset = DataReaderMulti(file="data/test.csv", size=args.testing_samples, batch_size=args.batch_size)
+    train_dataset = DataReaderMulti(file="data/train.csv", size=args.training_samples,  batch_size=args.batch_size)
     model = program_declaration()
-    # model.train(train_dataset, test_set=test_dataset, train_epoch_num=args.cur_epoch,
-    #             Optim=lambda params: torch.optim.AdamW(params, lr=args.learning_rate), device=cur_device)
-    # model.test(test_dataset, device=cur_device)
-    #
-    # correct = 0
-    # index = 0
-    # result = {"premise": [data['premise'][0] for data in test_dataset],
-    #           "hypothesis": [data['hypothesis'][0] for data in test_dataset],
-    #           "actual": ['entailment' if data['entailment'] else 'neutral' if data['neutral'] else 'contrast' for data
-    #                      in test_dataset],
-    #           "predict": []}
-    # for datanode in model.populate(test_dataset):
-    #     #print(datanode)
-    #     #print("Entailment: ", datanode.getAttribute(entailment, 'ILP'))
-    #     #print("Neutral: ", datanode.getAttribute(neutral, 'ILP'))
-    #     #print("Contrast: ", datanode.getAttribute(contradiction, 'ILP'))
-    #     result["predict"].append('entailment' if datanode.getAttribute(entailment, 'ILP')
-    #                              else 'neutral' if datanode.getAttribute(neutral, 'ILP') else 'contrast')
-    #     correct += datanode.getAttribute(entailment, 'ILP') if result["actual"][index] == 'entailment' else \
-    #         datanode.getAttribute(neutral, 'ILP') if result["actual"][index] == 'neutral' else \
-    #             datanode.getAttribute(contradiction, 'ILP')
-    #     index += 1
-    # #print("Accuracy = %.2f%%" % (correct / index * 100))
-    # result = pd.DataFrame(result)
-    # result.to_csv("report-{:}-{:}-{:}.csv".format(args.training_samples, args.testing_samples, args.cur_epoch))
+    model.train(train_dataset, test_set=test_dataset, train_epoch_num=args.cur_epoch,
+                Optim=lambda params: torch.optim.AdamW(params, lr=args.learning_rate), device=cur_device)
+    model.test(test_dataset, device=cur_device)
+
+    correct = 0
+    index = 0
+    result = {"premise": [],
+              "hypothesis": [],
+              "actual": [],
+              "predict": []}
+    for datanode in model.populate(test_dataset, device=cur_device):
+        for sentence in datanode.getChildDataNodes():
+            # print(sentence.getAttribute)
+            # print("Actual")
+            # print(sentence.getAttribute(entailment, 'label'))
+            # print(sentence.getAttribute(neutral, 'label'))
+            # print(sentence.getAttribute(contradiction, 'label'))
+            #
+            # print("ILP")
+            # print(sentence.getAttribute(entailment, 'ILP'))
+            # print(sentence.getAttribute(neutral, 'ILP'))
+            # print(sentence.getAttribute(contradiction, 'ILP'))
+
+            result["premise"].append(sentence.getAttribute("premise"))
+            result["hypothesis"].append(sentence.getAttribute("hypothesis"))
+            result["actual"].append('entailment' if sentence.getAttribute(entailment, 'label')
+                                     else 'neutral' if sentence.getAttribute(neutral, 'label') else 'contrast')
+            result["predict"].append('entailment' if sentence.getAttribute(entailment, 'ILP')
+                                     else 'neutral' if sentence.getAttribute(neutral, 'ILP') else 'contrast')
+            correct += sentence.getAttribute(entailment, 'ILP') if sentence.getAttribute(entailment, 'label') else \
+                sentence.getAttribute(neutral, 'ILP') if sentence.getAttribute(neutral, 'label') else \
+                    sentence.getAttribute(contradiction, 'ILP')
+    print("Accuracy = %.2f%%" % (correct / len(result["predict"]) * 100))
+    result = pd.DataFrame(result)
+    result.to_csv("report-{:}-{:}-{:}.csv".format(args.training_samples, args.testing_samples, args.cur_epoch))
 
 
 if __name__ == "__main__":
@@ -104,9 +114,10 @@ if __name__ == "__main__":
     parser.add_argument('--epoch', dest='cur_epoch', default=10, help='number of epochs to train model', type=int)
     parser.add_argument('--lr', dest='learning_rate', default=1e-5, help='learning rate of the adamW optimiser',
                         type=float)
-    parser.add_argument('--training_sample', dest='training_samples', default=550152,
+    parser.add_argument('--training_sample', dest='training_samples', default=550146,
                         help="number of data to train model", type=int)
     parser.add_argument('--testing_sample', dest='testing_samples', default=10000, help="number of data to test model",
                         type=int)
+    parser.add_argument('--batch_size', dest='batch_size', default=8, help="batch size of sample", type=int)
     args = parser.parse_args()
     main(args)
