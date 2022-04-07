502023073510cf5b24ab9ca3127f0ca239f0a806
Tanawan Premsro <btanawan2542@hotmail.com>
2022-04-07 14:30:19 -0400

    Change back to the version to run original dataset

--- examples/NLI/main_group_sentences.py
+++ examples/NLI/main_group_sentences.py
@@ -86,14 +86,12 @@ def main(args):
     # Set the cuda number we want to use
     cuda_number = args.cuda_number
     cur_device = "cuda:" + str(cuda_number) if torch.cuda.is_available() else 'cpu'
-    training_file = "adver_nli_train.jsonl"
-    testing_file = "adver_nli_test.jsonl"
+    training_file = "train.csv" if not args.adver_data else "adver_nli_train.jsonl"
+    testing_file = "test.csv" if not args.adver_data else "adver_nli_test.jsonl"
     test_dataset = DataReaderMulti(file="data/" + testing_file, size=args.testing_samples,
-                                   batch_size=args.batch_size, adver_data_set=args.adver_data,
-                                   adver_file="data/snli_genadv_1000_test.jsonl")
+                                   batch_size=args.batch_size, adver_data_set=args.adver_data)
     train_dataset = DataReaderMulti(file="data/" + training_file, size=args.training_samples,
-                                    batch_size=args.batch_size, adver_data_set=args.adver_data,
-                                    adver_file="data/snli_genadv_1000_dev.jsonl")
+                                    batch_size=args.batch_size, adver_data_set=args.adver_data)
     model = program_declaration(cur_device)
     model.train(train_dataset, test_set=test_dataset, train_epoch_num=args.cur_epoch,
                 Optim=lambda params: torch.optim.AdamW(params, lr=args.learning_rate), device=cur_device)
@@ -121,7 +119,7 @@ def main(args):
             result["premise"].append(sentence.getAttribute("premise"))
             result["hypothesis"].append(sentence.getAttribute("hypothesis"))
             result["actual"].append('entailment' if sentence.getAttribute(entailment, 'label')
-                                     else 'neutral' if sentence.getAttribute(neutral, 'label') else 'contrast')
+                                    else 'neutral' if sentence.getAttribute(neutral, 'label') else 'contrast')
             result["predict"].append('entailment' if sentence.getAttribute(entailment, 'ILP')
                                      else 'neutral' if sentence.getAttribute(neutral, 'ILP') else 'contrast')
 
@@ -144,12 +142,12 @@ if __name__ == "__main__":
     parser.add_argument('--epoch', dest='cur_epoch', default=10, help='number of epochs to train model', type=int)
     parser.add_argument('--lr', dest='learning_rate', default=1e-6, help='learning rate of the adamW optimiser',
                         type=float)
-    parser.add_argument('--training_sample', dest='training_samples', default=10000,
+    parser.add_argument('--training_sample', dest='training_samples', default=550146,
                         help="number of data to train model", type=int)
     parser.add_argument('--testing_sample', dest='testing_samples', default=10000, help="number of data to test model",
                         type=int)
     parser.add_argument('--batch_size', dest='batch_size', default=4, help="batch size of sample", type=int)
-    parser.add_argument('--adver_data', dest='adver_data', default=1, help="use adversarial data set ot not",
+    parser.add_argument('--adver_data', dest='adver_data', default=0, help="Using adversarial data set ot not",
                         type=bool)
     args = parser.parse_args()
     main(args)
