7d8eac9ab4ec8f934050f20f7b0ce0acdafb1d08
AdmiralDarius <DariusSafavy@gmail.com>
2022-04-27 10:35:24 -0400

    add CIFAR100

---
+++ examples/CIFAR100/main.py
@@ -0,0 +1,131 @@
+import argparse
+import sys
+
+from regr.program.model.pytorch import SolverModel
+from regr.program.primaldualprogram import PrimalDualProgram
+
+sys.path.append('.')
+sys.path.append('../..')
+
+import torch
+from torchvision.models import resnet18
+
+from regr.program import SolverPOIProgram
+from regr.sensor.pytorch.sensors import ReaderSensor, JointSensor, FunctionalSensor
+from regr.sensor.pytorch.learners import ModuleLearner
+from reader import create_readers
+import torch.nn as nn
+from regr.program.metric import MacroAverageTracker, PRF1Tracker, DatanodeCMMetric
+from regr.program.loss import NBCrossEntropyLoss
+from graph import graph, image_group_contains,image,category,Label,image_group
+
+
+class ImageNetwork(torch.nn.Module):
+    def __init__(self):
+        super(ImageNetwork, self).__init__()
+        self.conv = resnet18(pretrained=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cuda', dest='cuda_number', default=0, help='cuda number to train the models on', type=int)
+    parser.add_argument('--solver', help='the model solver', default='primal_dual')
+    parser.add_argument('--samplenum', dest='samplenum', default=1000,help='number of samples to choose from the dataset',type=int)
+    parser.add_argument('--epochs', dest='epochs', default=5, help='number of training epoch', type=int)
+    parser.add_argument('--lambdaValue', dest='lambdaValue', default=0.5, help='value of learning rate', type=float)
+    parser.add_argument('--lr', dest='learning_rate', default=2e-4, help='learning rate of the adam optimiser',type=float)
+
+    args = parser.parse_args()
+
+    device = "cuda:"+str(args.cuda_number)
+
+    image_group['pixels_group'] = ReaderSensor(keyword='pixels', device=device)
+    image_group['category_group'] = ReaderSensor(keyword='corase_label', device=device)
+    image_group['tag_group'] = ReaderSensor(keyword='fine_label', device=device)
+
+    def str_to_int_list(x):
+        return torch.LongTensor([[int(i)] for i in x])
+
+    def make_images(pixels_group, category_group, tag_group):
+        return torch.ones((len(category_group.split("@@")), 1)), torch.squeeze(pixels_group, 0), str_to_int_list(
+            category_group.split("@@")), str_to_int_list(tag_group.split("@@"))
+
+    image[image_group_contains, "pixels", 'category_', "tag_"] = JointSensor(image_group['pixels_group'],
+                                                                             image_group["category_group"],
+                                                                             image_group["tag_group"],
+                                                                             forward=make_images)
+    def label_reader(_, label):
+        return label
+
+    image[category] = FunctionalSensor(image_group_contains, "category_", forward=label_reader, label=True)
+    image[Label] = FunctionalSensor(image_group_contains, "tag_", forward=label_reader, label=True)
+
+
+    image['emb'] = ModuleLearner('pixels', module=resnet18(pretrained=True))
+    image[category] = ModuleLearner('emb', module=nn.Linear(1000, 20))
+    image[Label] = ModuleLearner('emb', module=nn.Linear(1000, 100))
+    if args.solver=="poi":
+        print("POI")
+        program = SolverPOIProgram(graph,inferTypes=['ILP', 'local/argmax'], loss=MacroAverageTracker(NBCrossEntropyLoss())\
+                               ,metric={'ILP': PRF1Tracker(DatanodeCMMetric()),'softmax': PRF1Tracker(DatanodeCMMetric('local/argmax'))})
+    elif args.solver=="primal_dual":
+
+        print("PrimalDualProgram")
+        program = PrimalDualProgram(graph, SolverModel, poi=(image,), inferTypes=['ILP', 'local/argmax'],
+                                    loss=MacroAverageTracker(NBCrossEntropyLoss()),
+                                    metric={'ILP': PRF1Tracker(DatanodeCMMetric()),
+                                            'softmax': PRF1Tracker(DatanodeCMMetric('local/argmax'))},beta=args.lambdaValue)
+
+    train_reader,test_reader=create_readers(train_num=args.samplenum,test_num=args.samplenum)
+
+    program.train(train_reader, train_epoch_num=args.epochs, Optim=lambda param: torch.optim.SGD(param, lr=args.learning_rate),device=device)
+    guessed_tag = {
+        "local/softmax": [],
+        "ILP": []
+    }
+    real_tag = []
+    guessed_category = {
+        "local/softmax": [],
+        "ILP": []
+    }
+
+    real_category = []
+    for pic_num, picture_group in enumerate(program.populate(test_reader, device=device)):
+        for image_ in picture_group.getChildDataNodes():
+            for key in ["local/softmax", "ILP"]:
+                if key == "ILP":
+                    guessed_tag[key].append(int(torch.argmax(image_.getAttribute(Label, key))))
+                else:
+                    guessed_tag[key].append(int(torch.argmax(image_.getAttribute(Label, key))))
+
+                if key == "ILP":
+                    guessed_category[key].append(int(torch.argmax(image_.getAttribute(category, key))))
+                else:
+                    guessed_category[key].append(int(torch.argmax(image_.getAttribute(category, key))))
+            real_tag.append(int(image_.getAttribute(Label, "label")[0]))
+            real_category.append(int(image_.getAttribute(category, "label")))
+
+    for key in ["local/softmax", "ILP"]:
+        print(f"##############################{key}#########################")
+        guessed_labels = guessed_tag[key]
+        real_labels = real_tag
+        print(guessed_labels)
+        print(real_labels)
+        correct = sum(1 if x == y else 0 for x, y in zip(real_labels, guessed_labels))
+        total = len(real_labels)+1
+        print("tags accuracy", correct / total)
+        guessed_labels = guessed_category[key]
+        real_labels = real_category
+        print(guessed_labels)
+        print(real_labels)
+        correct = sum(1 if x == y else 0 for x, y in zip(real_labels, guessed_labels))
+        total = len(real_labels)+1
+        print("category accuracy", correct / total)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ examples/CIFAR100/reader.py
@@ -0,0 +1,74 @@
+import pickle,torch
+from torchvision import transforms
+
+preprocess = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+def create_readers(train_num=50000,test_num=10000,batch_size=32):
+    structure={}
+    def unpickle(file_name):
+        with open(file_name , 'rb') as fo:
+            dict = pickle.load(fo, encoding='bytes')
+        return dict
+
+    train_data = unpickle("cifar-100-python/train")
+    test_data = unpickle("cifar-100-python/test")
+    meta = unpickle("cifar-100-python/meta")
+
+    train_reader,test_reader=[],[]
+    batch_train_reader, batch_test_reader = [], []
+    coarse_names=meta[b'coarse_label_names']
+    fine_names = meta[b'fine_label_names']
+    for i in range(train_num):
+        instance={}
+        instance['pixels']=preprocess(train_data[b'data'][i].reshape((32,32,3)))
+        instance['corase_label'] = train_data[b'coarse_labels'][i]
+        instance['fine_label'] = train_data[b'fine_labels'][i]
+        instance['corase_label_name'] = coarse_names[(train_data[b'coarse_labels'][i])].decode('ascii')
+        instance['fine_label_name'] = fine_names[(train_data[b'fine_labels'][i])].decode('ascii')
+        if instance['corase_label_name'] in structure:
+            structure[instance['corase_label_name']].add(instance['fine_label_name'])
+        else:
+            structure[instance['corase_label_name']]=set()
+            structure[instance['corase_label_name']].add(instance['fine_label_name'])
+        train_reader.append(instance)
+
+    for i in range(0,train_num,batch_size):
+        pixels_group,coarse_names_group,fine_names_group=[],[],[]
+        for i_ in range(i,min(i+batch_size,train_num)):
+            pixels_group.append(train_reader[i_]['pixels'])
+            coarse_names_group.append(train_reader[i_]['corase_label'])
+            fine_names_group.append(train_reader[i_]['fine_label'])
+        intance={}
+        intance['pixels']=torch.stack(pixels_group, dim=0).unsqueeze(dim=0)
+        intance['corase_label']="@@".join([str(j) for j in coarse_names_group])
+        intance['fine_label']="@@".join([str(j) for j in fine_names_group])
+        batch_train_reader.append(intance)
+
+    for i in range(test_num):
+        instance={}
+        instance['pixels']=preprocess(test_data[b'data'][i].reshape((32,32,3)))
+        instance['corase_label'] = test_data[b'coarse_labels'][i]
+        instance['fine_label'] = test_data[b'fine_labels'][i]
+        instance['corase_label_name'] = coarse_names[(test_data[b'coarse_labels'][i])].decode('ascii')
+        instance['fine_label_name'] = fine_names[(test_data[b'fine_labels'][i])].decode('ascii')
+        test_reader.append(instance)
+
+    for i in range(0,train_num,batch_size):
+        pixels_group,coarse_names_group,fine_names_group=[],[],[]
+        for i_ in range(i,min(i+batch_size,train_num)):
+            pixels_group.append(test_reader[i_]['pixels'])
+            coarse_names_group.append(test_reader[i_]['corase_label'])
+            fine_names_group.append(test_reader[i_]['fine_label'])
+        intance={}
+        intance['pixels']=torch.stack(pixels_group, dim=0).unsqueeze(dim=0)
+        intance['corase_label']="@@".join([str(j) for j in coarse_names_group])
+        intance['fine_label']="@@".join([str(j) for j in fine_names_group])
+        batch_test_reader.append(intance)
+
+    return batch_train_reader,batch_test_reader
