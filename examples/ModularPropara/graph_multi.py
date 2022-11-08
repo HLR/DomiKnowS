f3cff3083b833f214196864473cdb620b8e5c345
Hossein Faghihi <rajabyfa@msu.edu>
2022-11-08 21:13:32 +0000

    updating the constraints

--- examples/ModularPropara/graph_multi.py
+++ examples/ModularPropara/graph_multi.py
@@ -148,3 +148,31 @@ with Graph('global') as graph:
                 )
             ), active = True
         )
+
+    ### If the action is move, then the location from step before should be different from the current step
+    ifL(
+        # action a1 is move, i is a1's step and e is action entity
+        andL(
+            action_label.move('a1'), 
+            step('i', path=('a1', action_step)),
+            entity('e', path=('a1', action_entity)),
+            entity_location_label('x', path=(('i', lstep.reversed), ('e', lentity.reversed)))
+            ), 
+        andL(
+            step('j', path=('i', ebefore_arg2.reversed, ebefore_arg1)),
+            notL(entity_location_label('y', path=(('j', lstep.reversed), ('e', lentity.reversed), ('x', llocation, llocation.reversed))))
+        ),
+        active = True
+    )
+    
+    ### There can only be one location for each entity at each step
+    ifL(
+        andL(
+            entity('e'),
+            step('i')
+        ),
+        atMostL( 
+            entity_location_label('x', path=(('i', lstep.reversed), ('e', lentity.reversed))), 1
+        ),
+        active = True
+    )
