1b36b3ff0b6f5dd3f27334565f7ad8f6cf41bc37
Andrzej Uszok <auszok@ihmc.org>
2023-03-08 00:04:02 -0600

    Updated version creation

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup, find_packages
 import os
 
-DomiKnowS_version = '0.400'
+DomiKnowS_version = '0.402'
 DomiKnowS_version_env = os.getenv('DomiKnowS_VERSION')
+DomiKnowS_VERSION_Postfix = os.getenv('DomiKnowS_VERSION_Postfix')
 
 # Override version if set in environment
 if DomiKnowS_version_env is not None:
     DomiKnowS_version = DomiKnowS_version_env[1:]
+    if DomiKnowS_VERSION_Postfix is not None:
+        DomiKnowS_version = DomiKnowS_version + "-" + DomiKnowS_VERSION_Postfix
     
-print("Using version: " + DomiKnowS_version)
+print("Using DomiKnowS version: " + DomiKnowS_version)
     
 setup(
     name='DomiKnowS',
     version=DomiKnowS_version,
     description='A library provides integration between Domain Knowledge and Deep Learning.',
 
-    long_description ='The library allows to specify a problem domain with a conceptual [graph](https://github.com/HLR/DomiKnowS/blob/main/docs/developer/KNOWLEDGE.md#graph) including declarations of edges and nodes, as well as [logical constraints](https://github.com/HLR/DomiKnowS/blob/main/docs/developer/KNOWLEDGE.md#constraints) on the graph concepts and relations. [Neural network](https://github.com/HLR/DomiKnowS/blob/main/docs/developer/MODEL.md#model-declaration) outputs bounded to the graph edges and nodes. The logical constraints are conveted to ILP and Gurobi Solver is used for inferencing. This adds a relational overlay over elements in a network that relates physical concepts in applications. <br> <br> The example runing in Google colab environment, presenting the usage of the libarary is [here](https://colab.research.google.com/drive/1FvdePHv3h3NDSTkBw1VKwAmaZFWuGgTi).',
+    long_description ='The library allows to specify a problem domain with a conceptual [graph](https://github.com/HLR/DomiKnowS/blob/main/docs/developer/KNOWLEDGE.md#graph) including declarations of edges and nodes, as well as [logical constraints](https://github.com/HLR/DomiKnowS/blob/main/docs/developer/KNOWLEDGE.md#constraints) on the graph concepts and relations. [Neural network](https://github.com/HLR/DomiKnowS/blob/main/docs/developer/MODEL.md#model-declaration) outputs bounded to the graph edges and nodes. The logical constraints are converted to ILP and Gurobi Solver is used for inferring. This adds a relational overlay over elements in a network that relates physical concepts in applications. <br> <br> The example running in Google CoLab environment, presenting the usage of the library is [here](https://colab.research.google.com/drive/1FvdePHv3h3NDSTkBw1VKwAmaZFWuGgTi).',
     long_description_content_type='text/markdown',
     
     url='https://github.com/HLR/DomiKnowS',
