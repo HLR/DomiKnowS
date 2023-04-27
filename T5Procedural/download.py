76112cf964be45a2ede18dfe46fbb6e48258f61e
Hossein Rajaby Faghihi <rajabyfa@msu.edu>
2023-04-27 04:22:41 +0000

    fixing the code to download data

--- T5Procedural/download.py
+++ T5Procedural/download.py
@@ -1,36 +1,18 @@
-import requests
-
-def download_file_from_gdrive(file_id, destination_file):
-    URL = f"https://drive.google.com/uc?export=download&id={file_id}"
-    session = requests.Session()
-    response = session.get(URL, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {"id": file_id, "confirm": token}
-        response = session.get(URL, params=params, stream=True)
-
-    save_response_content(response, destination_file)
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith("download_warning"):
-            return value
-    return None
-
-def save_response_content(response, destination_file):
-    CHUNK_SIZE = 32768
-    with open(destination_file, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:
-                f.write(chunk)
+import os
+import gdown
 
 # Set the Google Drive file ID
-file_id = "1MUHUl0lmcVr4usXrZ_fm-YEbj0TfRoLJ"
+file_id = '1Ep2EBPSbY1fVE7SegXcv3nrUpGfzHAC5'
 
-# Set the destination file name
-destination_file = "data/prepared_results.pt"
+# Set the destination directory and file name
+destination_dir = 'data'
+destination_file = os.path.join(destination_dir, 'prepared_results.pt')
 
-# Download the file
-download_file_from_gdrive(file_id, destination_file)
-print("File downloaded successfully.")
+# Create the destination directory if it doesn't exist
+if not os.path.exists(destination_dir):
+    os.makedirs(destination_dir)
+
+# Download the file using gdown
+url = f'https://drive.google.com/uc?id={file_id}'
+gdown.download(url, output=destination_file, quiet=False)
+print('File downloaded successfully.')
