337d004c3225a6cbb223bf0596555100d015170b
AdmiralDarius <39480854+AdmiralDarius@users.noreply.github.com>
2023-10-20 15:32:18 -0400

    fix mnist binary graph graph.py

--- MNIST_binary/graph.py
+++ MNIST_binary/graph.py
@@ -31,11 +31,5 @@ with Graph('MNISTGraph') as graph:
     Eight = image(name='eighthandwriting')
     Nine = image(name='ninehandwriting')
 
-
     Numbers=[Zero,One,Two,Three,Four,Five,Six,Seven,Eight,Nine]
-    #for l1, l2 in combinations(Numbers, 2):
-    #    nandL(l1, l2)
-    #disjoint(*Numbers)
-    ifL(image("x"),exactL(Zero("x"),One("x"),Two("x"),Three("x"),Four("x"),Five("x"),Six("x"),Seven("x"),Eight("x"),Nine("x")), active = False)
-    
-    exactL(Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, active = True)
+    exactL(*Numbers)
