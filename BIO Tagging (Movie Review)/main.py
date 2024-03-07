d8d3a4cfcf5dda000bb0110b59967365ea795c98
Aliakbar Nafar <39480854+AdmiralDarius@users.noreply.github.com>
2024-03-07 17:35:16 -0500

    Update main.py

--- BIO Tagging (Movie Review)/main.py
+++ BIO Tagging (Movie Review)/main.py
@@ -1,5 +1,5 @@
 from domiknows.sensor.pytorch.sensors import ReaderSensor, JointSensor, FunctionalSensor
-from graph import graph,NERSentence,tag,NER_contains,Generated_label#,next_word,first_word, second_word
+from graph import graph,NERSentence,tag,NER_contains,Generated_label,next_word,first_word, second_word
 from reader import parse_corpus,encode_examples
 from transformers import T5ForConditionalGeneration, AdamW
 from transformers import T5Tokenizer
@@ -11,6 +11,7 @@ from domiknows.sensor.pytorch.learners import ModuleLearner
 from domiknows.program.metric import MacroAverageTracker, PRF1Tracker, DatanodeCMMetric
 from domiknows.program.loss import NBCrossEntropyLoss, BCEWithLogitsIMLoss
 import logging
+from domiknows.sensor.pytorch.relation_sensors import CompositionCandidateSensor
 logging.basicConfig(level=logging.INFO)
 
 from model import FilteredT5Model
@@ -18,6 +19,7 @@ model_name='t5-small'
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 new_tokens = ['<B>', '<I>', '<O>']
 tokenizer.add_tokens(new_tokens)
+print(tokenizer.convert_tokens_to_ids(new_tokens))
 reader_data=parse_corpus(file_path="trivia10k13train.bio",for_domiknows=True,samplenum=100)
 
 device='cuda:1'
@@ -35,7 +37,14 @@ tag[Generated_label] = FunctionalSensor(NER_contains,"Generated_label_", forward
 tag[Generated_label] = ModuleLearner(NER_contains,NERSentence['sentence_ids'],tag["Generated_label_"], module=FilteredT5Model(model_name, tokenizer))
 
 
-program = SolverPOIProgram(graph, inferTypes=['local/argmax'], loss=MacroAverageTracker(NBCrossEntropyLoss()),
-                           metric={'argmax': PRF1Tracker(DatanodeCMMetric('local/argmax'))})
+def link_words(tags):
+    n=len(tags)
+    return torch.eye(n,dtype=torch.long),torch.nn.functional.pad(torch.eye(n-1), (1, 0, 0, 1), "constant", 0)
+
+next_word[first_word.reversed, second_word.reversed] = JointSensor(tag, forward=link_words)
+
+
+program = SolverPOIProgram(graph,poi=[tag[Generated_label],next_word], inferTypes=['local/argmax',"ILP"], loss=MacroAverageTracker(NBCrossEntropyLoss()),
+                           metric={'ILP': PRF1Tracker(DatanodeCMMetric()),'argmax': PRF1Tracker(DatanodeCMMetric('local/argmax'))})
 
 program.train(reader_data,valid_set=reader_data, train_epoch_num=10, Optim=lambda param: AdamW(param, lr=1e-4),device=device)
