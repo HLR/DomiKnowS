c825ee8de3697f0d20186a8e545dd588630cb116
Aliakbar Nafar <39480854+DariusNafar@users.noreply.github.com>
2024-06-21 13:16:56 -0400

    Update main.py

--- graph_coloring/main.py
+++ graph_coloring/main.py
@@ -1,82 +1,55 @@
-import sys
-import torch
-
-sys.path.append('.')
-sys.path.append('../..')
-
+import sys, torch, argparse
 from domiknows.sensor.pytorch.sensors import ReaderSensor
 from domiknows.sensor.pytorch.relation_sensors import EdgeSensor, CompositionCandidateReaderSensor
 from domiknows.program import SolverPOIProgram
+from reader import CityReader, DummyCityLearner
+from domiknows.sensor.pytorch.learners import TorchLearner
+from graph import get_graph
+parser = argparse.ArgumentParser(description='Check a graphical structure and existL constraint in domiknows')
+parser.add_argument('--firestations', dest='firestations', default=[], help='list of cities to be defauled as firestation from 1 to 9', type=int, nargs='+')
+parser.add_argument('--constraint', dest='constraint',default="existL", choices=["orL", "existL","ifLnotLexistL","orLnotLexistL"], help="Choose a constraint")
+args = parser.parse_args()
 
-from graph import graph, world, city, world_contains_city, neighbor, city1, city2, firestationCity
-from sensors import DummyCityLearner
+graph, world, city, world_contains_city, neighbor, city1, city2, firestationCity = get_graph(args.constraint)
+#graph.detach()
 
-graph.detach()
+class DummyCityLearner(TorchLearner):
+    def __init__(self, *pre, fire_stations=[i+1 for i in range(9)]):
+        TorchLearner.__init__(self,*pre)
+        self.fire_stations=fire_stations
 
-def model_declaration():
+    def forward(self, x):
+        result = torch.zeros(len(x), 2)
+        result[:, 1] = -1000
+        for i in self.fire_stations:
+            result[i-1,1]=1000
+        return result
 
-    # --- City
-    world['index'] = ReaderSensor(keyword='world')
-    city['index'] = ReaderSensor(keyword='city')
-    city[world_contains_city] = EdgeSensor(city['index'], world['index'], relation=world_contains_city, forward=lambda x, _: torch.ones_like(x).unsqueeze(-1))
+dataset = CityReader().run() 
+world['index'] = ReaderSensor(keyword='world')
+city['index'] = ReaderSensor(keyword='city')
+city[world_contains_city] = EdgeSensor(city['index'], world['index'], relation=world_contains_city, forward=lambda x, _: torch.ones_like(x).unsqueeze(-1))
 
-    def readNeighbors(index, data, arg1, arg2):
-        city1, city2 = arg1, arg2
-        if city1.getAttribute('index') in data[int(city2.getAttribute('index'))]:
-            return True
-        else:
-            return False
+def readNeighbors(index, data, arg1, arg2):
+    city1, city2 = arg1, arg2
+    if city1.getAttribute('index') in data[int(city2.getAttribute('index'))]:
+        return True
+    else:
+        return False
 
-    neighbor[city1.reversed, city2.reversed] = CompositionCandidateReaderSensor(city['index'], keyword='links', relations=(city1.reversed, city2.reversed), forward=readNeighbors)
+neighbor[city1.reversed, city2.reversed] = CompositionCandidateReaderSensor(city['index'], keyword='links', relations=(city1.reversed, city2.reversed), forward=readNeighbors)
 
-    # --- Learners
-    city[firestationCity] = DummyCityLearner('index')
+city[firestationCity] = DummyCityLearner('index',fire_stations=list(args.firestations))
+program = SolverPOIProgram(graph, poi=[world, city, city[firestationCity], neighbor])
+
+for datanode in program.populate(dataset=dataset):
     
-    program = SolverPOIProgram(graph, poi=[world, city, city[firestationCity], neighbor])
-    return program
+    print("before inference")
+    print("city index:     :",list(range(1,10,1)))
+    print("is firestation? :", [int(child_node.getAttribute('<' + firestationCity.name + '>',"local/softmax")[1].item()) for child_node in datanode.getChildDataNodes()])
 
-
-
-def main():
-    from reader import CityReader
-    from graph import city, neighbor, firestationCity
-
-    lbp = model_declaration()
-
-    dataset = CityReader().run()  # Adding the info on the reader
-
-    for datanode in lbp.populate(dataset=dataset):
-        
-        print("before inference")
-        for child_node in datanode.getChildDataNodes():
-            print("city index: ", child_node.getAttribute("index"))
-            print("is firestation? : ", child_node.getAttribute('<' + firestationCity.name + '>')[1])
-
-        # call solver
-        conceptsRelations = (firestationCity)  
-        datanode.inferILPResults(*conceptsRelations, fun=None, minimizeObjective=True) 
-
-        result = []
-        for child_node in datanode.getChildDataNodes():
-            s = child_node.getAttribute('index')
-            f = child_node.getAttribute(firestationCity, 'ILP').item()
-            if f > 0:
-                r = (s, True)
-            else:
-                r = (s, False)
-            result.append(r)
-        
-        print("after inference")
-        for child_index, child_node in enumerate(datanode.getChildDataNodes()):
-            print("city index: ", child_node.getAttribute("index"))
-            print("is firestation? : ", child_node.getAttribute(firestationCity, 'ILP').item())
-            if child_index + 1 == 1:
-                assert child_node.getAttribute(firestationCity, 'ILP').item() == 1
-            elif child_index + 1 == 6:
-                assert child_node.getAttribute(firestationCity, 'ILP').item() == 1
-            else:
-                assert child_node.getAttribute(firestationCity, 'ILP').item() == 0
-
-
-if __name__ == '__main__':
-    main()
+    datanode.inferILPResults() 
+    
+    print("\nafter inference")
+    print("city index:     :",list(range(1,10,1)))
+    print("is firestation? :", [int(child_node.getAttribute('<' + firestationCity.name + '>',"ILP").item()) for child_node in datanode.getChildDataNodes()])
