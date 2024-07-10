a719c903066878df5424966b389c9194c63b1fa2
Aliakbar Nafar <39480854+DariusNafar@users.noreply.github.com>
2024-07-10 19:01:16 -0400

    Update main.py

--- graph_coloring/main.py
+++ graph_coloring/main.py
@@ -1,17 +1,32 @@
-import sys, torch, argparse
+
+import sys
+sys.path.append('../../../../domiknows/')
+from domiknows.graph import Graph
+from domiknows.sensor import Sensor
+import torch, argparse, time
 from domiknows.sensor.pytorch.sensors import ReaderSensor
 from domiknows.sensor.pytorch.relation_sensors import EdgeSensor, CompositionCandidateReaderSensor
 from domiknows.program import SolverPOIProgram
-from reader import CityReader
+
 from domiknows.sensor.pytorch.learners import TorchLearner
 from graph import get_graph
+
+
+Sensor.clear()
+
 parser = argparse.ArgumentParser(description='Check a graphical structure and existL constraint in domiknows')
-parser.add_argument('--firestations', dest='firestations', default=[], help='list of cities to be defauled as firestation from 1 to 9', type=int, nargs='+')
-parser.add_argument('--constraint', dest='constraint',default="existL", choices=["orL", "existL","ifLnotLexistL","orLnotLexistL"], help="Choose a constraint")
+parser.add_argument('--firestations', dest='firestations', default=[4], help='list of cities to be defauled as firestation from 1 to 9', type=int, nargs='+')
+parser.add_argument('--constraint', dest='constraint',default="None", choices=["None","orL", "existL","ifLnotLexistL","orLnotLexistL"], help="Choose a constraint")
+parser.add_argument('--atmostaL', dest='atmostaL',default=10,type=int)
+parser.add_argument('--atleastaL', dest='atleastaL',default=3,type=int)
+
 args = parser.parse_args()
 
-graph, world, city, world_contains_city, neighbor, city1, city2, firestationCity = get_graph(args.constraint)
-#graph.detach()
+graph, world, city, world_contains_city, neighbor, city1, city2, firestationCity = get_graph(args.constraint,args.atmostaL,args.atleastaL)
+
+
+#Graph.clear()
+#Sensor.clear()
 
 class DummyCityLearner(TorchLearner):
     def __init__(self, *pre, fire_stations=[i+1 for i in range(9)]):
@@ -25,7 +40,7 @@ class DummyCityLearner(TorchLearner):
             result[i-1,1]=1000
         return result
 
-dataset = CityReader().run() 
+dataset = [{'world': [0], 'city': [1, 2, 3, 4, 5, 6, 7, 8, 9], 'links': {1: [2, 3, 4, 5], 2: [1, 6], 3: [1], 4: [1], 5: [1], 6: [2, 7, 8, 9], 7: [6], 8: [6], 9: [6]}}]
 world['index'] = ReaderSensor(keyword='world')
 city['index'] = ReaderSensor(keyword='city')
 city[world_contains_city] = EdgeSensor(city['index'], world['index'], relation=world_contains_city, forward=lambda x, _: torch.ones_like(x).unsqueeze(-1))
@@ -39,11 +54,12 @@ def readNeighbors(index, data, arg1, arg2):
 
 neighbor[city1.reversed, city2.reversed] = CompositionCandidateReaderSensor(city['index'], keyword='links', relations=(city1.reversed, city2.reversed), forward=readNeighbors)
 
+
 city[firestationCity] = DummyCityLearner('index',fire_stations=list(args.firestations))
 program = SolverPOIProgram(graph, poi=[world, city, city[firestationCity], neighbor])
 
-for datanode in program.populate(dataset=dataset):
-    
+for datanode in program.populate(dataset=dataset):    
+
     print("before inference")
     print("city index:     :",list(range(1,10,1)))
     print("is firestation? :", [int(child_node.getAttribute('<' + firestationCity.name + '>',"local/softmax")[1].item()) for child_node in datanode.getChildDataNodes()])
