d1489fdb4091f25c4f725a370de2b466582bf43d
Alexander Wan <alexwan@berkeley.edu>
2024-07-15 20:20:19 -0400

    add incorrect test cases

--- test_regr/examples/orbs/graph.py
+++ test_regr/examples/orbs/graph.py
@@ -1,5 +1,5 @@
 from domiknows.graph import Graph, Concept, Relation
-from domiknows.graph.logicalConstrain import orL, existsL, ifL, notL, andL, atMostL, atLeastL
+from domiknows.graph.logicalConstrain import orL, existsL, ifL, notL, andL, atMostL, atLeastL, atLeastAL, exactL
 
 Graph.clear()
 Concept.clear()
@@ -37,6 +37,42 @@ def get_graph(constraint,atmost,atleast):
         elif constraint=="foreach_bag_existsL_notL": # try with --colored
             ifL(csp_range("x"), 
                 existsL(notL(colored_orbs("y",path=("x",csp_range_contains_orbs))))
+            )
+        elif constraint=="atleastAL_notatmostL":
+            # 1) there must be at least 5 colored orbs total
+            # 2) there can NOT be more than 1 colored orbs in each bag
+
+            atLeastAL(colored_orbs, 5)
+
+            ifL(csp_range("x"),
+                notL(
+                    atMostL(colored_orbs("y",path=("x",csp_range_contains_orbs)), 1)
+                )
+            )
+        elif constraint=='atleastAL_notexistL':
+            # 1) there must be at least 5 colored orbs total
+            # 2) there can NOT be exactly 5 colored orbs in each bag
+
+            atLeastAL(colored_orbs, 5)
+
+            ifL(csp_range("x"),
+                notL(
+                    exactL(colored_orbs("y",path=("x",csp_range_contains_orbs)), 5)
+                )
+            )
+        elif constraint=="ifL_atMostL_infeasible":
+            print(constraint)
+            # 1) there must be at least 5 colored orbs total
+            # 2) if there is a bag with <= 2 colored orbs, then that bag must have >= 4 colored orbs
+            # This should be infeasible.
+
+            atLeastAL(colored_orbs, 5)
+
+            ifL(csp_range("x"),
+                ifL(
+                    atMostL(colored_orbs("y",path=("x",csp_range_contains_orbs)), 2),
+                    atLeastL(colored_orbs("z",path=("x",csp_range_contains_orbs)), 4)
+                )
             )
         else:
             print("no contraint")
