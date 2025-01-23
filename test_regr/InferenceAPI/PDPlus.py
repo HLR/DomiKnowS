55dc972333037bda2708cab4782c3c35d7823f62
DariusNafar <DariusSafavy@gmail.com>
2025-01-23 18:30:45 -0500

    finalize the graph

--- test_regr/InferenceAPI/PDPlus.py
+++ test_regr/InferenceAPI/PDPlus.py
@@ -1,12 +1,10 @@
-import logging
+import logging, torch
 from domiknows.graph import Graph, Concept, Relation, andL, orL
 from NER_utils import generate_dataset, reader_format
 from domiknows.program.lossprogram import PrimalDualProgram
 from domiknows.program.model.pytorch import SolverModel
 from domiknows.sensor.pytorch import ModuleSensor
 from domiknows.sensor.pytorch.sensors import ReaderSensor, JointSensor
-
-import torch
 
 logging.basicConfig(level=logging.INFO)
 Graph.clear()
@@ -15,60 +13,75 @@ Relation.clear()
 
 with Graph(name='global') as graph:
 
-    people = Concept(name='people')
-    p1 = people(name='p1')
-    p2 = people(name='p2')
-    p3 = people(name='p3')
+    #people = Concept(name='people')
+    p1 = Concept(name='p1')
+    p2 = Concept(name='p2')
+    p3 = Concept(name='p3')
+    p1_is_real = p1(name='p1isreal')
+    p2_is_real = p2(name='p2isreal')
+    p3_is_real = p3(name='p3isreal')
 
-    location = Concept(name='location')
-    l1 = location(name='l1')
-    l2 = location(name='l2')
-    l3 = location(name='l3')
+    #location = Concept(name='location')
+    l1 = Concept(name='l1')
+    l2 = Concept(name='l2')
+    l3 = Concept(name='l3')
 
-    pair = Concept(name="pair")
-    people_arg1,location_arg1 = pair.has_a(people_arg1=people, location_arg1=location)
-    work_in = pair(name="work_in")
+    pair1 = Concept(name="pair1")
+    people_arg1,location_arg1 = pair1.has_a(people_arg1=p1, location_arg1=l1)
+    pair2 = Concept(name="pair2")
+    people_arg2, location_arg2 = pair2.has_a(people_arg2=p2, location_arg2=l2)
+    pair3 = Concept(name="pair3")
+    people_arg3, location_arg3 = pair3.has_a(people_arg3=p3, location_arg3=l3)
 
-    andL(andL(p1("x"),work_in("z",path=('x', people_arg1.reversed))),andL(p2("y"),work_in("t",path=('y', people_arg1.reversed))))
-    orL(andL(p2("x"),work_in("z",path=('x', people_arg1.reversed))),andL(p3("y"),work_in("t",path=('y', people_arg1.reversed))))
+    work_in1 = pair1(name="workin1")
+    work_in2 = pair2(name="workin2")
+    work_in3 = pair3(name="workin3")
+
+    andL(andL(p1_is_real("x"),work_in1("z",path=('x', people_arg1.reversed))),andL(p2_is_real("y"),work_in2("t",path=('y', people_arg2.reversed))))
+    orL(andL(p2_is_real("x"),work_in2("z",path=('x', people_arg2.reversed))),andL(p3_is_real("y"),work_in3("t",path=('y', people_arg3.reversed))))
 
 data_list = generate_dataset(sample_num=100)
 data_list = reader_format(data_list)
 
-location["embedding1"] = ReaderSensor(keyword="location1")
-location["embedding2"] = ReaderSensor(keyword="location2")
-location["embedding3"] = ReaderSensor(keyword="location3")
-people["embedding1"] = ReaderSensor(keyword="person1")
-people["embedding2"] = ReaderSensor(keyword="person2")
-people["embedding3"] = ReaderSensor(keyword="person3")
+p1["embedding"] = ReaderSensor(keyword="person1")
+p2["embedding"] = ReaderSensor(keyword="person2")
+p3["embedding"] = ReaderSensor(keyword="person3")
 
 class PeopleModel(torch.nn.Module):
     def __init__(self, size):
         super().__init__()
         self.size = size
-        self.layer = torch.nn.Linear(self.size, 1)
+        self.layer = torch.nn.Linear(self.size, 2)
 
     def forward(self, p):
-        return self.layer(p[0])
+        return self.layer(p[0]).unsqueeze(0)
 
 is_real_person=PeopleModel(2)
-people[p1] = ModuleSensor(people["embedding1"],module=is_real_person,device="cpu")
-people[p2] = ModuleSensor(people["embedding2"],module=is_real_person,device="cpu")
-people[p3] = ModuleSensor(people["embedding3"],module=is_real_person,device="cpu")
+p1[p1_is_real] = ModuleSensor(p1["embedding"],module=is_real_person,device="cpu")
+p2[p2_is_real] = ModuleSensor(p2["embedding"],module=is_real_person,device="cpu")
+p3[p3_is_real] = ModuleSensor(p3["embedding"],module=is_real_person,device="cpu")
+
+l1["embedding"] = ReaderSensor(keyword="location1")
+l2["embedding"] = ReaderSensor(keyword="location2")
+l3["embedding"] = ReaderSensor(keyword="location3")
 
 class WorkModel(torch.nn.Module):
     def __init__(self, size):
         super().__init__()
         self.size = size
-        self.layer = torch.nn.Linear(self.size, 1)
+        self.layer = torch.nn.Linear(self.size, 2)
 
     def forward(self, p, l):
-        return self.layer(torch.cat([p[0], l[0]]))
+        return self.layer(torch.cat([p[0], l[0]])).unsqueeze(0)
+
 
 work_in_model=WorkModel(4)
-pair[people_arg1.reversed,location_arg1.reversed] = JointSensor(forward=lambda : (torch.ones(1,1),torch.ones(1,1)))
-#pair[work_in] = ModuleSensor(#,module=work_in_model,device="cpu")
+pair1[people_arg1.reversed,location_arg1.reversed] = JointSensor(p1["embedding"],l1["embedding"],forward=lambda *_ : (torch.ones(1,1),torch.ones(1,1)))
+pair1[work_in1] = ModuleSensor(p1["embedding"],l1["embedding"],module=work_in_model,device="cpu")
+pair2[people_arg2.reversed,location_arg2.reversed] = JointSensor(p2["embedding"],l2["embedding"],forward=lambda *_ : (torch.ones(1,1),torch.ones(1,1)))
+pair2[work_in2] = ModuleSensor(p2["embedding"],l2["embedding"],module=work_in_model,device="cpu")
+pair3[people_arg3.reversed,location_arg3.reversed] = JointSensor(p3["embedding"],l3["embedding"],forward=lambda *_ : (torch.ones(1,1),torch.ones(1,1)))
+pair3[work_in3] = ModuleSensor(p3["embedding"],l3["embedding"],module=work_in_model,device="cpu")
 
-
-program = PrimalDualProgram(graph,SolverModel,poi=[people,location,pair],device="cpu")
-program.train(data_list, epochs=10, lr=0.001)
+program = PrimalDualProgram(graph,SolverModel,poi=[p1,p2,p3,l1,l2,l3,pair1,pair2,pair3],device="cpu")
+program.train(data_list, epochs=1, lr=0.001)
