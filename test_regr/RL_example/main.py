97bb35cfe40662a15f43c7d168947aaeea1d4b80
DariusNafar <DariusSafavy@gmail.com>
2025-03-06 14:31:39 -0500

    Initial RL example outside domiknows and its graph inside domiknows

--- test_regr/RL_example/main.py
+++ test_regr/RL_example/main.py
@@ -1,37 +1,22 @@
-from domiknows.graph import Graph, Concept, Relation
-from domiknows.graph.logicalConstrain import orL, existsL, ifL, notL, andL, atMostL, atLeastL, atMostAL, atLeastAL
-from domiknows.graph.concept import EnumConcept
+from graph import get_graph
+from domiknows.program.lossprogram import PrimalDualProgram
+from domiknows.sensor.pytorch.sensors import ReaderSensor, FunctionalSensor
+from domiknows.program.model.pytorch import SolverModel
+from domiknows.sensor.pytorch import ModuleLearner
+grid_size=3
+max_steps=12
 
-Graph.clear()
-Concept.clear()
-Relation.clear()
+graph, R_Location, F_Location, Location_status , Location, Status = get_graph(grid_size=grid_size,max_steps=max_steps)
 
+reader = {'InitialGrid':[[0,1,2,0,1,0,0,0,3]]}
 
-def get_graph(grid_size=3,max_steps=12):
+Status["InitialGrid"] = ReaderSensor(keyword='InitialGrid')
+for i in range(grid_size*grid_size):
+    Status[Location_status[i]] = FunctionalSensor(Status["InitialGrid"], forward=lambda x: x[0][i])
+for step in range(max_steps):
+    Location[R_Location[step]] = ModuleLearner(module=lambda x: x)
+    Location[F_Location[step]] = ModuleLearner(module=lambda x: x)
 
-    with Graph('global') as graph:
-        R_Location, F_Location, Location_status = [None]*max_steps, [None]*max_steps, [None]*max_steps
-        for step in range(max_steps):
-            R_Location[step] = Concept(name=f'RescueHelicpter_Location_{step}', ConceptClass=EnumConcept,values=[f"{str(i)}" for i in range(grid_size*grid_size)])
-            F_Location[step] = Concept(name=f'FireHelicpter_Location_{step}', ConceptClass=EnumConcept,values=[f"{str(i)}" for i in range(grid_size*grid_size)])
-
-        for i in range(grid_size*grid_size):
-            Location_status[i] = Concept(name=f'Location_status_{step}', ConceptClass=EnumConcept,values=['Clear','Fire','Rescue','Fire&Rescue','NotSetYet'])
-
-        # Rescue Helicopter cant goto fire location
-        for step in range(max_steps):
-            for i in range(grid_size*grid_size):
-                notL(andL(getattr(R_Location[step], f"{str(i)}" ), getattr(Location_status[step], 'Fire')))
-                notL(andL(getattr(R_Location[step], f"{str(i)}"), getattr(Location_status[step], 'Fire&Rescue')))
-
-        # Fire Helicopter and Rescue Helicopter cant be too far (manhatan distance of 2)
-        for step in range(max_steps):
-            for i in range(grid_size*grid_size):
-                for j in range(grid_size * grid_size):
-                    if abs(i//grid_size - j//grid_size) + abs(i%grid_size - j%grid_size) > 2:
-                        notL(andL(getattr(R_Location[step], f"{str(i)}"), getattr(F_Location[step], f"{str(j)}")))
-
-
-
-    return graph
-
+poi = [Location[F_Location[i]] for i in range(max_steps)] + [Location[R_Location[i]] for i in range(max_steps)] + [Status[Location_status[i]] for i in range(grid_size*grid_size)]
+program = PrimalDualProgram(graph,SolverModel, poi=poi)
+program.train(reader,epochs=10, lr=0.001)
