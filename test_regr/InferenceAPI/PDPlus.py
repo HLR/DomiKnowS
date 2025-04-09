0175013cfad9733110de66cec3c39782207ad5af
DariusNafar <DariusSafavy@gmail.com>
2025-04-09 11:53:49 -0400

    Fix the disconnected datanode issue

--- test_regr/InferenceAPI/PDPlus.py
+++ test_regr/InferenceAPI/PDPlus.py
@@ -2,7 +2,7 @@ import logging, torch
 from domiknows.graph import Graph, Concept, Relation, andL, orL
 from NER_utils import generate_dataset, reader_format
 from domiknows.program.loss import NBCrossEntropyLoss
-from domiknows.program.lossprogram import InferenceProgram
+from domiknows.program.lossprogram import InferenceProgram, PrimalDualProgram
 from domiknows.program.model.pytorch import SolverModel
 from domiknows.sensor.pytorch import ModuleSensor, FunctionalSensor
 from domiknows.sensor.pytorch.sensors import ReaderSensor, JointSensor
@@ -15,38 +15,36 @@ Relation.clear()
 
 with Graph(name='global') as graph:
 
-    p1 = Concept(name='p1')
-    p2 = Concept(name='p2')
-    p3 = Concept(name='p3')
+    person = Concept(name='person')
 
-    p1_is_real = p1(name='p1isreal')
-    p2_is_real = p2(name='p2isreal')
-    p3_is_real = p3(name='p3isreal')
+    p1 = person(name='p1')
+    p2 = person(name='p2')
+    p3 = person(name='p3')
 
-    l1 = Concept(name='l1')
-    l2 = Concept(name='l2')
-    l3 = Concept(name='l3')
+    location = Concept(name='location')
 
-    pair1 = Concept(name="pair1")
-    people_arg1,location_arg1 = pair1.has_a(people_arg1=p1, location_arg1=l1)
-    pair2 = Concept(name="pair2")
-    people_arg2, location_arg2 = pair2.has_a(people_arg2=p2, location_arg2=l2)
-    pair3 = Concept(name="pair3")
-    people_arg3, location_arg3 = pair3.has_a(people_arg3=p3, location_arg3=l3)
+    l1 = location(name='l1')
+    l2 = location(name='l2')
+    l3 = location(name='l3')
 
-    work_in1 = pair1(name="workin1")
-    work_in2 = pair2(name="workin2")
-    work_in3 = pair3(name="workin3")
+    pair = Concept(name="pair")
+    people_arg,location_arg = pair.has_a(people_arg=person, location_arg=location)
 
-    constarint1 = andL(andL(p1_is_real("x"),work_in1("z",path=('x', people_arg1.reversed))),andL(p2_is_real("y"),work_in2("t",path=('y', people_arg2.reversed))))
-    constarint2 = orL(andL(p2_is_real("x"),work_in2("z",path=('x', people_arg2.reversed))),andL(p3_is_real("y"),work_in3("t",path=('y', people_arg3.reversed))))
+    work_in1 = pair(name="workin1")
+    work_in2 = pair(name="workin2")
+    work_in3 = pair(name="workin3")
+
+    constarint1 = andL(andL(p1("x"),work_in1("z",path=('x', people_arg.reversed))),andL(p2("y"),work_in2("t",path=('y', people_arg.reversed))))
+    # constarint1 = andL(p1_is_real("x"), work_in1("z", path=('x', people_arg1.reversed)))
+    constarint2 = orL(andL(p2("x"),work_in2("z",path=('x', people_arg.reversed))),andL(p3("y"),work_in3("t",path=('y', people_arg.reversed))))
+    # constarint2 = andL(p2_is_real("x"), work_in2("z", path=('x', people_arg2.reversed)))
 
 data_list = generate_dataset(sample_num=1000)
 data_list = reader_format(data_list)
 
-p1["embedding"] = ReaderSensor(keyword="person1")
-p2["embedding"] = ReaderSensor(keyword="person2")
-p3["embedding"] = ReaderSensor(keyword="person3")
+person["embedding1"] = ReaderSensor(keyword="person1")
+person["embedding2"] = ReaderSensor(keyword="person2")
+person["embedding3"] = ReaderSensor(keyword="person3")
 
 graph.constraint[constarint1] = ReaderSensor(keyword="condition_1",label=True)
 graph.constraint[constarint2] = ReaderSensor(keyword="condition_2",label=True)
@@ -60,14 +58,14 @@ class PeopleModel(torch.nn.Module):
     def forward(self, p):
         return self.layer(p[0]).unsqueeze(0)
 
-is_real_person=PeopleModel(2)
-p1[p1_is_real] = ModuleSensor(p1["embedding"],module=is_real_person,device="cpu")
-p2[p2_is_real] = ModuleSensor(p2["embedding"],module=is_real_person,device="cpu")
-p3[p3_is_real] = ModuleSensor(p3["embedding"],module=is_real_person,device="cpu")
+is_real_person=PeopleModel(2).to("cpu")
+person[p1] = ModuleSensor(person["embedding1"],module=is_real_person,device="cpu")
+person[p2] = ModuleSensor(person["embedding2"],module=is_real_person,device="cpu")
+person[p3] = ModuleSensor(person["embedding3"],module=is_real_person,device="cpu")
 
-l1["embedding"] = ReaderSensor(keyword="location1")
-l2["embedding"] = ReaderSensor(keyword="location2")
-l3["embedding"] = ReaderSensor(keyword="location3")
+location["embedding1"] = ReaderSensor(keyword="location1")
+location["embedding2"] = ReaderSensor(keyword="location2")
+location["embedding3"] = ReaderSensor(keyword="location3")
 
 class WorkModel(torch.nn.Module):
     def __init__(self, size):
@@ -78,20 +76,12 @@ class WorkModel(torch.nn.Module):
     def forward(self, p, l):
         return self.layer(torch.cat([p[0], l[0]])).unsqueeze(0)
 
-work_in_model=WorkModel(4)
-pair1[people_arg1.reversed,location_arg1.reversed] = JointSensor(p1["embedding"],l1["embedding"],forward=lambda *_ : (torch.ones(1,1),torch.ones(1,1)))
-pair1[work_in1] = ModuleSensor(p1["embedding"],l1["embedding"],module=work_in_model,device="cpu")
-pair2[people_arg2.reversed,location_arg2.reversed] = JointSensor(p2["embedding"],l2["embedding"],forward=lambda *_ : (torch.ones(1,1),torch.ones(1,1)))
-pair2[work_in2] = ModuleSensor(p2["embedding"],l2["embedding"],module=work_in_model,device="cpu")
-pair3[people_arg3.reversed,location_arg3.reversed] = JointSensor(p3["embedding"],l3["embedding"],forward=lambda *_ : (torch.ones(1,1),torch.ones(1,1)))
-pair3[work_in3] = ModuleSensor(p3["embedding"],l3["embedding"],module=work_in_model,device="cpu")
+work_in_model=WorkModel(4).to("cpu")
+pair[people_arg.reversed,location_arg.reversed] = JointSensor(forward=lambda *_ : (torch.ones(1,1),torch.ones(1,1)))
+pair[work_in1] = ModuleSensor(person["embedding1"],location["embedding1"],module=work_in_model,device="cpu")
+pair[work_in2] = ModuleSensor(person["embedding2"],location["embedding2"],module=work_in_model,device="cpu")
+pair[work_in3] = ModuleSensor(person["embedding3"],location["embedding3"],module=work_in_model,device="cpu")
 
 # program = PrimalDualProgram(graph,SolverModel,poi=[p1,p2,p3,l1,l2,l3,pair1,pair2,pair3,graph.constraint],loss=MacroAverageTracker(NBCrossEntropyLoss()),device="cpu")
-program = InferenceProgram(
-    graph,
-    SolverModel,
-    poi=[p1,p2,p3,l1,l2,l3,pair1,pair2,pair3,graph.constraint],
-    device="cpu",
-    tnorm='G'
-)
-program.train(data_list, epochs=1, lr=1e-4, c_warmup_iters=0)
+program = InferenceProgram(graph,SolverModel,poi=[person,p1,p2,p3,location,pair,graph.constraint],device="cpu",tnorm='G')
+program.train(data_list, epochs=1, lr=1e-4, c_warmup_iters=0,device="cpu")
