c0b8f86c8e442bfc5c3f1723efe7e29a7959f4fb
Tanawan Premsri <premsrit@msu.edu>
2025-04-15 13:33:16 -0400

    Adding evaluation function

--- test_regr/InferenceAPI/PDPlus.py
+++ test_regr/InferenceAPI/PDPlus.py
@@ -85,3 +85,57 @@ pair[work_in3] = ModuleSensor(person["embedding3"],location["embedding3"],module
 # program = PrimalDualProgram(graph,SolverModel,poi=[p1,p2,p3,l1,l2,l3,pair1,pair2,pair3,graph.constraint],loss=MacroAverageTracker(NBCrossEntropyLoss()),device="cpu")
 program = InferenceProgram(graph,SolverModel,poi=[person,p1,p2,p3,location,pair,graph.constraint],device="cpu",tnorm='G')
 program.train(data_list, epochs=1, lr=1e-4, c_warmup_iters=0,device="cpu")
+
+
+
+program = InferenceProgram(graph, SolverModel, poi=[person, p1, p2, p3, location, pair, graph.constraint], device="cpu",
+                           tnorm='G')
+
+from tqdm import tqdm
+
+def check_constraints_acc(cur_program, cur_data):
+    acc = 0
+    total = 0
+    for datanode in tqdm(cur_program.populate(cur_data, device="cpu"), "Manually Testing"):
+        verify_constrains = datanode.verifyResultsLC()
+
+        condition_list = []
+        if not verify_constrains:
+            continue
+
+        for lc in verify_constrains:
+            condition_list.append(1 if verify_constrains[lc]["satisfied"] == 100.0 else 0)
+        constraints_keys = list(verify_constrains.keys())
+
+        # This might be a better way for this to finding the label, but this work
+        find_constraints_label = datanode.myBuilder.findDataNodesInBuilder(select=datanode.graph.constraint)[0]
+        constraint_labels_dict = find_constraints_label.getAttributes()
+        # Getting label of constraints and convert to 0-1
+        constraint_labels = [int(constraint_labels_dict[lc + "/label"].item()) for lc in constraints_keys]
+
+        # This is checking the constraint manually, should match the verify constraints
+        work1 = datanode.getAttribute(work_in1, 'local/softmax').argmax().item()
+        work2 = datanode.getAttribute(work_in2, 'local/softmax').argmax().item()
+        work3 = datanode.getAttribute(work_in3, 'local/softmax').argmax().item()
+
+        datanode_person = datanode.getRelationLinks(people_arg)[0]
+        people1 = datanode_person.getAttribute(p1, 'local/softmax').argmax().item()
+        people2 = datanode_person.getAttribute(p2, 'local/softmax').argmax().item()
+        people3 = datanode_person.getAttribute(p3, 'local/softmax').argmax().item()
+
+        # Condition used in ER examples
+        cond1_check = (work1 and people1) and (work2 and people2)
+        cond2_check = (work2 and people2) or (work3 and people3)
+        if condition_list[0] != cond1_check or condition_list[1] != cond2_check:
+            raise Exception("verify_constrains cannot be used")
+
+        # == check matching the label
+        acc += int(constraint_labels == condition_list)
+        total += 1
+
+    return acc / total
+
+
+print("Acc before training:", check_constraints_acc(program, data_list))
+program.train(data_list, train_epoch_num=10, lr=1e-6, c_lr=1e-6, c_warmup_iters=-1, device="cpu")
+print("Acc after training:", check_constraints_acc(program, data_list))
