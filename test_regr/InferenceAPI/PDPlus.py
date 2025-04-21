8c5061fe6e89bc8c62badee59a4783b7ce5c224b
Tanawan Premsri <premsrit@msu.edu>
2025-04-21 14:07:02 -0400

    Fix model parameters

--- test_regr/InferenceAPI/PDPlus.py
+++ test_regr/InferenceAPI/PDPlus.py
@@ -1,11 +1,14 @@
 import json
 import logging, torch
+
+import numpy as np
+
 from domiknows.graph import Graph, Concept, Relation, andL, orL
 from NER_utils import generate_dataset, reader_format
 from domiknows.program.loss import NBCrossEntropyLoss
 from domiknows.program.lossprogram import InferenceProgram, PrimalDualProgram
 from domiknows.program.model.pytorch import SolverModel
-from domiknows.sensor.pytorch import ModuleSensor, FunctionalSensor
+from domiknows.sensor.pytorch import ModuleSensor, FunctionalSensor, ModuleLearner
 from domiknows.sensor.pytorch.sensors import ReaderSensor, JointSensor
 from domiknows.program.metric import MacroAverageTracker
 
@@ -66,13 +69,14 @@ class PeopleModel(torch.nn.Module):
         self.layer = torch.nn.Linear(self.size, 2)
 
     def forward(self, p):
+        # print(self.layer.weight)
         return self.layer(p[0]).unsqueeze(0)
 
 
 is_real_person = PeopleModel(2).to("cpu")
-person[p1] = ModuleSensor(person["embedding1"], module=is_real_person, device="cpu")
-person[p2] = ModuleSensor(person["embedding2"], module=is_real_person, device="cpu")
-person[p3] = ModuleSensor(person["embedding3"], module=is_real_person, device="cpu")
+person[p1] = ModuleLearner(person["embedding1"], module=is_real_person, device="cpu")
+person[p2] = ModuleLearner(person["embedding2"], module=is_real_person, device="cpu")
+person[p3] = ModuleLearner(person["embedding3"], module=is_real_person, device="cpu")
 
 location["embedding1"] = ReaderSensor(keyword="location1")
 location["embedding2"] = ReaderSensor(keyword="location2")
@@ -86,23 +90,25 @@ class WorkModel(torch.nn.Module):
         self.layer = torch.nn.Linear(self.size, 2)
 
     def forward(self, p, l):
+        # print(self.layer.weight)
         return self.layer(torch.cat([p[0], l[0]])).unsqueeze(0)
 
 work_in_model = WorkModel(4).to("cpu")
 pair[people_arg.reversed, location_arg.reversed] = JointSensor(forward=lambda *_: (torch.ones(1, 1), torch.ones(1, 1)))
-pair[work_in1] = ModuleSensor(person["embedding1"], location["embedding1"], module=work_in_model, device="cpu")
-pair[work_in2] = ModuleSensor(person["embedding2"], location["embedding2"], module=work_in_model, device="cpu")
-pair[work_in3] = ModuleSensor(person["embedding3"], location["embedding3"], module=work_in_model, device="cpu")
+pair[work_in1] = ModuleLearner(person["embedding1"], location["embedding1"], module=work_in_model, device="cpu")
+pair[work_in2] = ModuleLearner(person["embedding2"], location["embedding2"], module=work_in_model, device="cpu")
+pair[work_in3] = ModuleLearner(person["embedding3"], location["embedding3"], module=work_in_model, device="cpu")
 
-# program = PrimalDualProgram(graph,SolverModel,poi=[p1,p2,p3,l1,l2,l3,pair1,pair2,pair3,graph.constraint],loss=MacroAverageTracker(NBCrossEntropyLoss()),device="cpu")
 program = InferenceProgram(graph, SolverModel, poi=[person, p1, p2, p3, location, pair, graph.constraint], device="cpu",
                            tnorm='G')
+
+# print(list(program.model.parameters()))
 
 from tqdm import tqdm
 
 
 def check_constraints_acc(cur_program, cur_data):
-    acc = 0
+    acc = [0, 0]
     total = 0
     for datanode in tqdm(cur_program.populate(cur_data, device="cpu"), "Manually Testing"):
         verify_constrains = datanode.verifyResultsLC()
@@ -113,37 +119,44 @@ def check_constraints_acc(cur_program, cur_data):
 
         for lc in verify_constrains:
             condition_list.append(1 if verify_constrains[lc]["satisfied"] == 100.0 else 0)
-        constraints_keys = list(verify_constrains.keys())
 
         # This might be a better way for this to finding the label, but this work
+        constraints_keys = list(verify_constrains.keys())
         find_constraints_label = datanode.myBuilder.findDataNodesInBuilder(select=datanode.graph.constraint)[0]
         constraint_labels_dict = find_constraints_label.getAttributes()
         # Getting label of constraints and convert to 0-1
         constraint_labels = [int(constraint_labels_dict[lc + "/label"].item()) for lc in constraints_keys]
 
-        # This is checking the constraint manually, should match the verify constraints
-        work1 = datanode.getAttribute(work_in1, 'local/softmax').argmax().item()
-        work2 = datanode.getAttribute(work_in2, 'local/softmax').argmax().item()
-        work3 = datanode.getAttribute(work_in3, 'local/softmax').argmax().item()
 
-        datanode_person = datanode.getRelationLinks(people_arg)[0]
-        people1 = datanode_person.getAttribute(p1, 'local/softmax').argmax().item()
-        people2 = datanode_person.getAttribute(p2, 'local/softmax').argmax().item()
-        people3 = datanode_person.getAttribute(p3, 'local/softmax').argmax().item()
+        # # This is checking the constraint manually, should match the verify constraints
+        # work1 = datanode.getAttribute(work_in1, 'local/softmax').argmax().item()
+        # work2 = datanode.getAttribute(work_in2, 'local/softmax').argmax().item()
+        # work3 = datanode.getAttribute(work_in3, 'local/softmax').argmax().item()
+        #
+        # datanode_person = datanode.getRelationLinks(people_arg)[0]
+        # people1 = datanode_person.getAttribute(p1, 'local/softmax').argmax().item()
+        # # print(datanode_person.getAttribute(p1, 'local/softmax'))
+        # people2 = datanode_person.getAttribute(p2, 'local/softmax').argmax().item()
+        # people3 = datanode_person.getAttribute(p3, 'local/softmax').argmax().item()
+        #
+        # # Condition used in ER examples
+        # cond1_check = (work1 and people1) and (work2 and people2)
+        # cond2_check = (work2 and people2) or (work3 and people3)
+        # if condition_list[0] != cond1_check or condition_list[1] != cond2_check:
+        #     raise Exception("verify_constrains cannot be used")
 
-        # Condition used in ER examples
-        cond1_check = (work1 and people1) and (work2 and people2)
-        cond2_check = (work2 and people2) or (work3 and people3)
-        if condition_list[0] != cond1_check or condition_list[1] != cond2_check:
-            raise Exception("verify_constrains cannot be used")
-
+        # print(constraint_labels, condition_list)
         # == check matching the label
-        acc += int(constraint_labels == condition_list)
+        for i in range(2):
+            acc[i] += int(constraint_labels[i] == condition_list[i])
         total += 1
 
-    return acc / total
+    return np.array(acc) / total
 
 
+print(list(program.model.parameters()))
 print("Acc before training:", check_constraints_acc(program, data_list))
-program.train(data_list, train_epoch_num=10, lr=1e-4, c_lr=1e-8, c_warmup_iters=-1, device="cpu")
+# print(list(program.model.parameters()))
+program.train(data_list, Optim=lambda param: torch.optim.AdamW(param, 1e-3), train_epoch_num=100, c_lr=1e-4, c_warmup_iters=-1, device="cpu")
 print("Acc after training:", check_constraints_acc(program, data_list))
+print(list(program.model.parameters()))
