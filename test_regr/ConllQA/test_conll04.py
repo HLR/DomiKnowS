4268531c43b0b04d71dfdd0ea3947dbd091474ec
Tanawan Premsri <premsrit@msu.edu>
2025-10-20 08:36:33 -0400

    Update Testcase

--- test_regr/ConllQA/test_conll04.py
+++ test_regr/ConllQA/test_conll04.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 def test_zero_counting_godel():
-    command = ['python', 'main.py', "--epochs", "1", "--train_size", "10", "--train_portion", "zero_counting_YN"]
+    command = ['python', 'main.py', "--epochs", "1", "--train_portion", "zero_counting_YN"]
     result = subprocess.run(command, capture_output=True, text=True)
     # Checking no assert error in the main command (Accuracy error / General error)
     print(result.stdout)
@@ -11,7 +11,7 @@ def test_zero_counting_godel():
     assert result.returncode == 0, result.stderr[result.stderr.find("Traceback"):]
 
 def test_zero_counting_lukas():
-    command = ['python', 'main.py', "--epochs", "1", "--train_size", "10", "--train_portion", "zero_counting_YN", "--counting_tnorm", "L"]
+    command = ['python', 'main.py', "--epochs", "1", "--train_portion", "zero_counting_YN", "--counting_tnorm", "L"]
     result = subprocess.run(command, capture_output=True, text=True)
     # Checking no assert error in the main command (Accuracy error / General error)
     print(result.stdout)
@@ -19,14 +19,14 @@ def test_zero_counting_lukas():
     assert result.returncode == 0, result.stderr[result.stderr.find("Traceback"):]
 
 def test_zero_counting_product():
-    command = ['python', 'main.py', "--epochs", "1", "--train_size", "10", "--train_portion", "zero_counting_YN", "--counting_tnorm", "P"]
+    command = ['python', 'main.py', "--epochs", "1", "--train_portion", "zero_counting_YN", "--counting_tnorm", "P"]
     result = subprocess.run(command, capture_output=True, text=True)
     # Checking no assert error in the main command (Accuracy error / General error)
     assert result.returncode == 0, result.stderr[result.stderr.find("Traceback"):]
 
 
 def test_zero_counting_simple_product():
-    command = ['python', 'main.py', "--epochs", "1", "--train_size", "10", "--train_portion", "zero_counting_YN", "--counting_tnorm", "SP"]
+    command = ['python', 'main.py', "--epochs", "1", "--train_portion", "zero_counting_YN", "--counting_tnorm", "SP"]
     result = subprocess.run(command, capture_output=True, text=True)
     # Checking no assert error in the main command (Accuracy error / General error)
     assert result.returncode == 0, result.stderr[result.stderr.find("Traceback"):]
@@ -34,25 +34,25 @@ def test_zero_counting_simple_product():
 
 
 def test_over_counting_godel():
-    command = ['python', 'main.py', "--epochs", "1", "--train_size", "10", "--train_portion", "over_counting_YN", "--counting_tnorm", "G"]
+    command = ['python', 'main.py', "--epochs", "1", "--train_portion", "over_counting_YN", "--counting_tnorm", "G"]
     result = subprocess.run(command, capture_output=True, text=True)
     # Checking no assert error in the main command (Accuracy error / General error)
     assert result.returncode == 0, result.stderr[result.stderr.find("Traceback"):]
 
 def test_over_counting_lukas():
-    command = ['python', 'main.py', "--epochs", "1", "--train_size", "10", "--train_portion", "over_counting_YN", "--counting_tnorm", "L"]
+    command = ['python', 'main.py', "--epochs", "1", "--train_portion", "over_counting_YN", "--counting_tnorm", "L"]
     result = subprocess.run(command, capture_output=True, text=True)
     # Checking no assert error in the main command (Accuracy error / General error)
     assert result.returncode == 0, result.stderr[result.stderr.find("Traceback"):]
 
 def test_over_counting_product():
-    command = ['python', 'main.py', "--epochs", "1", "--train_size", "10", "--train_portion", "over_counting_YN", "--counting_tnorm", "P"]
+    command = ['python', 'main.py', "--epochs", "1", "--train_portion", "over_counting_YN", "--counting_tnorm", "P"]
     result = subprocess.run(command, capture_output=True, text=True)
     # Checking no assert error in the main command (Accuracy error / General error)
     assert result.returncode == 0, result.stderr[result.stderr.find("Traceback"):]
 
 def test_over_counting_simple_produce():
-    command = ['python', 'main.py', "--epochs", "1", "--train_size", "10", "--train_portion", "over_counting_YN", "--counting_tnorm", "SP"]
+    command = ['python', 'main.py', "--epochs", "1", "--train_portion", "over_counting_YN", "--counting_tnorm", "SP"]
     result = subprocess.run(command, capture_output=True, text=True)
     # Checking no assert error in the main command (Accuracy error / General error)
     assert result.returncode == 0, result.stderr[result.stderr.find("Traceback"):]
