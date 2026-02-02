fbe8a6032f93721838b40058d700a44d0be3a394
Tanawan Premsri <premsrit@msu.edu>
2026-02-02 15:31:58 -0500

    Update CLEVR translator

--- test_regr/Clever/convert_CLEVR_domiKnowS.py
+++ test_regr/Clever/convert_CLEVR_domiKnowS.py
@@ -3,156 +3,7 @@ from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Union
 
 
-# ---------- Internal expression trees ----------
-@dataclass(frozen=True)
-class Cond:  # boolean condition over variable x
-    op: str
-    args: tuple
-
-
-def AND(*conds: Cond) -> Cond:
-    # flatten nested ands and drop trivial singletons
-    flat = []
-    for c in conds:
-        if c.op == "and":
-            flat.extend(c.args)
-        else:
-            flat.append(c)
-    if len(flat) == 1:
-        return flat[0]
-    return Cond("and", tuple(flat))
-
-
-def OR(*conds: Cond) -> Cond:
-    flat = []
-    for c in conds:
-        if c.op == "or":
-            flat.extend(c.args)
-        else:
-            flat.append(c)
-    if len(flat) == 1:
-        return flat[0]
-    return Cond("or", tuple(flat))
-
-
-def LESS(*conds: Cond) -> Cond:
-    flat = []
-    for c in conds:
-        if c.op == "count":
-            flat.append(c.args)
-        else:
-            flat.append(c)
-    return Cond("less", tuple(flat))
-
-
-def MORE(*conds: Cond) -> Cond:
-    flat = []
-    for c in conds:
-        if c.op == "count":
-            flat.append(c.args)
-        else:
-            flat.append(c)
-    return Cond("greater", tuple(flat))
-
-
-def EQUAL(*conds: Cond) -> Cond:
-    flat = []
-    for c in conds:
-        if c.op == "count":
-            flat.append(c.args)
-        else:
-            flat.append(c)
-    return Cond("equalCounts", tuple(flat))
-
-
-def PRED(name: str) -> Cond:
-    return Cond("pred", (name,))
-
-
-def RELATE(name: str) -> Cond:
-    return Cond("relate", (name,))
-
-
-# ---------- Rendering to YOUR DSL ----------
-var = 0
-use_count = 0
-relation_val = 0
-need_relation2 = False
-
-
-def render_cond_to_your_dsl(cond: Cond) -> str:
-    """
-    Renders Cond to:
-      - pred: is_<name>('x') or is_<name>(path='x') depending on `use_path_kw`
-      - and/or: andL(...), orL(...)
-    """
-    global relation_val
-    global var
-    global use_count
-    global need_relation2
-    if cond.op == "pred":
-        if cond.args[0] == "scene":
-            return ""
-        pred_name = cond.args[0]
-        # your examples mix is_large('x') and is_gray(path='x').
-        # We'll default to using path='x' for most, but allow special-casing below.
-        var_name = chr(var + 97)
-        use_count += 1
-        suffix = ""
-        if use_count > 1:
-            return f"{pred_name}(path='{var_name}')"
-        if need_relation2:
-            suffix = f", path=('rel{relation_val}', obj2)"
-            need_relation2 = False
-        return f"{pred_name}('{var_name}'{suffix})"
-
-    if cond.op == "and" or cond.op == "or":
-        all_str = [render_cond_to_your_dsl(c) for c in cond.args[:]]
-        all_str = [c for c in all_str if c != ""]
-        if len(all_str) == 1:
-            return all_str[0]
-        return f"{cond.op}L(" + ", ".join(all_str) + ")"
-
-    if cond.op == "unique":
-        expr = render_cond_to_your_dsl(cond.args)
-        return f"{expr}"
-
-    if cond.op == "count":
-        var += 1
-        use_count = 0
-        expr = render_cond_to_your_dsl(cond.args)
-        return f"sumL({expr})"
-
-    if cond.op == "equalCounts" or cond.op == "less" or cond.op == "greater":
-        var += 1
-        use_count = 0
-        x_vars = render_cond_to_your_dsl(cond.args[0])
-        var += 1
-        use_count = 0
-        y_vars = render_cond_to_your_dsl(cond.args[1])
-        return f"{cond.op}L({x_vars}, {y_vars})"
-
-    if cond.op == "exist":
-        var += 1
-        use_count = 0
-        expr = render_cond_to_your_dsl(cond.args)
-        return f"existsL(andL({expr}))"
-
-    if cond.op == "relate":
-        var_name = chr(var + 97)
-        pred_name = cond.args[0]
-        relation_val += 1
-        str_path = f"{pred_name}('rel{relation_val}', path=('{var_name}', obj1.reversed))"
-
-        need_relation2 = True
-
-        return str_path
-
-    raise ValueError(f"Unknown Cond op: {cond.op}")
-
-
-# ---------- Translator from their step program ----------
-def translate_left_steps_to_your_dsl(all_program, current_idx, first_initial, indent=0):
+def translate_left_domiknows(all_program, current_idx, first_initial, indent=0):
     """
     Supports the common CLEVR-like operators:
       scene, filter_color, filter_material, filter_shape, filter_size,
@@ -174,7 +25,7 @@ def translate_left_steps_to_your_dsl(all_program, current_idx, first_initial, in
         relation_val = 0
         need_relation2 = False
 
-    values: List[Union[Cond, str]] = []
+    values = []
     step = all_program[current_idx]
 
     fn = step["function"]
@@ -212,31 +63,31 @@ def translate_left_steps_to_your_dsl(all_program, current_idx, first_initial, in
             var_name = chr(var + 96)
             init_str = f"{attr_value}(path=('{var_name}'))"
 
-        filter_str = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial=False)
+        filter_str = translate_left_domiknows(all_program, ins[0], first_initial=False)
 
         return f"{init_str}, {filter_str}" if filter_str != "" else f"{init_str}"
 
     elif fn == "union":
         if len(ins) != 2:
             raise ValueError(f"union expects 2 inputs at step {current_idx}")
-        base_ins_l = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial)
-        base_ins_r = translate_left_steps_to_your_dsl(all_program, ins[1], first_initial)
+        base_ins_l = translate_left_domiknows(all_program, ins[0], first_initial)
+        base_ins_r = translate_left_domiknows(all_program, ins[1], first_initial)
 
         return f"orL({base_ins_l}, {base_ins_r})"
 
     elif fn == "count":
         if len(ins) != 1:
             raise ValueError(f"count expects 1 input at step {current_idx}")
-        base_ins = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial=True)
+        base_ins = translate_left_domiknows(all_program, ins[0], first_initial=True)
 
         return f"sumL(andL({base_ins}))"
 
     elif fn == "less_than":
         if len(ins) != 2:
             raise ValueError(f"less than expects 2 input at step {current_idx}")
-        base_ins_l = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial=True)
+        base_ins_l = translate_left_domiknows(all_program, ins[0], first_initial=True)
         var += 1
-        base_ins_r = translate_left_steps_to_your_dsl(all_program, ins[1], first_initial=True)
+        base_ins_r = translate_left_domiknows(all_program, ins[1], first_initial=True)
         var += 1
 
         return f"lessL(andL({base_ins_l}), andL({base_ins_r}))"
@@ -244,9 +95,9 @@ def translate_left_steps_to_your_dsl(all_program, current_idx, first_initial, in
     elif fn == "greater_than":
         if len(ins) != 2:
             raise ValueError(f"greater than expects 2 input at step {current_idx}")
-        base_ins_l = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial=True)
+        base_ins_l = translate_left_domiknows(all_program, ins[0], first_initial=True)
         var += 1
-        base_ins_r = translate_left_steps_to_your_dsl(all_program, ins[1], first_initial=True)
+        base_ins_r = translate_left_domiknows(all_program, ins[1], first_initial=True)
         var += 1
 
         return f"greaterL(andL({base_ins_l}), andL({base_ins_r}))"
@@ -254,7 +105,7 @@ def translate_left_steps_to_your_dsl(all_program, current_idx, first_initial, in
     elif fn == "unique":
         if len(ins) != 1:
             raise ValueError(f"exist expects 1 input at step {current_idx}")
-        base_ins = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial=True)
+        base_ins = translate_left_domiknows(all_program, ins[0], first_initial=True)
         var += 1
 
         return f"{base_ins}"
@@ -262,15 +113,15 @@ def translate_left_steps_to_your_dsl(all_program, current_idx, first_initial, in
     elif fn == "exist":
         if len(ins) != 1:
             raise ValueError(f"exist expects 1 input at step {current_idx}")
-        base_ins = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial=True)
+        base_ins = translate_left_domiknows(all_program, ins[0], first_initial=True)
         var += 1
         return f"existsL(andL({base_ins}))"
 
     elif fn == "intersect":
         if len(ins) != 2:
             raise ValueError(f"intersect expects 2 inputs at step {current_idx}")
-        base_ins_l = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial)
-        base_ins_r = translate_left_steps_to_your_dsl(all_program, ins[1], first_initial)
+        base_ins_l = translate_left_domiknows(all_program, ins[0], first_initial)
+        base_ins_r = translate_left_domiknows(all_program, ins[1], first_initial)
 
         return f"andL({base_ins_l}, {base_ins_r})"
 
@@ -278,12 +129,19 @@ def translate_left_steps_to_your_dsl(all_program, current_idx, first_initial, in
         if len(ins) != 2:
             raise ValueError(f"union expects 2 inputs at step {current_idx}")
 
-        base_ins_l = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial=True)
+        base_ins_l = translate_left_domiknows(all_program, ins[0], first_initial=True)
         var += 1
-        base_ins_r = translate_left_steps_to_your_dsl(all_program, ins[1], first_initial=True)
+        base_ins_r = translate_left_domiknows(all_program, ins[1], first_initial=True)
         var += 1
 
         return f"equalCounts(andL({base_ins_l}), andL({base_ins_r}))"
+
+    elif "query" in fn:
+        if len(ins) != 1:
+            raise ValueError(f"query expects 1 inputs at step {current_idx}")
+        query_type = fn.split("_")[1]
+        target_obj = translate_left_domiknows(all_program, ins[0], first_initial=True)
+        return f"queryL({query_type}, iota(andL({target_obj})))"
 
     elif fn == "relate":
         if len(ins) != 1:
@@ -307,8 +165,7 @@ def translate_left_steps_to_your_dsl(all_program, current_idx, first_initial, in
 
         need_relation2 = True
         relation_val += 1
-        next_obj = translate_left_steps_to_your_dsl(all_program, ins[0], first_initial=True)
-
+        next_obj = translate_left_domiknows(all_program, ins[0], first_initial=True)
         return f"{str_path}, {next_obj}"
     else:
         raise NotImplementedError(f"Unsupported function '{fn}' at step {current_idx}")
@@ -327,61 +184,21 @@ if __name__ == "__main__":
     #                                            first_initial=True))
     #     break
 
-    program = [
-        {
-            "inputs": [],
-            "function": "scene",
-            "value_inputs": []
-        },
-        {
-            "inputs": [
-                0
-            ],
-            "function": "filter_shape",
-            "value_inputs": [
-                "cube"
-            ]
-        },
-        {
-            "inputs": [
-                1
-            ],
-            "function": "unique",
-            "value_inputs": []
-        },
-        {
-            "inputs": [
-                2
-            ],
-            "function": "relate",
-            "value_inputs": [
-                "front"
-            ]
-        },
-        {
-            "inputs": [
-                3
-            ],
-            "function": "unique",
-            "value_inputs": []
-        },
-        {
-            "inputs": [
-                4
-            ],
-            "function": "relate",
-            "value_inputs": [
-                "left"
-            ]
-        },
-        {
-            "inputs": [
-                5
-            ],
-            "function": "exist",
-            "value_inputs": []
-        }
-    ]
-    print(translate_left_steps_to_your_dsl(program, len(program) - 1, first_initial=True))
+    program = [{'inputs': [], 'function': 'scene', 'value_inputs': []},
+               {'inputs': [0], 'function': 'filter_size', 'value_inputs': ['large']},
+               {'inputs': [1], 'function': 'filter_color', 'value_inputs': ['brown']},
+               {'inputs': [2], 'function': 'filter_shape', 'value_inputs': ['sphere']},
+               {'inputs': [3], 'function': 'unique', 'value_inputs': []},
+               {'inputs': [4], 'function': 'relate', 'value_inputs': ['left']},
+               {'inputs': [], 'function': 'scene', 'value_inputs': []},
+               {'inputs': [6], 'function': 'filter_color', 'value_inputs': ['brown']},
+               {'inputs': [7], 'function': 'filter_shape', 'value_inputs': ['cylinder']},
+               {'inputs': [8], 'function': 'unique', 'value_inputs': []},
+               {'inputs': [9], 'function': 'relate', 'value_inputs': ['right']},
+               {'inputs': [5, 10], 'function': 'intersect', 'value_inputs': []},
+               {'inputs': [11], 'function': 'filter_size', 'value_inputs': ['large']},
+               {'inputs': [12], 'function': 'unique', 'value_inputs': []},
+               {'inputs': [13], 'function': 'query_material', 'value_inputs': []}]
+    print(translate_left_domiknows(program, len(program) - 1, first_initial=True))
     # for result in results:
     #     str_op = translate_left_steps_to_your_dsl(result["program"])
