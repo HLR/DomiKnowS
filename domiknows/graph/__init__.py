6f21f175f1629e4e8dfa2e3510d9a7ae4c5a757c
Andrzej Uszok <auszok@ihmc.org>
2026-03-02 09:46:27 -0600

    Add missing imports for logical constraints in __init__.py

--- domiknows/graph/__init__.py
+++ domiknows/graph/__init__.py
@@ -6,6 +6,9 @@ from .logicalConstrain import andL, nandL, orL, ifL, norL, xorL, notL, equivalen
 from .logicalConstrain import eqL, fixedL, forAllL
 from .logicalConstrain import existsL, atLeastL, atMostL, exactL
 from .logicalConstrain import existsAL, atLeastAL, atMostAL, exactAL
+from .logicalConstrain import iotaL, queryL, sumL
+from .logicalConstrain import greaterL, greaterEqL, lessL, lessEqL, equalCountsL, notEqualCountsL
+from .logicalConstrain import execute
 from .candidates import CandidateSelection, combinationC
 from .property import Property
 from .trial import Trial
