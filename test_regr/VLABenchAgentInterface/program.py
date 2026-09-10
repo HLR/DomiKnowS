2c6ad1181a9dad5ec5627554d50ce3b42c68f27f
Andrzej Uszok <auszok@ihmc.org>
2026-09-10 00:13:50 -0500

    increase condiment lift assist step

--- test_regr/VLABenchAgentInterface/program.py
+++ test_regr/VLABenchAgentInterface/program.py
@@ -1299,7 +1299,7 @@ class VLABenchHierarchicalReinforcementProgram(ReinforcementProgram):
                                     [
                                         lift_target_robot[0],
                                         lift_target_robot[1],
-                                        current[2] + 0.005,
+                                        current[2] + 0.02,
                                     ],
                                     dtype=np.float64,
                                 )
